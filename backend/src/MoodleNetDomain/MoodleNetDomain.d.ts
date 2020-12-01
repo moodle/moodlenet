67790592f1e9f4bc2cab1ea8e8ed64d2bd58d3a8
aleclofabbro <alessandro.giansanti@gmail.com>
2020-12-01 19:14:58 +0100

    split Domain definition

--- backend/src/MoodleNetDomain/MoodleNetDomain.d.ts
+++ backend/src/MoodleNetDomain/MoodleNetDomain.d.ts
@@ -1,42 +1,7 @@
-import { Api } from '../lib/domain/api/types'
-import { LookupType, Event } from '../lib/domain/event/types'
-import { Flow } from '../lib/domain/types/path'
-import { EmailObj, VerifyEmailReq } from './services/Email/types'
+import { Accounting } from './services/Accounting/Accounting'
+import { Email } from './services/Email/Email'
 
 export type MoodleNetDomain = {
-  Accounting: {
-    Register_New_Account: {
-      Request: Api<AccountRequest, { success: true } | { success: false; reason: string }>
-      Email_Confirm_Result: Api<
-        LookupType<MoodleNetDomain, 'Email.Verify_Email.Result'>,
-        { done: boolean }
-      >
-      ActivateNewAccount: Api<
-        { requestFlowKey: string; password: string; username: string },
-        { success: true } | { success: false; reason: string }
-      >
-    }
-    AccountActivated: Event<{ requestFlowKey: string }>
-  }
-
-  Email: {
-    Send_One: {
-      Req: Api<
-        { emailObj: EmailObj },
-        { success: true; emailId: string } | { error: string; success: false }
-      >
-      SentEmail: Event<{ success: true; emailId: string } | { error: string; success: false }>
-    }
-    Verify_Email: {
-      Req: Api<VerifyEmailReq, {}>
-      Attempt_Send: Api<{}, { success: true } | { error: string; success: false }>
-      Confirm_Email: Api<
-        { token: string },
-        { success: true; flow: Flow } | { error: string; success: false }
-      >
-      Result: Event<{ email: string } & ({ success: true } | { error: string; success: false })>
-    }
-  }
+  Accounting: Accounting
+  Email: Email
 }
-
-export type AccountRequest = { email: string }
