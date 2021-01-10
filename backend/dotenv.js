f398eb1cbea2d0ddcf6364a66347d077e4a416eb
aleclofabbro <alessandro.giansanti@gmail.com>
2021-01-10 15:39:10 +0100

    renamed services/ to apis/ and *.service.ts to *.api.ts

--- backend/dotenv.js
+++ backend/dotenv.js
@@ -1,3 +1,7 @@
+// TODO: implement all services with no in-code-env-vars-defaults
+// define a git-tracked .defaults.env with *all* defaultable env vars with docs
+// try to find a way for workers-implementations to define their own .defaults.env too, and be loaded in some way at start
+
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 
