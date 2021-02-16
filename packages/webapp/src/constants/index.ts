8a114d7cc9145da3a64fb7af0b642cb67bdaea3d
aleclofabbro <alessandro.giansanti@gmail.com>
2021-02-16 11:20:14 +0100

    setup apollodevtools

--- packages/webapp/src/constants/index.ts
+++ packages/webapp/src/constants/index.ts
@@ -2,3 +2,5 @@ export const NODE_ENV = process.env.NODE_ENV
 export const PUBLIC_URL = process.env.PUBLIC_URL
 
 export const GRAPHQL_ENDPOINT = process.env.REACT_APP_GRAPHQL_ENDPOINT
+
+export const env = process.env.NODE_ENV
--- packages/webapp/src/contexts/Global/Apollo/client.ts
+++ packages/webapp/src/contexts/Global/Apollo/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
-import { GRAPHQL_ENDPOINT } from '../../../constants'
+import { env, GRAPHQL_ENDPOINT } from '../../../constants'
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache({}),
   link: new HttpLink({ uri: GRAPHQL_ENDPOINT }),
+  connectToDevTools: env !== 'production',
 })
