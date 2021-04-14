b372530bbdcfd7132e9fee3c09df66074f269040
aleclofabbro <alessandro.giansanti@gmail.com>
2021-04-14 10:20:23 +0200

    removed unused mutation defs in pages gql changed email sender to be Moodlenet

--- packages/backend/src/MoodleNetDomain/services/UserAuth/assets/defaultConfig/index.ts
+++ packages/backend/src/MoodleNetDomain/services/UserAuth/assets/defaultConfig/index.ts
@@ -7,17 +7,17 @@ export const DefaultConfig: UserAuthConfig = {
   createdAt: 1606513900934, //2020-11-27T21:51:40.934Z
   newUserRequestEmail: {
     ...newUserRequestEmail,
-    from: 'Bob <bob@example.com>',
+    from: 'Moodlenet <noreply@moodle.net>',
     subject: 'please confirm your email',
   },
   changeUserEmailRequestEmail: {
     ...changeUserEmailRequestEmail,
-    from: 'Bob <bob@example.com>',
+    from: 'Moodlenet <noreply@moodle.net>',
     subject: 'please confirm your new email',
   },
   tempSessionEmail: {
     ...tempSessionEmail,
-    from: 'Bob <bob@example.com>',
+    from: 'Moodlenet <noreply@moodle.net>',
     subject: 'reset password',
   },
   newUserVerificationWaitSecs: 2 * 24 * 60 * 60,
