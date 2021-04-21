78344dfb6a7b0d64cf63bde3a7d51f57383c65ea
aleclofabbro <alessandro.giansanti@gmail.com>
2021-04-21 11:04:30 +0200

    moved env vars retrieval in defaultDeploy

--- packages/backend/src/MoodleNetDomain/defaultDeploy.ts
+++ packages/backend/src/MoodleNetDomain/defaultDeploy.ts
@@ -9,20 +9,23 @@ import {
 } from './services/ContentGraph/impl/arango/defaultDeploy'
 import { defaultArangoEmailSetup, defaultArangoEmailStartServices } from './services/Email/impl/arango/defaultDeploy'
 import { createMailgunSender } from './services/Email/sendersImpl/mailgun/mailgunSender'
+import { startGraphQLHTTPGateway } from './services/GraphQLHTTPGateway/GraphQLHTTPGateway'
+import { startHttpServer } from './services/HTTPServer/HTTPServer'
 import {
   defaultArangoUserAuthImpl,
   defaultArangoUserAuthStartServices,
 } from './services/UserAuth/impl/arango/defaultDeploy'
 
-export const startDefaultMoodlenet = async ({
-  arangoUrl,
-  mailgunApiKey,
-  mailgunDomain,
-}: {
-  arangoUrl: string
-  mailgunApiKey: string
-  mailgunDomain: string
-}) => {
+export const startDefaultMoodlenet = async () => {
+  const httpGqlPort = Number(process.env.HTTP_GRAPHQL_PORT) || 8080
+  const mailgunApiKey = process.env.MAILGUN_API_KEY
+  const mailgunDomain = process.env.MAILGUN_DOMAIN
+  const arangoUrl = process.env.ARANGO_HOST
+  if (!(arangoUrl && mailgunApiKey && mailgunDomain)) {
+    throw new Error(`missing env`)
+  }
+  const expressApp = startHttpServer({ port: httpGqlPort })
+  startGraphQLHTTPGateway({ app: expressApp.serviceRoot })
   const baseDbCfg: Config = {
     url: arangoUrl,
   }
