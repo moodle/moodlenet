558beccc64553486447d729fef8627d98873637e
aleclofabbro <alessandro.giansanti@gmail.com>
2021-06-28 13:11:03 +0200

    fixed some flaws

--- packages/webapp/src/ui/components/Button/Button.stories.tsx
+++ packages/webapp/src/ui/components/Button/Button.stories.tsx
@@ -1,37 +1,34 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
-import { Button } from './Button';
-
-
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Button } from './Button'
 export default {
   title: 'MoodleNet/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
   primary: true,
   label: 'Button',
-};
+}
 
-export const Secondary = Template.bind({});
+export const Secondary = Template.bind({})
 Secondary.args = {
   label: 'Button',
-};
+}
 
-export const Large = Template.bind({});
+export const Large = Template.bind({})
 Large.args = {
   size: 'large',
   label: 'Button',
-};
+}
 
-export const Small = Template.bind({});
+export const Small = Template.bind({})
 Small.args = {
   size: 'small',
   label: 'Button',
-};
+}
