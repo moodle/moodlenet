a72c8247075ca0f9bff7b6e53dbf0540f752a283
aleclofabbro <alessandro.giansanti@gmail.com>
2021-07-05 14:42:30 +0200

    Initial default config for UserAuth added a version step (0.0.2) on arango-user-auth db maintainance added yarn upgrade-version script

--- packages/backend/src/defaultDeploy/setup/db.ts
+++ packages/backend/src/defaultDeploy/setup/db.ts
@@ -1,7 +1,7 @@
 import { Database } from 'arangojs'
 import contentGraphLadder from '../../adapters/content-graph/arangodb/db/maintainance/ladder'
 import userAuthLadder from '../../adapters/user-auth/arangodb/db/maintainance/ladder'
-import { initializeDB } from '../../lib/helpers/arango/migrate/lib'
+import { initializeDB, upgradeToLatest } from '../../lib/helpers/arango/migrate/lib'
 import { DBEnv } from '../env/db'
 
 export const setupDb = async ({
@@ -20,4 +20,13 @@ export const setupDb = async ({
   await initializeDB({ dbname: userAuthDBName, ladder: userAuthLadder, forceDrop })({ sys_db })
 }
 
-export default setupDb
+export const upgradeToLatestDb = async ({ env: { arangoUrl, contentGraphDBName, userAuthDBName } }: { env: DBEnv }) => {
+  const contentGraphDB = new Database({ url: arangoUrl, databaseName: contentGraphDBName })
+  const userAuthDB = new Database({ url: arangoUrl, databaseName: userAuthDBName })
+
+  console.log(`upgrading ContentGraphDB`)
+  await upgradeToLatest({ ladder: contentGraphLadder })({ db: contentGraphDB })
+
+  console.log(`upgrading UserAuthDB`)
+  await upgradeToLatest({ ladder: userAuthLadder })({ db: userAuthDB })
+}
