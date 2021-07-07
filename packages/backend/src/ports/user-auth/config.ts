6d5b9cf46c298b99ad551b62930d2ac300accff2
aleclofabbro <alessandro.giansanti@gmail.com>
2021-07-07 14:02:22 +0200

    implemented adapters and ports for userauth config

---
+++ packages/backend/src/ports/user-auth/config.ts
@@ -0,0 +1,31 @@
+import { UserAuthConfig } from '../../adapters/user-auth/arangodb/types'
+import { SessionEnv } from '../../lib/auth/types'
+import { QMModule, QMQuery } from '../../lib/qmino'
+
+export type Adapter = {
+  getLatestConfig(): Promise<UserAuthConfig>
+  saveConfig(_: UserAuthConfig): Promise<unknown>
+}
+export const getLatest = QMQuery(
+  ({ sessionEnv: { user } }: { sessionEnv: SessionEnv }) =>
+    async ({ getLatestConfig }: Adapter) => {
+      if (!(user.role === 'Admin' || user.role === 'System')) {
+        return false
+      }
+      const latestConfig = await getLatestConfig()
+      return latestConfig
+    },
+)
+
+export const save = QMQuery(
+  ({ cfg, sessionEnv: { user } }: { cfg: UserAuthConfig; sessionEnv: SessionEnv }) =>
+    async ({ saveConfig }: Adapter) => {
+      if (!(user.role === 'Admin' || user.role === 'System')) {
+        return false
+      }
+      await saveConfig(cfg)
+      return true
+    },
+)
+
+QMModule(module)
