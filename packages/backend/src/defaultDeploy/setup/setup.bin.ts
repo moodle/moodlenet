bf2c2da647e0864da93d75bf89f07d17d6e98bbe
aleclofabbro <alessandro.giansanti@gmail.com>
2021-07-07 10:20:54 +0200

    default deploy will setup and|or upgrade dbs on startup

--- packages/backend/src/defaultDeploy/setup/setup.bin.ts
+++ packages/backend/src/defaultDeploy/setup/setup.bin.ts
@@ -2,5 +2,5 @@ import env from '../env'
 import { setupDb } from './db'
 const forceDrop = process.env.FORCE_DROP_DBS === 'true'
 ;(async () => {
-  await setupDb({ env: env.db, forceDrop })
+  await setupDb({ env: env.db, actionOnDBExists: forceDrop ? 'drop' : 'abort' })
 })()
