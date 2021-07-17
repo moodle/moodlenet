73a7b68ebd0b1159bf3ee3e0bf19d4bf558b472d
aleclofabbro <alessandro.giansanti@gmail.com>
2021-07-17 18:16:44 +0200

    1

--- packages/webapp/src/ui/pages/Profile/Profile.stories.tsx
+++ packages/webapp/src/ui/pages/Profile/Profile.stories.tsx
@@ -1,13 +1,16 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useEffect, useReducer } from 'react'
 import { CollectionCardStoryProps } from '../../components/cards/CollectionCard/CollectionCard.stories'
 import { OverallCardStoryProps } from '../../components/cards/OverallCard/OverallCard.stories'
 import { ProfileCardStoryProps } from '../../components/cards/ProfileCard/ProfileCard.stories'
+import { ResourceCardProps } from '../../components/cards/ResourceCard/ResourceCard'
 import { ResourceCardStoryProps } from '../../components/cards/ResourceCard/ResourceCard.stories'
 import { ScoreCardStoryProps } from '../../components/cards/ScoreCard/ScoreCard.stories'
 import { HeaderProps } from '../../components/Header/Header'
 import { HeaderStoryProps } from '../../components/Header/Header.stories'
 import { SubHeaderStoryProps } from '../../components/SubHeader/SubHeader.stories'
 import { withPropsListStatic, withPropsStatic } from '../../lib/ctrl'
+import { withPropsFor } from '../../lib/__/ctrl'
 import { HeaderPageTemplateProps } from '../../templates/page/HeaderPageTemplate'
 import { HeaderPageProps } from '../HeaderPage/HeaderPage'
 import { HeaderPageStoryProps } from '../HeaderPage/HeaderPage.stories'
@@ -34,7 +37,24 @@ export const ProfileStoryProps: ProfileProps = {
   profileCardProps: ProfileCardStoryProps,
   scoreCardProps: ScoreCardStoryProps,
   collectionCardWithPropsList: withPropsListStatic([CollectionCardStoryProps, CollectionCardStoryProps]),
-  resourceCardWithPropsList: withPropsListStatic([ResourceCardStoryProps]),
+  // resourceCardWithPropsList: [ResourceCardStoryProps, ResourceCardStoryProps, ResourceCardStoryProps],
+  resourceCardWithPropsList: [ResourceCardStoryProps, ResourceCardStoryProps, ResourceCardStoryProps].map((p, i) => {
+    return withPropsFor<ResourceCardProps>({
+      useCtrlHook: () => {
+        const [n, inc] = useReducer(n => n + 1, 1)
+        useEffect(() => {
+          const i = setInterval(inc, 1000)
+          return () => {
+            clearInterval(i)
+          }
+        }, [])
+        const color = ['red', 'green', 'blue'][n % 3]
+        return [p, { wrappers: [[({ children }) => <div style={{ color }}>{children}</div>, {}]] }]
+      },
+      hookArg: null,
+      key: i,
+    })
+  }),
   username: 'Juanito',
 }
 
