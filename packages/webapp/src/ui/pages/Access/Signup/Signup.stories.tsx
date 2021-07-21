9ed342981f00376df868fae5cfc52e31ca9ce043
bru.mas <brumasribera@gmail.com>
2021-07-21 18:32:30 +0200

    Styling touches

--- packages/webapp/src/ui/pages/Access/Signup/Signup.stories.tsx
+++ packages/webapp/src/ui/pages/Access/Signup/Signup.stories.tsx
@@ -6,19 +6,29 @@ import { Signup, SignupProps } from './Signup'
 const meta: ComponentMeta<typeof Signup> = {
   title: 'Pages/Signup',
   component: Signup,
-  excludeStories: ['LoginStoryProps'],
+  excludeStories: ['SignupStoryProps', 'EmailSendStoryProps'],
 }
 
 const SignupStory: ComponentStory<typeof Signup> = args => <Signup {...args} />
 
-export const LoginStoryProps: SignupProps = {
+export const SignupStoryProps: SignupProps = {
   accessHeaderProps: AccessHeaderStoryProps,
   onSubmit: action('Submit signup'),
   loginErrorMessage: null,
+  requestSent: false
 }
 
-export const SignupPage = SignupStory.bind({})
-SignupPage.args = LoginStoryProps
-SignupPage.parameters = { layout: 'fullscreen' }
+export const EmailSendStoryProps: SignupProps = {
+  ...SignupStoryProps,
+  requestSent: true
+}
+
+export const SignUpPage = SignupStory.bind({})
+SignUpPage.args = SignupStoryProps
+SignUpPage.parameters = { layout: 'fullscreen' }
+
+export const EmailSent = SignupStory.bind({})
+EmailSent.args = EmailSendStoryProps
+EmailSent.parameters = { layout: 'fullscreen' }
 
 export default meta
