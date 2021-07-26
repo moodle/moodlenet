815dc023e0a736ab9abfb1eab8595a1d93e7e69e
bru.mas <brumasribera@gmail.com>
2021-07-26 10:11:51 +0200

    Remove subheader from upload pages

--- packages/webapp/src/ui/pages/NewResource/UploadResource/UploadResource.tsx
+++ packages/webapp/src/ui/pages/NewResource/UploadResource/UploadResource.tsx
@@ -1,42 +1,46 @@
-import verifiedIcon from '../../../assets/icons/verified.svg'
-import SecondaryButton from '../../../components/atoms/SecondaryButton/SecondaryButton'
+import { Trans } from '@lingui/macro'
+import Card from '../../../components/atoms/Card/Card'
 import { withCtrl } from '../../../lib/ctrl'
+import uploadFileIcon from '../../../static/icons/upload-file.svg'
 import './styles.scss'
 
+type UploadResourceState = 'Initial' | 'ContentUploaded' | 'ImageUploaded' | 'AllSet' | 'AllSetImage'
+type ContentType = 'File' | 'Link' | ''
+
 export type UploadResourceProps = {
-  backgroundUrl: string
-  username: string
-  avatarUrl: string
-  firstName: string
-  lastName: string
-  organizationName: string
-  location: string
-  siteUrl: string
-  description: string
+  state: UploadResourceState
+  type?: ContentType
+  imageUrl?: string
 }
 
-export const UploadResource = withCtrl<UploadResourceProps>(
-  ({ avatarUrl, username, backgroundUrl, description, firstName, lastName, location, organizationName, siteUrl }) => {
+export const UploadResource = withCtrl<UploadResourceProps>(({
+  state,
+  type,
+  imageUrl
+}) => {
     return (
       <div className="upload-resource">
-        <img className="background" src={backgroundUrl} alt="Background" />
-        <img className="avatar" src={avatarUrl} alt="Avatar" />
-        <div className="info">
-          <div className="title">
-            {firstName} {lastName}
-            <img className="verified-icon" src={verifiedIcon} alt="Verified" />
-          </div>
-          <div className="subtitle">
-            @{username}&nbsp; · &nbsp;
-            {organizationName} MoodleNet&nbsp; | &nbsp;
-            {location}&nbsp; · &nbsp;
-            <span style={{ cursor: 'pointer' }}>{siteUrl}</span>
-          </div>
-          <div className="presentation">{description}</div>
-          <div className="buttons">
-            <SecondaryButton>Edit UploadResource</SecondaryButton>
-            <SecondaryButton>Go to Setting</SecondaryButton>
-          </div>
+        <div className="main-column">
+          <Card>
+            <div className='main-container'>
+              <div className="uploader">
+                <div className="file upload" hidden={state !== 'Initial'}>
+                  <img src={uploadFileIcon} />
+                  <span><Trans>Drop a file here or click to upload!</Trans></span>
+                </div>
+                <div className="image upload" hidden={state !== 'ContentUploaded'}></div>
+              </div>
+              {state === 'ImageUploaded' ? <img className="image-uploaded" src={imageUrl} alt="Background" />: <></>}
+            </div>
+            <div className="bottom-container">
+              <div className="link" hidden={state !== 'Initial'}></div>
+              <div className="uploaded-file" hidden={state!== 'ContentUploaded' || type !== 'File'}></div>
+              <div className="uploaded-link" hidden={state!== 'ContentUploaded' || type !== 'Link'}></div>
+            </div>
+          </Card>
+        </div>
+        <div className="side-column">
+
         </div>
       </div>
     )
