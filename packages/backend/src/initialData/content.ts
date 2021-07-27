91411d77ffcfea76efb831ad752f92504af687f7
aleclofabbro <alessandro.giansanti@gmail.com>
2021-07-27 18:44:22 +0200

    small initial populate fixes

--- packages/backend/src/initialData/content.ts
+++ packages/backend/src/initialData/content.ts
@@ -1,8 +1,10 @@
 import { Organization } from '@moodlenet/common/lib/content-graph/types/node'
+import { newGlyphPermId } from '@moodlenet/common/lib/utils/content-graph/slug-id'
 import { nanoid } from 'nanoid'
 
+const localOrgPermId = newGlyphPermId()
 export const localOrganizationData = ({ domain }: { domain: string }): Organization => ({
-  _permId: '__local__',
+  _permId: localOrgPermId,
   name: 'MoodleNet',
   // shortIntro: 'Our global network to share and curate open educational resources',
   intro: `Join our social network to share and curate open educational resources with educators world-wide.
@@ -14,16 +16,18 @@ Build your profile as an educator.`,
     date: Number(new Date()),
     status: 'Active',
   },
-  _slug: 'moodle-net-central',
+  _slug: '--local--',
   _type: 'Organization',
   logo: null,
 })
 
-const rootAuthId = nanoid(16)
+const rootAuthId = nanoid(18)
+const rootPermId = newGlyphPermId()
 
 export const getRootUser = ({ domain }: { domain: string }) => ({
   clearPassword: `root`,
   slug: `__root__`,
+  rootPermId,
   rootAuthId,
   email: `root@${domain}`,
 })
