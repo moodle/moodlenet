8dac6559dbb58339e5ca72b9e43f58f1ccf6a765
bru.mas <bru.mas@moodle.com>
2021-08-05 13:53:50 +0200

    Add Legal dropdown

--- packages/webapp/src/ui/pages/NewResource/FieldsData.tsx
+++ packages/webapp/src/ui/pages/NewResource/FieldsData.tsx
@@ -1,12 +1,8 @@
 import { t } from "@lingui/macro"
 import { DropdownOptionsType } from "../../components/atoms/Dropdown/Dropdown"
 import byIcon from '../../static/icons/license/by.svg'
-import ccIcon from '../../static/icons/license/cc.svg'
-import ncEuIcon from '../../static/icons/license/nc-eu.svg'
-import ncJpIcon from '../../static/icons/license/nc-jp.svg'
 import ncIcon from '../../static/icons/license/nc.svg'
 import ndIcon from '../../static/icons/license/nd.svg'
-import pdIcon from '../../static/icons/license/pd.svg'
 import saIcon from '../../static/icons/license/sa.svg'
 import zeroIcon from '../../static/icons/license/zero.svg'
 
@@ -49,38 +45,22 @@ export const CategoryDropdown: Field  =  {
 
 const by = <img src={byIcon} alt="BY"/>
 const zero = <img src={zeroIcon} alt="CCO"/>
-const cc = <img src={ccIcon} alt="BY"/>
 const nc = <img src={ncIcon} alt="CCO"/>
-const ncEu = <img src={ncEuIcon} alt="BY"/>
-const ncJp = <img src={ncJpIcon} alt="CCO"/>
 const nd = <img src={ndIcon} alt="BY"/>
-const pd = <img src={pdIcon} alt="BY"/>
 const sa = <img src={saIcon} alt="CCO"/>
 
 export const LicenseDropdown: Field  =  {
     label: t`Just a text field`,
     options: [
-        ['CCO', <div>{zero}</div>],
-        ['CC-BY', <div>{by}{zero}</div>],
-        ['CC-BY', <div>{by}{cc}</div>],
-        ['CC-BY', <div>{by}{nc}</div>],
-        ['CC-BY', <div>{by}{ncEu}</div>],
-        ['CC-BY', <div>{by}{ncJp}</div>],
-        ['CC-BY', <div>{by}{nd}</div>],
-        ['CC-BY', <div>{by}{pd}</div>],
-        ['CC-BY', <div>{by}{sa}</div>],
+        ['CCO (Public domain)', <div>{zero}</div>],
+        ['CC-BY (Attribution)', <div>{zero}{by}</div>],
+        ['CC-BY-SA (Attribution-ShareAlike)', <div>{zero}{by}{sa}</div>],
+        ['CC-BY-NC (Attribution-NonCommercial)', <div>{zero}{by}{nc}</div>],
+        ['CC-BY-NC-SA (Attribution-NonCommercial-ShareAlike)', <div>{zero}{by}{nc}{sa}</div>],
+        ['CC-BY-ND (Attribution-NonCommercial)', <div>{zero}{by}{nd}</div>],
+        ['CC-BY-NC-ND (Attribution-NonCommercial-NoDerivatives)', <div>{zero}{by}{nc}{nd}</div>],
     ]
 }
-
-/*
-CC0 (Public domain)
-CC-BY (Attribution)
-CC-BY-SA (Attribution-ShareAlike)
-CC-BY-NC (Attribution-NonCommercial)
-CC-BY-NC-SA (Attribution-NonCommercial-ShareAlike)
-CC-BY-ND (Attribution-NonCommercial)
-CC-BY-NC-ND (Attribution-NonCommercial-NoDerivatives)
-*/
 
 
 
