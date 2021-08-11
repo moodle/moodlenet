8ae82ffb224dae0f31d781647a8636dee6b6a485
bru.mas <bru.mas@moodle.com>
2021-08-11 21:11:07 +0200

    Create Collection Page

---
+++ packages/webapp/src/ui/pages/NewCollection/CreateCollection/CreateCollection.tsx
@@ -0,0 +1,123 @@
+import { Trans } from '@lingui/macro'
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded'
+import React, { useCallback } from 'react'
+import Card from '../../../components/atoms/Card/Card'
+import Dropdown from '../../../components/atoms/Dropdown/Dropdown'
+import InputTextField from '../../../components/atoms/InputTextField/InputTextField'
+import PrimaryButton from '../../../components/atoms/PrimaryButton/PrimaryButton'
+import { withCtrl } from '../../../lib/ctrl'
+import { FormikBag } from '../../../lib/formik'
+import { ReactComponent as UploadImageIcon } from '../../../static/icons/upload-image.svg'
+import { DropdownField } from '../FieldsData'
+import { NewCollectionFormValues } from '../types'
+import './styles.scss'
+
+export type CreateCollectionProps = {
+  step: 'CreateCollectionStep'
+  formBag: FormikBag<NewCollectionFormValues>
+  imageUrl: string
+  categories: DropdownField
+  nextStep: (() => unknown) | undefined
+}
+
+export const CreateCollection = withCtrl<CreateCollectionProps>(
+  ({ formBag, imageUrl, categories, nextStep }) => {
+    const [form] = formBag
+    const setFieldValue = form.setFieldValue
+    const background = {
+      backgroundImage: 'url(' + imageUrl + ')',
+      backgroundSize: 'cover',
+    }
+
+    const uploadImage = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.currentTarget.files?.item(0)
+        setFieldValue('image', selectedFile ?? null)
+      },
+      [setFieldValue],
+    )
+
+    const deleteImage = useCallback(() => {
+      setFieldValue('image', null)
+    }, [setFieldValue])
+
+    const dd = useCallback(
+      (v: string) => {
+        setFieldValue('category', v)
+      },
+      [setFieldValue],
+    )
+    const dataInputs = (
+      <div>
+        <InputTextField
+          autoUpdate={true}
+          label="Title"
+          placeholder=""
+          getText={text => form.setFieldValue('title', text)}
+          value={form.values.title}
+        />
+        <InputTextField
+          autoUpdate={true}
+          textarea={true}
+          label="Description"
+          placeholder=""
+          value={form.values.description}
+          getText={text => form.setFieldValue('description', text)}
+        />
+        <Dropdown {...categories} getValue={dd} />
+      </div>
+    )
+
+    const selectImage = () => {
+      //FIXME: useRef()s
+      document.getElementById('uploadImage')?.click()
+    }
+
+    return (
+      <div className="upload-resource">
+        <div className="content">
+          <div className="main-column">
+            <div className="card-title">
+              <Trans>Cover Image</Trans>
+            </div>
+            <Card>
+              <div className="main-container">
+                {!imageUrl ? (
+                  <div className="uploader">
+                    <div className="image upload" onClick={selectImage}>
+                      <input
+                        id="uploadImage"
+                        type="file"
+                        accept=".jpg,.jpeg,.png,.gif"
+                        name="myImage"
+                        onChange={uploadImage}
+                        hidden
+                      />
+                      <UploadImageIcon />
+                      <span>
+                        <Trans>Click to upload an image!</Trans>
+                      </span>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="image-container" style={background}>
+                    <div className="delete-image" onClick={deleteImage}>
+                      <CloseRoundedIcon />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Card>
+            <div className="small-screen-details">{dataInputs}</div>
+          </div>
+          <div className="side-column">{dataInputs}</div>
+        </div>
+        <div className="footer">
+          <PrimaryButton disabled={!nextStep} onClick={nextStep}>
+            <Trans>Finish</Trans>
+          </PrimaryButton>
+        </div>
+      </div>
+    )
+  },
+)
---
+++ packages/webapp/src/ui/pages/NewCollection/Ctrl/NewResourceCtrl.tsx
@@ -0,0 +1,309 @@
+import { AssetRefInput } from '@moodlenet/common/lib/graphql/types.graphql.gen'
+import { DistOmit } from '@moodlenet/common/lib/utils/types'
+import { nodeId2UrlPath } from '@moodlenet/common/lib/webapp/sitemap/helpers'
+import { Reducer, useCallback, useEffect, useMemo, useReducer, useState } from 'react'
+import { useHistory } from 'react-router'
+import { useSession } from '../../../../context/Global/Session'
+import { useUploadTempFile } from '../../../../helpers/data'
+import {
+  categoriesOptions,
+  getGrade,
+  getIscedF,
+  getLang,
+  getLicense,
+  getOriginalCreationTimestampByStrings,
+  getType,
+  langOptions,
+  licensesOptions,
+  monthOptions,
+  resGradeOptions,
+  resTypeOptions,
+  yearsOptions
+} from '../../../../helpers/resource-relation-data-static-and-utils'
+import { ctrlHook, CtrlHook } from '../../../lib/ctrl'
+import { useFormikBag } from '../../../lib/formik'
+import { useHeaderPageTemplateCtrl } from '../../../templates/page/HeaderPageTemplateCtrl/HeaderPageTemplateCtrl'
+import { UploadResourceProps } from '../CreateCollection/CreateCollection'
+import { NewCollectionProps } from '../NewCollection'
+import { NewCollectionFormValues } from '../types'
+import {
+  useCreateResourceMutation,
+  useCreateResourceRelationMutation,
+  useNewCollectionDataPageLazyQuery
+} from './NewCollectionCtrl.gen'
+
+const initialSetStepProps: DistOmit<UploadResourceProps, 'formBag' | 'deleteContent' | 'nextStep'> = {
+  step: 'UploadResourceStep',
+  state: 'ChooseResource',
+  imageUrl: '',
+  categories: categoriesOptions,
+  licenses: licensesOptions,
+}
+
+export type NewCollectionCtrlProps = {}
+
+export const useNewCollectionCtrl: CtrlHook<NewCollectionProps, NewCollectionCtrlProps> = () => {
+  const { session } = useSession()
+  const { push } = useHistory()
+  const myId = session?.profile.id
+  const [loadMyColl, mycollectionsQRes] = useNewCollectionDataPageLazyQuery()
+  const uploadTempFile = useUploadTempFile()
+  const [createResourceMut /* , createResourceMutRes */] = useCreateResourceMutation()
+  const [createResourceRelMut /* , createResourceRelMutRes */] = useCreateResourceRelationMutation()
+
+  const mycollections = useMemo(() => mycollectionsQRes.data?.node?.myCollections.edges.map(_ => _.node) ?? [], [
+    mycollectionsQRes.data?.node?.myCollections,
+  ])
+
+  useEffect(() => {
+    if (myId) {
+      loadMyColl({ variables: { myId } })
+    }
+  }, [myId, loadMyColl])
+  const [form, formBag] = useFormikBag<NewCollectionFormValues>({
+    initialValues: {
+      addToCollections: [],
+      category: '',
+      content: '',
+      description: '',
+      format: null,
+      image: null,
+      language: null,
+      level: null,
+      license: null,
+      name: '',
+      originalDateMonth: null,
+      originalDateYear: null,
+      title: '',
+      contentType: 'Link',
+      type: null,
+    },
+    onSubmit: console.log.bind(console, 'submit NewCollection'),
+  })
+
+  const [sform] = formBag
+  const sformSetField = sform.setFieldValue
+
+  const deleteContent = useCallback(() => {
+    sformSetField('image', '')
+    sformSetField('content', '')
+  }, [sformSetField])
+
+  type StepProps = DistOmit<NewCollectionProps['stepProps'], 'nextStep'>
+  type StepPropsHistoryItem = [curr: StepProps, prev: StepPropsHistoryItem | null]
+  const [[stepProps, prevStepProps], setNextStepProps] = useReducer<Reducer<StepPropsHistoryItem, StepProps | 'back'>>(
+    ([curr, prev], next) => {
+      if (next === 'back') {
+        return prev ?? [curr, prev]
+      }
+      return [next, [curr, prev]]
+    },
+    [
+      {
+        ...initialSetStepProps,
+        formBag,
+        deleteContent,
+      },
+      null,
+    ],
+  )
+  const { content, name, image } = sform.values
+
+  const [imageUrl, setImageUrl] = useState('')
+  useEffect(() => {
+    const imageObjectUrl = image instanceof File ? URL.createObjectURL(image) : ''
+    setImageUrl(imageObjectUrl)
+    return () => URL.revokeObjectURL(imageObjectUrl)
+  }, [image, setImageUrl])
+
+  useEffect(() => {
+    if (!sform.values.content && prevStepProps) {
+      setNextStepProps('back')
+    }
+  }, [sform.values.content, prevStepProps])
+
+  const previousStep = useCallback(() => setNextStepProps('back'), [setNextStepProps])
+
+  const nextStep = useMemo(() => {
+    // console.log('nextStep', { deleteContent, formBag, previousStep, sform, stepProps, formValues: form.values })
+    if (stepProps.step === 'UploadResourceStep') {
+      if (stepProps.state === 'ChooseResource') {
+        if (form.values.content) {
+          return () => {
+            if (form.values.content instanceof File && form.values.content.type.toLowerCase().startsWith('image')) {
+              sformSetField('image', form.values.content)
+            }
+            setNextStepProps({
+              ...initialSetStepProps,
+              step: 'UploadResourceStep',
+              state: 'EditData',
+              deleteContent,
+              formBag,
+              imageUrl,
+            })
+          }
+          //   categories,
+        }
+      } else if (stepProps.state === 'EditData') {
+        if (form.values.title && form.values.description && form.values.category && form.values.license) {
+          return () => {
+            setNextStepProps({
+              step: 'AddToCollectionsStep',
+              collections: mycollections.map(_ => _.name),
+              previousStep,
+            })
+          }
+        }
+      }
+    } else if (stepProps.step === 'AddToCollectionsStep') {
+      return () => {
+        setNextStepProps({
+          step: 'ExtraDetailsStep',
+          // formats: resFormatOptions,
+          languages: langOptions,
+          levels: resGradeOptions,
+          types: resTypeOptions,
+          months: monthOptions,
+          years: yearsOptions,
+          previousStep,
+          formBag,
+        })
+      }
+    } else if (stepProps.step === 'ExtraDetailsStep') {
+      return async () => {
+        const {
+          content,
+          category,
+          image,
+          title,
+          description,
+          level,
+          language,
+          license,
+          type,
+          addToCollections,
+          originalDateMonth,
+          originalDateYear,
+        } = form.values
+        if (!content) {
+          return previousStep()
+        }
+        const contentAssetRef: AssetRefInput =
+          typeof content === 'string'
+            ? {
+                location: content,
+                type: 'ExternalUrl',
+              }
+            : {
+                location: await uploadTempFile('resource', content),
+                type: 'TmpUpload',
+              }
+
+        const imageAssetRef: AssetRefInput = !image
+          ? { location: '', type: 'NoAsset' }
+          : typeof image === 'string'
+          ? {
+              location: image,
+              type: 'ExternalUrl',
+            }
+          : {
+              location: await uploadTempFile('image', image),
+              type: 'TmpUpload',
+            }
+
+        const resourceCreationResp = await createResourceMut({
+          variables: {
+            res: {
+              nodeType: 'Resource',
+              Resource: {
+                content: contentAssetRef,
+                description,
+                name: title,
+                image: imageAssetRef,
+                originalCreationDate: getOriginalCreationTimestampByStrings({ originalDateMonth, originalDateYear }),
+              },
+            },
+          },
+        })
+        const resource = resourceCreationResp.data?.resource
+        if (resource?.__typename === 'CreateNodeMutationSuccess' && resource.node.__typename === 'Resource') {
+          const resId = resource.node.id
+
+          const { langId } = getLang(language)
+          const createLangRelPr = createResourceRelMut({
+            variables: { edge: { edgeType: 'Features', from: resId, to: langId, Features: {} } },
+          })
+
+          const { licenseId } = getLicense(license)
+          const createReLicenRelPr = createResourceRelMut({
+            variables: { edge: { edgeType: 'Features', from: resId, to: licenseId, Features: {} } },
+          })
+
+          const { typeId } = getType(type)
+          const createTypeRelPr = createResourceRelMut({
+            variables: { edge: { edgeType: 'Features', from: resId, to: typeId, Features: {} } },
+          })
+
+          const { gradeId } = getGrade(level)
+          const createGradeRelPr = createResourceRelMut({
+            variables: { edge: { edgeType: 'Features', from: resId, to: gradeId, Features: {} } },
+          })
+
+          const { iscedFId } = getIscedF(category)
+          const createRIscedRelPr = createResourceRelMut({
+            variables: { edge: { edgeType: 'Features', from: resId, to: iscedFId, Features: {} } },
+          })
+
+          await Promise.all(
+            addToCollections
+              .map(async collName => {
+                const collectionId = mycollections.find(_ => _.name === collName)!.id
+                return createResourceRelMut({
+                  variables: { edge: { edgeType: 'Features', to: resId, from: collectionId, Features: {} } },
+                })
+              })
+              .concat([createLangRelPr, createReLicenRelPr, createTypeRelPr, createGradeRelPr, createRIscedRelPr]),
+          )
+          push(nodeId2UrlPath(resId))
+        }
+      }
+    }
+    return undefined
+  }, [
+    push,
+    createResourceRelMut,
+    deleteContent,
+    createResourceMut,
+    uploadTempFile,
+    formBag,
+    previousStep,
+    sformSetField,
+    stepProps,
+    form.values,
+    mycollections,
+    imageUrl,
+  ])
+
+  useEffect(() => {
+    if (content) {
+      if (content instanceof File) {
+        return name !== content.name ? sformSetField('name', content.name) : null
+      } else {
+        return name !== content ? sformSetField('name', content) : null
+      }
+    } else {
+      return sformSetField('name', '')
+    }
+  }, [content, sformSetField, name, image])
+
+  const NewCollectionProps = useMemo<NewCollectionProps>(() => {
+    return {
+      headerPageTemplateProps: ctrlHook(useHeaderPageTemplateCtrl, {}),
+      stepProps: { ...stepProps, nextStep, imageUrl }, //FIXME: stepProps are created in `nextStep()`, so they're static
+    }
+  }, [nextStep, stepProps, imageUrl])
+
+  console.log({ vals: form.values, step: NewCollectionProps.stepProps })
+
+  return NewCollectionProps && [NewCollectionProps]
+}
