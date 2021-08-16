bcfc5594dbfe0be112815eac1d5c90a41c9dc6f6
aleclofabbro <alessandro.giansanti@gmail.com>
2021-08-16 17:26:55 +0200

    added a rough Terms and conditions page linked from signup page button learn more

--- packages/webapp/src/ui/pages/Access/AccessHeader/AccessHeader.tsx
+++ packages/webapp/src/ui/pages/Access/AccessHeader/AccessHeader.tsx
@@ -1,7 +1,7 @@
 import PrimaryButton from '../../../components/atoms/PrimaryButton/PrimaryButton'
 import SecondaryButton from '../../../components/atoms/SecondaryButton/SecondaryButton'
 import HeaderTitle from '../../../components/Header/HeaderTitle/HeaderTitle'
-import { Href } from '../../../elements/link'
+import { Href, Link } from '../../../elements/link'
 import { withCtrl } from '../../../lib/ctrl'
 import { Organization } from '../../../types'
 import './styles.scss'
@@ -10,28 +10,35 @@ export type AccessHeaderProps = {
   organization: Pick<Organization, 'logo' | 'name' | 'url'>
   homeHref: Href
   page: 'login' | 'signup' | 'activation'
+  termsAndConditionsHref: Href
 }
 
-export const AccessHeader = withCtrl<AccessHeaderProps, 'page'>(({ organization, homeHref, page }) => {
-  return (
-    <div className="access-header">
-      <div className="content">
-        <HeaderTitle organization={organization} homeHref={homeHref} />
-        {page !== 'activation' ? (
-          <div className="buttons">
-            {page === 'login' ? (
-              <SecondaryButton color="orange">Sign up</SecondaryButton>
-            ) : (
-              <SecondaryButton color="orange">Login</SecondaryButton>
-            )}
-            <PrimaryButton>Learn more</PrimaryButton>
-          </div>
-        ) : (
-          <></>
-        )}
+export const AccessHeader = withCtrl<AccessHeaderProps, 'page'>(
+  ({ organization, homeHref, page, termsAndConditionsHref }) => {
+    return (
+      <div className="access-header">
+        <div className="content">
+          <HeaderTitle organization={organization} homeHref={homeHref} />
+          {page !== 'activation' ? (
+            <div className="buttons">
+              {page === 'login' ? (
+                <SecondaryButton color="orange">Sign up</SecondaryButton>
+              ) : (
+                <SecondaryButton color="orange">Login</SecondaryButton>
+              )}
+              <PrimaryButton>
+                <Link href={termsAndConditionsHref} target="__blank">
+                  Learn more
+                </Link>
+              </PrimaryButton>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
-    </div>
-  )
-})
+    )
+  },
+)
 AccessHeader.displayName = 'AccessHeader'
 export default AccessHeader
