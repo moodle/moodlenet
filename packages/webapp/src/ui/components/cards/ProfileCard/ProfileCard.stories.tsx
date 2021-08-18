48e781d31c1ec4dec59d50c31aa04cc726d64e90
bru.mas <bru.mas@moodle.com>
2021-08-18 17:43:31 +0200

    Send and email

--- packages/webapp/src/ui/components/cards/ProfileCard/ProfileCard.stories.tsx
+++ packages/webapp/src/ui/components/cards/ProfileCard/ProfileCard.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { ProfileCard, ProfileCardProps } from './ProfileCard'
 
@@ -7,7 +8,7 @@ const meta: ComponentMeta<typeof ProfileCard> = {
   argTypes: {
     // backgroundColor: { control: 'color' },
   },
-  excludeStories: ['ProfileCardStoryProps'],
+  excludeStories: ['ProfileCardStoryProps', 'ProfileCardLoggedOutStoryProps', 'ProfileCardLoggedInStoryProps', 'ProfileCardOwnerStoryProps'],
   decorators:[
     (Story)=>(<div style={{maxWidth:500}}><Story/></div>)
   ]
@@ -22,12 +23,36 @@ export const ProfileCardStoryProps: ProfileCardProps = {
   organizationName: 'UM',
   location: 'Malta',
   siteUrl: 'https://iuri.is/',
+  email: 'info@moodle.com',
+  isOwner: false,
+  isAuthenticated: false,
+  toggleFollow: action('toogleFollow'),
   description: 'Italian biologist specialized in endangered rainforest monitoring. Cooperating with local organizations to improve nature reserves politics.'
+}
+
+export const ProfileCardLoggedOutStoryProps: ProfileCardProps = {
+  ...ProfileCardStoryProps,
+}
+
+export const ProfileCardLoggedInStoryProps: ProfileCardProps = {
+  ...ProfileCardStoryProps,
+  isAuthenticated: true
+}
+
+export const ProfileCardOwnerStoryProps: ProfileCardProps = {
+  ...ProfileCardLoggedInStoryProps,
+  isOwner: true
 }
 
 const ProfileCardStory: ComponentStory<typeof ProfileCard> = args => <ProfileCard {...args} />
 
-export const Default = ProfileCardStory.bind({})
-Default.args = ProfileCardStoryProps
+export const LoggedOut = ProfileCardStory.bind({})
+LoggedOut.args = ProfileCardLoggedOutStoryProps
+
+export const LoggedIn = ProfileCardStory.bind({})
+LoggedIn.args = ProfileCardLoggedInStoryProps
+
+export const Owner = ProfileCardStory.bind({})
+Owner.args = ProfileCardOwnerStoryProps
 
 export default meta
