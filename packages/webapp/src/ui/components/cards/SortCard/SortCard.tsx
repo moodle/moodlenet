59c037bbad41e74fc4cac1d9c9e1c7475defdb42
bru.mas <brumasribera@gmail.com>
2021-08-26 22:33:17 +0200

    Display filters on small screens

--- packages/webapp/src/ui/components/cards/SortCard/SortCard.tsx
+++ packages/webapp/src/ui/components/cards/SortCard/SortCard.tsx
@@ -2,14 +2,18 @@ import { FC, useCallback, useState } from 'react'
 import SortButton, { SortState } from './SortButton/SortButton'
 import './styles.scss'
 
+export type SortCardDirection = 'horizontal' | 'vertical'
+
 export type SortCardProps = {
   className: string
   title: string
   content: [name: string, label: string, state: SortState][]
+  direction?: SortCardDirection
   onChange(sortby: string, state: SortState): unknown
 }
 
-export const SortCard: FC<SortCardProps> = ({ className, content, title, onChange }) => {
+export const SortCard: FC<SortCardProps> = ({ className, direction, content, title, onChange }) => {
+  className += ' ' + direction
   const [currentSort, setCurrentSort] = useState<{ name: string; state: SortState }>({
     name: content[1] ? content[1][0] : 'none',
     state: 'inactive',
@@ -39,6 +43,10 @@ export const SortCard: FC<SortCardProps> = ({ className, content, title, onChang
       <div className="content">{inContent}</div>
     </div>
   )
+}
+
+SortCard.defaultProps = {
+  direction: 'vertical'
 }
 
 export default SortCard
