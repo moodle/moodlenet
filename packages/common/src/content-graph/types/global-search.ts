3aae3bb155005c9acbaf7c5c2b7e42eddddac4a7
aleclofabbro <alessandro.giansanti@gmail.com>
2021-08-27 17:04:47 +0200

    fixed minor regression in search filter

--- packages/common/src/content-graph/types/global-search.ts
+++ packages/common/src/content-graph/types/global-search.ts
@@ -3,10 +3,11 @@ import { Maybe } from '../../utils/types'
 
 export const globalSearchSortBy = ['Popularity', 'Relevance', 'Recent'] as const
 export type GlobalSearchSortBy = typeof globalSearchSortBy[number]
+export const isGlobalSearchSortBy = isOneOf(globalSearchSortBy)
 export type GlobalSearchSort = { by: GlobalSearchSortBy; asc?: Maybe<boolean> }
 
 export const globalSearchNodeTypes = ['Collection', 'Resource', 'IscedField'] as const
 
 export type GlobalSearchNodeType = 'Collection' | 'Resource' | 'IscedField'
 
-export const isGlobalSearchNodeType = isOneOf(globalSearchSortBy)
+export const isGlobalSearchNodeType = isOneOf(globalSearchNodeTypes)
