9818beed0c7402e6c4ad4dfa52d0a20bd00e6408
bru.mas <brumasribera@gmail.com>
2021-09-07 12:14:35 +0200

    Add what-input lib to avoid outline on clicked inputs, maily see on Safari

--- packages/webapp/src/ui/templates/page/MainPageWrapper.tsx
+++ packages/webapp/src/ui/templates/page/MainPageWrapper.tsx
@@ -1,13 +1,16 @@
-import { FC } from 'react'
-import '../../styles/main.scss'
-import '../../styles/view.scss'
+import { FC } from 'react';
+import useWhatInput from 'react-use-what-input';
+import '../../styles/main.scss';
+import '../../styles/view.scss';
 
 export type MainPageWrapperProps = {
   onKeyDown?(arg0: unknown): unknown
 }
 export const MainPageWrapper: FC<MainPageWrapperProps> = ({ children, onKeyDown }) => {
+  const [currentInput, currentIntent] = useWhatInput();
+
   return (
-    <div className="main-page-wrapper" onKeyDown={onKeyDown}>
+    <div className={`main-page-wrapper current-input-${currentInput} current-intent-${currentIntent}`} onKeyDown={onKeyDown}>
       {children}
     </div>
   )
