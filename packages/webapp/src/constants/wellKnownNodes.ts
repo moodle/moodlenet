fc9de87b75b637efd787a0693bbd86d36420938b
aleclofabbro <alessandro.giansanti@gmail.com>
2021-09-13 15:58:54 +0200

    isced fields filtered by Anna's review, brutally hardcoded into webapp code

--- packages/webapp/src/constants/wellKnownNodes.ts
+++ packages/webapp/src/constants/wellKnownNodes.ts
@@ -1,11 +1,11 @@
 import { getFileFormats } from '@moodlenet/common/lib/content-graph/initialData/file-format/fileFormats'
-import { getIscedFields } from '@moodlenet/common/lib/content-graph/initialData/ISCED/Fields/IscedFields'
+import { getSelectedIscedFields } from '@moodlenet/common/lib/content-graph/initialData/ISCED/Fields/IscedFields'
 import { getIscedGrades } from '@moodlenet/common/lib/content-graph/initialData/ISCED/Grades/IscedGrades'
 import { getIso639_3 } from '@moodlenet/common/lib/content-graph/initialData/ISO_639_3/ISO_639_3'
 import { getLicenses } from '@moodlenet/common/lib/content-graph/initialData/licenses/licenses'
 import { getResourceTypes } from '@moodlenet/common/lib/content-graph/initialData/resource-type/resource-type'
 
-export const iscedFields = getIscedFields()
+export const iscedFields = getSelectedIscedFields()
 export const iscedGrades = getIscedGrades()
 export const iso639_3 = getIso639_3()
 export const licenses = getLicenses()
