a222bf0fe2314a0e1453ef4bd057e5f6f92604b7
aleclofabbro <alessandro.giansanti@gmail.com>
2021-09-20 09:51:19 +0200

    port umbrella function logging resp and catches + args and portId

--- packages/backend/src/lib/qmino/index.ts
+++ packages/backend/src/lib/qmino/index.ts
@@ -1,4 +1,5 @@
 import * as QM from './lib'
+import { QMPortId } from './types'
 
 export const Qmino = (transport: QM.Transport): QM.QMino => {
   const open: QM.QMino['open'] = async (port, adapter, teardown) => {
@@ -12,22 +13,18 @@ export const Qmino = (transport: QM.Transport): QM.QMino => {
     if (actionExtract.type !== 'query') {
       throw new Error(`cannot query an action [${actionExtract.id}] of type ${actionExtract.type}`)
     }
-    return transport.send(actionExtract.id, actionExtract.args, waitsForResponse).catch(e => {
-      console.error(`Rejection on ${actionExtract.id}`)
-      console.error(e instanceof Error ? e.stack : e)
-      throw e
-    }) as Promise<Res>
+    const { args, id } = actionExtract
+    const resp = transport.send(id, args, waitsForResponse)
+    return portLogUmbrella(resp, { args, id })
   }
   const callSync = async <Res>(action: QM.QMAction<any, Res>, waitsForResponse: QM.WaitsForResponse) => {
     const actionExtract = QM.extractAction(transport, action)
     if (actionExtract.type !== 'command') {
       throw new Error(`cannot callSync an action [${actionExtract.id}] of type ${actionExtract.type}`)
     }
-    return transport.send(actionExtract.id, actionExtract.args, waitsForResponse).catch(e => {
-      console.error(`Rejection on ${actionExtract.id}`)
-      console.error(e instanceof Error ? e.stack : e)
-      throw e
-    }) as Promise<Res>
+    const { args, id } = actionExtract
+    const resp = transport.send(id, args, waitsForResponse)
+    return portLogUmbrella(resp, { args, id })
   }
 
   return {
@@ -42,3 +39,19 @@ export const QMCommand = QM.QMCommand
 export const QMQuery = QM.QMQuery
 export const QMModule = QM.QMModule
 export * from './types'
+
+async function portLogUmbrella<T>(p: Promise<T>, { args, id }: { id: QMPortId; args: any[] }): Promise<T> {
+  const logmsg = `
+- QMino port access -
+  portId : ${id.join('::')} 
+  args : ${JSON.stringify(args, null, 2)}
+  `
+  try {
+    const res = await p
+    console.log(`${logmsg}response : ${JSON.stringify(res, null, 2)}}`, res)
+    return res
+  } catch (e) {
+    console.error(`${logmsg}error : ${String(e)} - ${e instanceof Error ? e.stack : ''}`, e)
+    throw e
+  }
+}
