1b192ba48ddb6c41d52263ecb6c2555d0ad90367
aleclofabbro <alessandro.giansanti@gmail.com>
2021-09-20 12:24:32 +0200

    implemented a free mapping for content node home pages url paths

--- packages/webapp/src/routes/CategoryRoute.tsx
+++ packages/webapp/src/routes/CategoryRoute.tsx
@@ -1,11 +1,12 @@
 import { nodeSlugId } from '@moodlenet/common/lib/utils/content-graph/id-key-type-guards'
 import { Routes } from '@moodlenet/common/lib/webapp/sitemap'
+import { getContentNodeHomePageRoutePath } from '@moodlenet/common/lib/webapp/sitemap/helpers'
 import { ctrlHook } from '../ui/lib/ctrl'
 import { Category } from '../ui/pages/Category/Category'
 import { useCategoryCtrl } from '../ui/pages/Category/Ctrl/CategoryPageCtrl'
 import { MNRouteProps, RouteFC } from './lib'
 
-export const CategoryRouteComponent: RouteFC<Routes.CategoryPage> = ({
+export const CategoryRouteComponent: RouteFC<Routes.ContentNodeHomePage> = ({
   match: {
     params: { slug },
   },
@@ -15,8 +16,8 @@ export const CategoryRouteComponent: RouteFC<Routes.CategoryPage> = ({
   return <Category {...props} />
 }
 
-export const CategoryRoute: MNRouteProps<Routes.CategoryPage> = {
+export const CategoryRoute: MNRouteProps<Routes.ContentNodeHomePage> = {
   component: CategoryRouteComponent,
-  path: '/iscedfield/:slug',
+  path: getContentNodeHomePageRoutePath('IscedField'),
   exact: true,
 }
--- packages/webapp/src/routes/ResourceRoute.tsx
+++ packages/webapp/src/routes/ResourceRoute.tsx
@@ -1,11 +1,12 @@
 import { nodeSlugId } from '@moodlenet/common/lib/utils/content-graph/id-key-type-guards'
 import { Routes } from '@moodlenet/common/lib/webapp/sitemap'
+import { getContentNodeHomePageRoutePath } from '@moodlenet/common/lib/webapp/sitemap/helpers'
 import { ctrlHook } from '../ui/lib/ctrl'
 import { useResourceCtrl } from '../ui/pages/Resource/Ctrl/ResourcePageCtrl'
 import { Resource } from '../ui/pages/Resource/Resource'
 import { MNRouteProps, RouteFC } from './lib'
 
-export const ResourceRouteComponent: RouteFC<Routes.ResourcePage> = ({
+export const ResourceRouteComponent: RouteFC<Routes.ContentNodeHomePage> = ({
   match: {
     params: { slug },
   },
@@ -15,8 +16,8 @@ export const ResourceRouteComponent: RouteFC<Routes.ResourcePage> = ({
   return <Resource {...props} />
 }
 
-export const ResourceRoute: MNRouteProps<Routes.ResourcePage> = {
+export const ResourceRoute: MNRouteProps<Routes.ContentNodeHomePage> = {
   component: ResourceRouteComponent,
-  path: '/resource/:slug',
+  path: getContentNodeHomePageRoutePath('Resource'),
   exact: true,
 }
