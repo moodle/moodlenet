4cd0b6db7457cc60d4ca491213f04c0d2077662d
bru.mas <bru.mas@moodle.com>
2021-09-22 12:58:30 +0200

    Add snackbar for policies acceptance

--- packages/webapp/src/ui/templates/page/MainPageWrapper.tsx
+++ packages/webapp/src/ui/templates/page/MainPageWrapper.tsx
@@ -1,18 +1,40 @@
-import { FC } from 'react';
-import useWhatInput from 'react-use-what-input';
-import '../../styles/main.scss';
-import '../../styles/view.scss';
+import { t, Trans } from '@lingui/macro'
+import { FC, useState } from 'react'
+import useWhatInput from 'react-use-what-input'
+import Snackbar from '../../components/atoms/Snackbar/Snackbar'
+import '../../styles/main.scss'
+import '../../styles/view.scss'
 
 export type MainPageWrapperProps = {
+  cookiesAccepted: boolean
   onKeyDown?(arg0: unknown): unknown
 }
-export const MainPageWrapper: FC<MainPageWrapperProps> = ({ children, onKeyDown }) => {
-  const [currentInput, currentIntent] = useWhatInput();
+export const MainPageWrapper: FC<MainPageWrapperProps> = ({ children, cookiesAccepted, onKeyDown }) => {
+  const [currentInput, currentIntent] = useWhatInput()
+  const [isShowingCookierPrompt, setIsShowingCookierPrompt] = useState<boolean>(!cookiesAccepted)
 
   return (
-    <div className={`main-page-wrapper current-input-${currentInput} current-intent-${currentIntent}`} onKeyDown={onKeyDown}>
+    <div
+      className={`main-page-wrapper current-input-${currentInput} current-intent-${currentIntent}`}
+      onKeyDown={onKeyDown}
+    >
+      {isShowingCookierPrompt && (
+        <Snackbar className="policies-snackbar" buttonText={t`Accept`} style={{backgroundColor: 'black'}} onClose={() => setIsShowingCookierPrompt(false)}>
+          <Trans>
+            If you continue browsing this website, you agree to our
+            <a href="https://moodle.com/privacy-notice/" target="_blank" rel="noreferrer">
+              Privacy notice
+            </a>
+            and
+            <a href="https://moodle.com/cookies-policy/" target="_blank" rel="noreferrer">
+              Cookies policy
+            </a>
+          </Trans>
+        </Snackbar>
+      )}
       {children}
     </div>
   )
 }
+MainPageWrapper.defaultProps = {}
 MainPageWrapper.displayName = 'MainPageWrapper'
