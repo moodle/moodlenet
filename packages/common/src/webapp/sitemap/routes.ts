7488fdfa62d6e7fa877e36ac51af76871840a1cc
aleclofabbro <alessandro.giansanti@gmail.com>
2021-09-24 12:40:41 +0200

    missed a module

--- packages/common/src/webapp/sitemap/routes.ts
+++ packages/common/src/webapp/sitemap/routes.ts
@@ -6,14 +6,14 @@ export type Login = RouteDef<'/login/:activationEmailToken?', { activationEmailT
 export type RecoverPassword = RouteDef<'/recover-password', {}>
 export type NewPassword = RouteDef<'/new-password/:token', { token: string }>
 export type Signup = RouteDef<'/signup', {}>
-export type TermsAndConditions = RouteDef<'/terms', {}>
 export type GlobalSearch = RouteDef<'/search', {}>
 export type CreateNewResource = RouteDef<'/create-new-resource', {}>
 export type CreateNewCollection = RouteDef<'/create-new-collection', {}>
 export type BookmarksPage = RouteDef<'/bookmarks', {}>
 export type FollowingPage = RouteDef<'/following', {}>
 export type LMSMoodleLanding = RouteDef<'/lms/moodle/search', {}>
-export type CookiesPolicies = RouteDef<'/cookies-policies', {}>
+export type CookiesPolicy = RouteDef<'/cookies-policy', {}>
+export type UserAgreement = RouteDef<'/user-agreement', {}>
 
 // node home pages
 export type ContentNodeHomePage = RouteDef<`/${string}/:slug`, { slug: string }>
