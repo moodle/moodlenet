b9845623ec18d26d95f2a8552e7c3b9b3ed453ba
aleclofabbro <alessandro.giansanti@gmail.com>
2021-09-25 12:06:40 +0200

    try fix serviceworker issue

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -18,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+navigator?.serviceWorker?.register('service-worker.js')
