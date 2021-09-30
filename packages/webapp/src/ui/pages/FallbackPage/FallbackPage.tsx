8446e2f15e7436a746d86a907350ecfb87bc60be
bru.mas <brumasribera@gmail.com>
2021-09-30 13:52:55 +0200

    Add Fallback Page

---
+++ packages/webapp/src/ui/pages/FallbackPage/FallbackPage.tsx
@@ -0,0 +1,35 @@
+import { Trans } from '@lingui/macro'
+import { ReactComponent as HatLogo } from '../../assets/icons/hat-moodle.svg'
+import { ReactComponent as MLogo } from '../../assets/icons/m-moodle.svg'
+import { CP, withCtrl } from '../../lib/ctrl'
+import { HeaderPageTemplate, HeaderPageTemplateProps } from '../../templates/page/HeaderPageTemplate'
+import './styles.scss'
+
+export type FallbackPageProps = {
+  headerPageTemplateProps: CP<HeaderPageTemplateProps>
+}
+
+export const FallbackPage = withCtrl<FallbackPageProps>(
+  ({
+    headerPageTemplateProps,
+  }) => {
+    return (
+      <HeaderPageTemplate {...headerPageTemplateProps}>
+        <div className="fallback-page">
+          <div className="content">
+            <Trans>Page not found or access not allowed</Trans>
+          </div>
+          <div className="hat-logo">
+            <HatLogo />
+          </div>
+          <div className="m-logo">
+            <MLogo />
+          </div>
+        </div>
+      </HeaderPageTemplate>
+    )
+  },
+)
+
+FallbackPage.defaultProps = {
+}
