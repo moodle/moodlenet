06170e8f5571a1cdfdddca1270c2b26718c0255a
aleclofabbro <alessandro.giansanti@gmail.com>
2021-10-21 16:56:21 +0200

    fixed delete node

--- packages/backend/src/adapters/content-graph/arangodb/aql/writes/deleteNode.ts
+++ packages/backend/src/adapters/content-graph/arangodb/aql/writes/deleteNode.ts
@@ -2,15 +2,15 @@ import { BV } from '@moodlenet/common/lib/content-graph/bl/graph-lang'
 import { GraphNode, GraphNodeType } from '@moodlenet/common/lib/content-graph/types/node'
 import { aq } from '../../../../../lib/helpers/arango/query'
 import { AqlGraphNode } from '../../types'
-import { aqlGraphNode2GraphNode } from '../helpers'
+import { aqlGraphNode2GraphNode, graphNode2AqlIdentifier } from '../helpers'
 
 export const deleteNodeQ = (nodeId: BV<GraphNode | null>, type: GraphNodeType) => {
   const q = aq<AqlGraphNode>(`
     let nodeToDelete = ${nodeId}
-    REMOVE nodeToDelete IN ${type} OPTIONS { ignoreErrors: true }
+    REMOVE ${graphNode2AqlIdentifier('nodeToDelete')} IN ${type} OPTIONS { ignoreErrors: true }
 
     RETURN ${aqlGraphNode2GraphNode('OLD')}
   `)
-  // console.log(q)
+  console.log(q)
   return q
 }
