240523ea2a880b412444e88e7924b332d92b962c
aleclofabbro <alessandro.giansanti@gmail.com>
2021-10-22 12:04:58 +0200

    root email env var on first install

--- packages/backend/src/adapters/user-auth/arangodb/db/maintainance/versionSteps/0.0.1.ts
+++ packages/backend/src/adapters/user-auth/arangodb/db/maintainance/versionSteps/0.0.1.ts
@@ -4,7 +4,6 @@ import { DistOmit } from '@moodlenet/common/lib/utils/types'
 import { VersionUpdater } from '../../../../../../lib/helpers/arango/migrate/types'
 import { justExecute } from '../../../../../../lib/helpers/arango/query'
 import { MNStaticEnv } from '../../../../../../lib/types'
-import { passwordHasher } from '../../../../../../ports/user-auth/adapters'
 import { ActiveUser } from '../../../../../../ports/user-auth/types'
 import { saveConfigQ } from '../../../queries/config'
 import { createNewUserQ } from '../../../queries/createNewUser'
@@ -16,7 +15,11 @@ const rootUserActive: DistOmit<ActiveUser, 'email' | 'password' | 'id' | 'create
 }
 
 const init_0_0_1: VersionUpdater<MNStaticEnv> = {
-  async initialSetUp({ db, ctx: { domain } }) {
+  async initialSetUp({ db /* ctx: { domain }  */ }) {
+    const rootEmail = process.env.ROOT_EMAIL ?? ''
+    if (!EMAILREGEX.test(rootEmail)) {
+      throw new Error(`User Auth setup: need a env ROOT_EMAIL to be a valid email`)
+    }
     console.log(`creating user-auth collection ${CONFIG}`)
     await db.createCollection(CONFIG)
     await justExecute(saveConfigQ(DefaultConfig), db)
@@ -24,11 +27,11 @@ const init_0_0_1: VersionUpdater<MNStaticEnv> = {
     console.log(`creating user-auth collection ${USER}`)
     await db.createCollection(USER)
 
-    const password = await passwordHasher('root')
+    const password = `---no-root-password-set---${Math.random().toString(36).substr(2)}`
     await justExecute(
       createNewUserQ({
         ...rootUserActive,
-        email: `root@${domain}`,
+        email: rootEmail,
         password,
       }),
       db,
@@ -37,3 +40,7 @@ const init_0_0_1: VersionUpdater<MNStaticEnv> = {
 }
 
 module.exports = init_0_0_1
+
+// http://emailregex.com/
+const EMAILREGEX =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
