d8c082cbe8fb8fc6dddc1d83fb6f9e2fcd36f0cb
aleclofabbro <alessandro.giansanti@gmail.com>
2021-11-03 12:59:19 +0100

    better assertions byTerm, added "concept map" type

--- packages/backend/src/adapters/b-rules/search/byTerm.ts
+++ packages/backend/src/adapters/b-rules/search/byTerm.ts
@@ -4,15 +4,21 @@ import { graphOperators } from '../../../ports/content-graph/graph-lang/graph'
 import { BRules, operators } from '../../../ports/content-graph/search/byTerm'
 import { isLocalOrganizationAuthId } from '../helpers'
 
-type Rules = 'mustBePublishedOrIssuerIsCreatorOfNode'
+type Rules = 'mustBePublished' | 'mustBePublishedOrIssuerIsCreatorOfNode'
 
 export const searchNodeBRules: SockOf<BRules> = async ({ arg, sessionEnv }) => {
-  if (await isLocalOrganizationAuthId(sessionEnv.authId)) {
-    return { ...arg, assertions: {} }
-  }
-  const { or } = await baseOperators()
   const { isCreator, graphNode, isPublished } = await graphOperators()
   const { searchNode } = await operators()
+
+  if (await isLocalOrganizationAuthId(sessionEnv.authId)) {
+    return {
+      ...arg,
+      assertions: {
+        mustBePublished: isPublished(searchNode),
+      },
+    }
+  }
+  const { or } = await baseOperators()
   const assertions: Assertions<Rules> = {
     mustBePublishedOrIssuerIsCreatorOfNode: or(
       isPublished(searchNode),
