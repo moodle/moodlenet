569a0cea514d42ebb98a3e123a75620c1d27a497
aleclofabbro <alessandro.giansanti@gmail.com>
2021-11-05 18:04:08 +0100

    fixed editNode-rule allowing profile to self edit

--- packages/backend/src/adapters/b-rules/node/edit.ts
+++ packages/backend/src/adapters/b-rules/node/edit.ts
@@ -1,10 +1,10 @@
 import { SockOf } from '../../../lib/plug'
-import { Assertions } from '../../../ports/content-graph/graph-lang/base'
+import { Assertions, baseOperators } from '../../../ports/content-graph/graph-lang/base'
 import { graphOperators } from '../../../ports/content-graph/graph-lang/graph'
 import { bRules, operators } from '../../../ports/content-graph/node/edit'
 import { isLocalOrganizationAuthId } from '../helpers'
 
-type Rules = 'mustBeCreatorOfNode'
+type Rules = 'mustBeCreatorOfNodeOrSelf'
 
 export const editNodeBRules: SockOf<typeof bRules> = async ({ arg, sessionEnv }) => {
   if (!sessionEnv.authId) {
@@ -13,10 +13,14 @@ export const editNodeBRules: SockOf<typeof bRules> = async ({ arg, sessionEnv })
   if (await isLocalOrganizationAuthId(sessionEnv.authId)) {
     return { ...arg, assertions: {} }
   }
-  const { isCreator, graphNode } = await graphOperators()
+  const { or } = await baseOperators()
+  const { isCreator, graphNode, isSameNode } = await graphOperators()
   const { editNode } = await operators()
   const assertions: Assertions<Rules> = {
-    mustBeCreatorOfNode: isCreator({ authNode: graphNode(sessionEnv.authId), ofGlyph: editNode }),
+    mustBeCreatorOfNodeOrSelf: or(
+      isSameNode(graphNode(sessionEnv.authId), editNode),
+      isCreator({ authNode: graphNode(sessionEnv.authId), ofGlyph: editNode }),
+    ),
   }
 
   return { ...arg, assertions }
