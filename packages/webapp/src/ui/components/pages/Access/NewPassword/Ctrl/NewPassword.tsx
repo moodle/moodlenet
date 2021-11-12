e7d4a4f2192d444d615e14c7e97bed1915579ef6
aleclofabbro <alessandro.giansanti@gmail.com>
2021-11-12 13:12:46 +0100

    yup schemas for user-access forms

--- packages/webapp/src/ui/components/pages/Access/NewPassword/Ctrl/NewPassword.tsx
+++ packages/webapp/src/ui/components/pages/Access/NewPassword/Ctrl/NewPassword.tsx
@@ -1,4 +1,6 @@
+import { t } from '@lingui/macro'
 import { useCallback, useMemo, useState } from 'react'
+import { object, SchemaOf, string } from 'yup'
 import { useSession } from '../../../../../../context/Global/Session'
 import { useRedirectProfileHomeIfLoggedIn } from '../../../../../../hooks/glob/nav'
 import { ctrlHook, CtrlHook } from '../../../../../lib/ctrl'
@@ -7,29 +9,48 @@ import { useMainPageWrapperCtrl } from '../../../../templates/MainPageWrapperCtr
 import { useAccessHeaderCtrl } from '../../AccessHeader/Ctrl/AccessHeaderCtrl'
 import { NewPasswordFormValues, NewPasswordProps } from '../NewPassword'
 
-export const useNewPasswordCtrl: CtrlHook<NewPasswordProps, { recoverPasswordToken: string }> = ({
-  recoverPasswordToken,
-}) => {
+const validationSchema: SchemaOf<NewPasswordFormValues> = object({
+  newPassword: string()
+    .required(t`Please provide a password`)
+    .min(6, 'Password is too short should be 6 chars minimum.'),
+})
+export const useNewPasswordCtrl: CtrlHook<
+  NewPasswordProps,
+  { recoverPasswordToken: string }
+> = ({ recoverPasswordToken }) => {
   useRedirectProfileHomeIfLoggedIn({ delay: 618 })
   const { changeRecoverPassword } = useSession()
-  const [newPasswordErrorMessage, setNewPasswordErrorMessage] = useState<string | null>(null)
+  const [newPasswordErrorMessage, setNewPasswordErrorMessage] = useState<
+    string | null
+  >(null)
   const onSubmit = useCallback<SubmitForm<NewPasswordFormValues>>(
     ({ newPassword }) =>
-      changeRecoverPassword({ newPassword, recoverPasswordToken }).then(err => {
-        setNewPasswordErrorMessage(err)
-      }),
-    [changeRecoverPassword, recoverPasswordToken],
+      changeRecoverPassword({ newPassword, recoverPasswordToken }).then(
+        (err) => {
+          setNewPasswordErrorMessage(err)
+        }
+      ),
+    [changeRecoverPassword, recoverPasswordToken]
   )
   const [, formBag] = useFormikBag<NewPasswordFormValues>({
     initialValues: { newPassword: '' },
     onSubmit,
+    validationSchema,
   })
   const newPasswordProps = useMemo<NewPasswordProps>(() => {
     const newPasswordProps: NewPasswordProps = {
-      accessHeaderProps: ctrlHook(useAccessHeaderCtrl, {}, 'New Password Access Header'),
+      accessHeaderProps: ctrlHook(
+        useAccessHeaderCtrl,
+        {},
+        'New Password Access Header'
+      ),
       formBag,
       newPasswordErrorMessage,
-      mainPageWrapperProps: ctrlHook(useMainPageWrapperCtrl, {}, 'main-page-wrapper'),
+      mainPageWrapperProps: ctrlHook(
+        useMainPageWrapperCtrl,
+        {},
+        'main-page-wrapper'
+      ),
     }
     return newPasswordProps
   }, [formBag, newPasswordErrorMessage])
