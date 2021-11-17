624ec1553cf9c2f9e1e763274cf890d5d807c143
aleclofabbro <alessandro.giansanti@gmail.com>
2021-11-17 16:34:05 +0100

    fixes in setup initial population

--- packages/backend/src/adapters/content-graph/arangodb/db/maintainance/versionSteps/2.0.0/populateDBNodes.ts
+++ packages/backend/src/adapters/content-graph/arangodb/db/maintainance/versionSteps/2.0.0/populateDBNodes.ts
@@ -12,7 +12,13 @@ export const populateDBNodes = async ({ db }: { db: Database }) =>
       const nodeType = nodes[0]?._type!
       console.log(`creating ${nodes.length} ${nodeType} nodes`)
       const collection = db.collection(nodeType)
-      await collection.saveAll(nodes)
+      const aqlNodes = nodes.map(node => {
+        const aqlNode: any = { ...node }
+        delete aqlNode._permId
+        aqlNode._key = node._permId
+        return aqlNode
+      })
+      await collection.saveAll(aqlNodes)
     },
     Promise.resolve(),
   )
