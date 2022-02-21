2da4f496cec911495503d490a39169eeb997336b
aleclofabbro <alessandro.giansanti@gmail.com>
2022-02-21 13:00:34 +0100

    hooked followers page

--- packages/common/src/webapp/sitemap/routes.ts
+++ packages/common/src/webapp/sitemap/routes.ts
@@ -1,19 +1,21 @@
 import { RouteDef } from './types'
 
-export type Landing = RouteDef<'/', {}>
+type EmptyParams = Record<string, never>
+export type Landing = RouteDef<'/', EmptyParams>
 // export type Activation = RouteDef<'/activate-new-user/:token', { token: string }>
 export type Login = RouteDef<'/login/:activationEmailToken?', { activationEmailToken?: string }>
-export type RecoverPassword = RouteDef<'/recover-password', {}>
+export type RecoverPassword = RouteDef<'/recover-password', EmptyParams>
 export type NewPassword = RouteDef<'/new-password/:token', { token: string }>
-export type Signup = RouteDef<'/signup', {}>
-export type GlobalSearch = RouteDef<'/search', {}>
-export type CreateNewResource = RouteDef<'/create-new-resource', {}>
-export type CreateNewCollection = RouteDef<'/create-new-collection', {}>
-export type BookmarksPage = RouteDef<'/bookmarks', {}>
-export type FollowingPage = RouteDef<'/following', {}>
-export type LMSMoodleLanding = RouteDef<'/lms/moodle/search', {}>
-export type CookiesPolicy = RouteDef<'/cookies-policy', {}>
-export type UserAgreement = RouteDef<'/user-agreement', {}>
+export type Signup = RouteDef<'/signup', EmptyParams>
+export type GlobalSearch = RouteDef<'/search', EmptyParams>
+export type CreateNewResource = RouteDef<'/create-new-resource', EmptyParams>
+export type CreateNewCollection = RouteDef<'/create-new-collection', EmptyParams>
+export type BookmarksPage = RouteDef<'/bookmarks', EmptyParams>
+export type FollowersPage = RouteDef<'/followers', EmptyParams>
+export type FollowingPage = RouteDef<'/following', EmptyParams>
+export type LMSMoodleLanding = RouteDef<'/lms/moodle/search', EmptyParams>
+export type CookiesPolicy = RouteDef<'/cookies-policy', EmptyParams>
+export type UserAgreement = RouteDef<'/user-agreement', EmptyParams>
 
 // node home pages
 export type ContentNodeHomePage = RouteDef<`/${string}/:slug`, { slug: string }>
