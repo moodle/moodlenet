7605fb50d25ca3acba3da7bccd4315a1a0b7836f
aleclofabbro <alessandro.giansanti@gmail.com>
2022-02-21 13:00:34 +0100

    hooked followers page

--- packages/webapp/src/ui/components/molecules/cards/SmallProfileCard/Ctrl/SmallProfileCardCtrl.tsx
+++ packages/webapp/src/ui/components/molecules/cards/SmallProfileCard/Ctrl/SmallProfileCardCtrl.tsx
@@ -8,6 +8,7 @@ import { useCallback, useMemo } from 'react'
 import { useLocalInstance } from '../../../../../../context/Global/LocalInstance'
 import { useSession } from '../../../../../../context/Global/Session'
 import { getMaybeAssetRefUrl } from '../../../../../../helpers/data'
+import { mainPath } from '../../../../../../hooks/glob/nav'
 import { href } from '../../../../../elements/link'
 import { CtrlHook } from '../../../../../lib/ctrl'
 import {
@@ -16,6 +17,7 @@ import {
   useProfilePageUserDataQuery,
 } from '../../../../pages/Profile/Ctrl/ProfileCtrl.gen'
 import { SmallProfileCardProps } from '../SmallProfileCard'
+const followersHref = href(mainPath.followers)
 
 export type SmallProfileCardCtrlArg = { id: ID }
 export const useSmallProfileCardCtrl: CtrlHook<
@@ -104,6 +106,7 @@ export const useSmallProfileCardCtrl: CtrlHook<
               resources: profileNode.resourcesCount,
               kudos,
               years: 0,
+              followersHref,
             },
             avatarUrl: getMaybeAssetRefUrl(profileNode.avatar),
             organizationName: org.name,
---
+++ packages/webapp/src/ui/components/pages/Followers/Ctrl/FollowersCtrl.tsx
@@ -0,0 +1,67 @@
+import {
+  isEdgeNodeOfType,
+  narrowNodeType,
+} from '@moodlenet/common/dist/graphql/helpers'
+import { useEffect, useMemo } from 'react'
+import { useSession } from '../../../../../context/Global/Session'
+import { ctrlHook, CtrlHook } from '../../../../lib/ctrl'
+import { useSmallProfileCardCtrl } from '../../../molecules/cards/SmallProfileCard/Ctrl/SmallProfileCardCtrl'
+import { useHeaderPageTemplateCtrl } from '../../../templates/HeaderPageTemplateCtrl/HeaderPageTemplateCtrl'
+import { FollowersProps } from '../Followers'
+import { useFollowersPageLazyQuery } from './Followers.gen'
+
+export const useFollowersCtrl: CtrlHook<FollowersProps, {}> = () => {
+  // const [sortBy, setSortBy] = useState<GlobalFollowersSort>('Popularity')
+  const { session } = useSession()
+  const [queryFollowers, followersQ] = useFollowersPageLazyQuery({
+    fetchPolicy: 'cache-and-network',
+  })
+  useEffect(() => {
+    if (!session?.profile.id) {
+      return
+    }
+    queryFollowers({
+      variables: {
+        profileId: session.profile.id,
+      },
+    })
+  }, [queryFollowers, session?.profile.id])
+
+  const profileNode = narrowNodeType(['Profile', 'Organization'])(
+    followersQ.data?.node
+  )
+
+  const followers = useMemo(
+    () =>
+      (profileNode?.followers.edges || [])
+        .filter(isEdgeNodeOfType(['Profile']))
+        .map(({ node }) => node),
+    [profileNode?.followers.edges]
+  )
+
+  const followersUIProps = useMemo(() => {
+    const props: FollowersProps = {
+      headerPageTemplateProps: ctrlHook(
+        useHeaderPageTemplateCtrl,
+        {},
+        'header-page-template'
+      ),
+
+      browserProps: {
+        smallProfileCardPropsList: followers.map((profile) =>
+          ctrlHook(
+            useSmallProfileCardCtrl,
+            { id: profile.id },
+            `Profile's Followers ${profile.id} Card`
+          )
+        ),
+        collectionCardPropsList: null,
+        resourceCardPropsList: null,
+        subjectCardPropsList: null,
+        setSortBy: null,
+      },
+    }
+    return props
+  }, [followers])
+  return [followersUIProps]
+}
