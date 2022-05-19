b1052958b27bf198e502bbc5c1eb68ee5a78c47d
aleclofabbro <alessandro.giansanti@gmail.com>
2022-05-19 22:13:16 +0200

    extensions.ts as webpack virtual module :)

--- packages/test-extension/src/webapp/TestExtPage.tsx
+++ packages/test-extension/src/webapp/TestExtPage.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 export const TestExtPage: FC = () => {
   return (
     <div>
-      <h2>Test aaaa Extension</h2>
+      <h2>Test nice Extension</h2>
       <h3>...stuff</h3>
       <span>...more</span>
     </div>
