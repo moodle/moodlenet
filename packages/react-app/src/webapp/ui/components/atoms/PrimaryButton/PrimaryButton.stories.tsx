5ccd35a46720db4e77c955bc0a52340133bdd9e4
Bru Mas Ribera <bru.mas@moodle.com>
2022-05-27 18:12:06 +0200

    Add primary button left behind

---
+++ packages/react-app/src/webapp/ui/components/atoms/PrimaryButton/PrimaryButton.stories.tsx
@@ -0,0 +1,15 @@
+// import { ComponentMeta, ComponentStory } from '@storybook/react'
+// import { PrimaryButton } from './PrimaryButton'
+
+// const meta: ComponentMeta<typeof PrimaryButton> = {
+//   title: 'Atoms/PrimaryButton',
+//   component: PrimaryButton,
+// }
+
+// const PrimaryButtonStory: ComponentStory<typeof PrimaryButton> = () => (
+//   <PrimaryButton>Primary Button</PrimaryButton>
+// )
+
+// export const Default = PrimaryButtonStory.bind({})
+
+// export default meta
