41ca33e5d2d7b4cf002ab9ed3559315461fca4ef
Bru Mas Ribera <brumasribera@gmail.com>
2022-05-27 18:12:06 +0200

    Add primary button left behind

---
+++ packages/react-app/src/webapp/ui/components/atoms/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode } from 'react'
+import './styles.scss'
+
+export type PrimaryButtonProps = {
+  onClick?(arg0?: unknown): unknown | any
+  className?: string
+  disabled?: boolean
+  color?: '' | 'green' | 'red' | 'grey' | 'blue' | 'card' | 'light-gray'
+  onHoverColor?: '' | 'green' | 'orange' | 'red' | 'green'
+  children?: ReactNode
+}
+
+export const PrimaryButton: FC<PrimaryButtonProps> = ({
+  children,
+  className,
+  color,
+  onHoverColor,
+  disabled,
+  onClick,
+}) => {
+  return (
+    <div
+      className={`primary-button button ${className} ${onHoverColor} ${disabled ? 'disabled' : ''} ${color}`}
+      tabIndex={!disabled ? 0 : undefined}
+      onClick={!disabled ? onClick : () => {}}
+      onKeyDown={e => !disabled && onClick && e.key === 'Enter' && onClick()}
+    >
+      {children}
+    </div>
+  )
+}
+
+PrimaryButton.defaultProps = {
+  color: '',
+  onHoverColor: '',
+}
+
+export default PrimaryButton
