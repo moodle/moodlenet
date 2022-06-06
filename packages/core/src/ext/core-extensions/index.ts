1b3865e195a3f5454d4e0e03e5e290e28f427728
aleclofabbro <alessandro.giansanti@gmail.com>
2022-06-06 13:50:29 +0200

    cleanup

--- packages/core/src/ext/core-extensions/index.ts
+++
@@ -1,4 +0,0 @@
-export * as coreExt from './blob-store'
-export * as identityManager from './identity-manager'
-export * as keyValueStore from './key-value-store'
-export * as sysLog from './sys-log'
