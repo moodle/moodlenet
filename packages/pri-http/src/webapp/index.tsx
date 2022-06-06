386469b8069ac9389e317c5cb1a6831f2b96f058
aleclofabbro <alessandro.giansanti@gmail.com>
2022-06-06 16:05:17 +0200

    minor important fixes

--- packages/pri-http/src/webapp/index.tsx
+++ packages/pri-http/src/webapp/index.tsx
@@ -2,7 +2,7 @@ import { ReactAppExtMain } from '@moodlenet/react-app'
 import { Context, createContext, FC, PropsWithChildren, useMemo } from 'react'
 import { HttpAdapterCtx } from './types'
 import { sub } from './xhr-adapter'
-// import './_test_sub'
+import './_test_sub'
 
 const ext: ReactAppExtMain<{
   HttpAdapterCtx: Context<HttpAdapterCtx>
