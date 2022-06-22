868fe428a16d3ff6a30cd10fc684590351c04ebc
aleclofabbro <alessandro.giansanti@gmail.com>
2022-06-22 11:56:08 +0200

    ...

--- packages/core/src/main/core-pkgs.ts
+++ packages/core/src/main/core-pkgs.ts
@@ -1,6 +1,6 @@
 export default {
-  'core': '0.0.4',
-  'http-server': '0.0.4',
-  'react-app': '0.0.4',
-  'pri-http': '0.0.4',
+  'core': '0.0.5',
+  'http-server': '0.0.5',
+  'react-app': '0.0.5',
+  'pri-http': '0.0.5',
 }
