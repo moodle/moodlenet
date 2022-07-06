ab58f087b4e47aabfd4961ed3b8fb2ef49ee30b5
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-06 14:00:21 +0200

    added react-scripts dev-dep in react-app for importing web-resources stuff in react

--- packages/react-app/moodlenet-react-app-lib.d.ts
+++ packages/react-app/moodlenet-react-app-lib.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="react-scripts" />
 /// <reference path="./src/react-app-lib/index.ts" />
 
 declare module 'moodlenet-react-app-lib' {
--- packages/react-app/src/webpackWatch.ts
+++ packages/react-app/src/webpackWatch.ts
@@ -31,7 +31,8 @@ async function start({
   overrideCfg?: (_: Configuration) => Configuration
   virtualModules: VirtualModulesPlugin
 }) {
-  const mode: Configuration['mode'] = process.env.NODE_ENV ?? ('production' as any)
+  const mode: Configuration['mode'] =
+    process.env.NODE_ENV === 'test' ? 'none' : process.env.NODE_ENV ?? ('production' as any)
   const isDevelopment = mode === 'development'
   const wp = webpack(overrideCfg(defaultConfig()), () => {
     // a cb .. otherways err:DEP_WEBPACK_WATCH_WITHOUT_CALLBACK
