30a8b371746429eb09fa03cb5cccf899cd8e7414
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-07-07 14:27:50 +0200

    path fixing

--- packages/gauth/src/server/routes/dist/auth.js
+++ packages/gauth/src/server/routes/dist/auth.js
@@ -1,26 +1,13 @@
-/*******************************
- gli users presi dai provider , passport li vede con questi parametri :
-    id: '110888829689446897313',
-    displayName: 'Ettore Bevilacqua',
-    name: { familyName: 'Bevilacqua', givenName: 'Ettore' },
-    photos: [
-      {
-        value: 'https://lh3.googleusercontent.com/a/AItbvmmQ1LfB0J2T-ohbHx0rddqM1rCI2bhIFUUYScai=s96-c'
-      }
-    ],
-    provider: 'google',
-  ****/
-
-import express, { Router } from 'express';
-import passport from 'passport';
-import passportGoogle from "passport-google-oauth20";
-import { verify } from '../db';
-
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var passport_1 = require("passport");
+var passport_google_oauth20_1 = require("passport-google-oauth20");
+var db_1 = require("../db");
 // import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../config";
-const GoogleStrategy = passportGoogle.Strategy;
-
-const GOOGLE_CLIENT_ID = '760051383014-iln3njej3ij9otp0jufrkvo1lu40i6k4.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET = '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'
+var GoogleStrategy = passport_google_oauth20_1["default"].Strategy;
+var GOOGLE_CLIENT_ID = '760051383014-iln3njej3ij9otp0jufrkvo1lu40i6k4.apps.googleusercontent.com';
+var GOOGLE_CLIENT_SECRET = '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';
 /*
 gauth profile  {
   id: '110888829689446897313',
@@ -50,7 +37,6 @@ gauth profile  {
   }
 }
 */
-
 /*
 passport.use(
   new GoogleStrategy(
@@ -65,25 +51,32 @@ passport.use(
     }
   )
 );*/
-passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: '/_/moodlenet-gauth/a/oauth2/redirect/google',
-  scope: ['profile'],
-  state: true
-},
-  (accessToken, refreshToken, profile, done) => {
-    console.log('gauth token ' , accessToken, refreshToken)
-    console.log('xxxxx gauth profile ' , profile)
-    // console.log('gauth accessToken ' , accessToken, refreshToken)
-  
-    verify('https://accounts.google.com', profile, (err: any, user: any) => {
-      // console.log('xxxxxxxxxxxxxxxxxxxxxx has verify', err, user);
-      done(err, user)
-    })
-  }
-));
-
+passport_1["default"].use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: '/_/moodlenet-gauth/a/oauth2/redirect/google',
+    scope: ['profile'],
+    state: true
+}, function (accessToken, refreshToken, profile, done) {
+    console.log('gauth token ', accessToken, refreshToken);
+    console.log('gauth profile ', profile.id);
+    console.log('gauth accessToken ', accessToken, refreshToken);
+    /*
+    id: '110888829689446897313',
+    displayName: 'Ettore Bevilacqua',
+    name: { familyName: 'Bevilacqua', givenName: 'Ettore' },
+    photos: [
+      {
+        value: 'https://lh3.googleusercontent.com/a/AItbvmmQ1LfB0J2T-ohbHx0rddqM1rCI2bhIFUUYScai=s96-c'
+      }
+    ],
+    provider: 'google',
+    */
+    db_1.verify('https://accounts.google.com', profile, function (err, user) {
+        // console.log('xxxxxxxxxxxxxxxxxxxxxx has verify', err, user);
+        done(err, user);
+    });
+}));
 // Configure the Facebook strategy for use by Passport.
 //
 // OAuth 2.0-based strategies require a `verify` function which receives the
@@ -91,7 +84,6 @@ passport.use(new GoogleStrategy({
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-
 /*
  passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
@@ -104,7 +96,6 @@ function(accessToken:any, refreshToken:any, profile:any, cb:any) {
   verify('https://accounts.google.com', profile, cb)
 }));
 */
-
 // Configure Passport authenticated session persistence.
 //
 // In order to restore authentication state across HTTP requests, Passport needs
@@ -114,22 +105,17 @@ function(accessToken:any, refreshToken:any, profile:any, cb:any) {
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Facebook profile is serialized
 // and deserialized.
-passport.serializeUser(function (user: any, cb) {
-  process.nextTick(function () {
-    cb(null, { id: user.id, username: user.username, name: user.name });
-  });
+passport_1["default"].serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username, name: user.name });
+    });
 });
-
-passport.deserializeUser(function (user: any, cb) {
-  process.nextTick(function () {
-    return cb(null, user);
-  });
+passport_1["default"].deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
 });
-
-
-const router: Router = express.Router();
-
-
+var router = express_1["default"].Router();
 /* GET /login
  *
  * This route prompts the user to log in.constconst
@@ -139,9 +125,8 @@ const router: Router = express.Router();
  * will be sent to the `GET /login/federated/accounts.google.com` route.
  */
 router.get('/login', function (_, res) {
-  res.render('login');
+    res.render('login');
 });
-
 /* GET /login/federated/accounts.google.com
  *
  * This route redirects the user to Google, where they will authenticate.
@@ -154,26 +139,28 @@ router.get('/login', function (_, res) {
  * Once Google has completed their interaction with the user, the user will be
  * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
  */
-router.get('/login/federated/google', passport.authenticate('google'));
-
+router.get('/login/federated/google', passport_1["default"].authenticate('google'));
 /*
     This route completes the authentication sequence when Google redirects the
     user back to the application.  When a new user signs in, a user account is
     automatically created and their Google account is linked.  When an existing
     user returns, they are signed in to their linked account.
 */
-router.get('/oauth2/redirect/google', passport.authenticate('google', {
-  successReturnToOrRedirect: '/_/moodlenet-gauth/users',
-  failureRedirect: '/_/moodlenet-gauth/a/login',
+router.get('/oauth2/redirect/google', passport_1["default"].authenticate('google', {
+    successReturnToOrRedirect: '/_/moodlenet-gauth/users',
+    failureRedirect: '/login'
 }));
-
+router.get('/test/oauth2/redirect/google', function (req, res) {
+    // res.json(req.query);
+    console.log('xxxxx rerurn from google xxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+    res.redirect('/_/moodlenet-gauth/users');
+});
 /* POST /logout
  *
  * This route logs the user out.
  */
 router.post('/logout', function (req, res) {
-  req.logout({}, () => console.log('logout'));
-  res.redirect('/');
+    req.logout({}, function () { return console.log('logout'); });
+    res.redirect('/');
 });
-
-export default router
+exports["default"] = router;
