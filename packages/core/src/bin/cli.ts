9a11e5935fd48d6f49e3684c9fadd1529e8098b7
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-14 14:19:42 +0200

    core/bin/cli#boot require-installed-core.boot({})

--- packages/core/src/bin/cli.ts
+++ packages/core/src/bin/cli.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import { resolve } from 'path'
-import boot from '../main/boot'
+// import boot from '../main/boot'
 import install from '../main/install'
+import { SYS_CONFIG_FILE_NAME } from '../main/prepareFileSystem'
 import { MainFolders } from '../types/sys'
 const [cmd, _deploymentFolder, _systemFolder] = process.argv.slice(2)
 const deploymentFolder = _deploymentFolder ? resolve(process.cwd(), _deploymentFolder) : process.cwd()
@@ -17,8 +18,8 @@ if (cmd === 'boot') {
   const DEV_MODE_VALUE = 'development'
   const NODE_ENV = process.env.NODE_ENV ?? DEV_MODE_VALUE
   const devMode = NODE_ENV === DEV_MODE_VALUE
-
-  boot({ mainFolders, devMode })
+  const coreInstallationFolder = require(resolve(systemFolder, SYS_CONFIG_FILE_NAME)).core.installationFolder as string
+  require(resolve(coreInstallationFolder, 'lib', 'index.js'))({ mainFolders, devMode })
 } else if (cmd === 'install') {
   install({ mainFolders })
 } else {
