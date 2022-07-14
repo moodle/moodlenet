bc1c6193f6ea17011eda99656878aabafb1a0720
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-14 11:17:27 +0200

    added publish core/bin & cli _system arg

--- packages/core/src/bin/cli.ts
+++ packages/core/src/bin/cli.ts
@@ -3,14 +3,15 @@ import { resolve } from 'path'
 import boot from '../main/boot'
 import install from '../main/install'
 import { MainFolders } from '../types/sys'
-const [cmd, _deploymentFolder] = process.argv.slice(2)
+const [cmd, _deploymentFolder, _systemFolder] = process.argv.slice(2)
 const deploymentFolder = _deploymentFolder ? resolve(process.cwd(), _deploymentFolder) : process.cwd()
-const systemFolder = deploymentFolder ?? resolve(deploymentFolder, '_system')
+const systemFolder = _systemFolder ?? resolve(deploymentFolder, '_system')
 
 const mainFolders: MainFolders = {
   deploymentFolder,
   systemFolder,
 }
+console.log({ _deploymentFolder, deploymentFolder, _systemFolder, systemFolder })
 console.log({ cmd, deploymentFolder })
 if (cmd === 'boot') {
   const DEV_MODE_VALUE = 'development'
