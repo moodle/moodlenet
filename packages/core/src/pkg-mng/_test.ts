c9b61fee2193751e7522c26b775ff24c3588c1a9
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-15 10:02:56 +0200

    wip

--- packages/core/src/pkg-mng/_test.ts
+++ packages/core/src/pkg-mng/_test.ts
@@ -1,16 +1,17 @@
 import { mkdir, rm } from 'fs/promises'
+import { resolve } from 'path'
 import { inspect } from 'util'
 import { createPkgMng } from '.'
 ;(async function start() {
   const pkgsFolder = '../../.ignore/pkgmngfolder'
   await rm(pkgsFolder, { recursive: true, force: true })
   await mkdir(pkgsFolder, { recursive: true })
-  const pkgMng = createPkgMng({ pkgsFolder })
+  const pkgMng = createPkgMng({ pkgsFolder, symlinkFolder: resolve(__dirname, '..', '..', '..') })
   await pkgMng.install({
     type: 'symlink',
     fromFolder: '/home/alec/MOODLENET/repo/moodlenet3/packages/passport-auth',
   })
   // await pkgMng.install({ type: 'npm', registry: 'https://registry.npmjs.org', pkgId: 'tiny' })
-  const allInfos = await pkgMng.getAllInstalledPackagesInfo()
+  const allInfos = await pkgMng.getAllSymlinkPackagesInfo()
   console.log(inspect(allInfos, false, 10, true))
 })()
