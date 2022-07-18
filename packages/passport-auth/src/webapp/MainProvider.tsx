c7094fda4e8bf762b0bfdb0ce119a6a1081b8767
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-18 15:23:19 +0200

    addedd passport settings stub + removed Settings from header menu

--- packages/passport-auth/src/webapp/MainProvider.tsx
+++ packages/passport-auth/src/webapp/MainProvider.tsx
@@ -1,10 +1,12 @@
 import { ExtContextProviderComp } from '@moodlenet/react-app'
 import lib from 'moodlenet-react-app-lib'
 import * as loginComponents from './Login'
+import * as settingsComponents from './Settings'
 
 export const MainProvider: ExtContextProviderComp = ({ children }) => {
   lib.auth.useRegisterLogin(loginComponents)
   lib.auth.useRegisterSignup(loginComponents)
+  lib.settings.useRegisterSettingsItem(settingsComponents)
   return <>{children}</>
 }
 
