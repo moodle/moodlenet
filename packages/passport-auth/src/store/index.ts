f780042acbb26e92e0c803fba0920ab3d10cf9d3
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-07-19 14:55:56 +0200

    feat: test

--- packages/passport-auth/src/store/index.ts
+++ packages/passport-auth/src/store/index.ts
@@ -18,7 +18,7 @@ export default function configApiKeyStore({ folder }: { folder: string }) {
     write,
   }
 
-   async async function patchConfigs(patch: PassportConfigs) {
+  async function patchConfigs(patch: PassportConfigs) {
     const currConfigs = await read()
     const patchedConfigs = { ...currConfigs, ...patch }
     await write(patchedConfigs)
@@ -30,7 +30,7 @@ export default function configApiKeyStore({ folder }: { folder: string }) {
   }
 
   async function write(configs: any) {
-    await writeFile(file(), JSON.stringify(configs, null, 2))
+      await writeFile(file(), JSON.stringify(configs, null, 2))
   }
 
   function file() {
