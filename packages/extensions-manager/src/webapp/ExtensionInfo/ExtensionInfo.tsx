6209211d2b5c96294bf27b23118643dfeb7bd4aa
Bru Mas Ribera <brumasribera@gmail.com>
2022-07-20 15:14:47 +0200

    Fix extension config loading

--- packages/extensions-manager/src/webapp/ExtensionInfo/ExtensionInfo.tsx
+++ packages/extensions-manager/src/webapp/ExtensionInfo/ExtensionInfo.tsx
@@ -58,6 +58,7 @@ const ExtensionInfo: FC<ExtensionInfoProps> = ({ toggleIsInstalling, searchPacka
 
     promise.finally(toggleIsInstalling)
   }, [isInstalled, searchPackagesResObject.installPkgReq])
+
   type CodeBlockProps = {
     node: any
     children: ReactNode & ReactNode[]
--- packages/extensions-manager/src/webapp/SettingsEnabledExt.tsx
+++ packages/extensions-manager/src/webapp/SettingsEnabledExt.tsx
@@ -4,6 +4,6 @@ import Packages from './Packages/Packages'
 
 export const Menu: FC = () => {
   const { setSelectedExtConfig } = useContext(StateContext)
-  return <span onClick={() => setSelectedExtConfig(null)}>Installed Extensions</span>
+  return <span onClick={() => setSelectedExtConfig(null)}>Manage Extensions</span>
 }
 export const Content = Packages
