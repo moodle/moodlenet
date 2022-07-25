e706870e2a34815df7c93193eee652f95ffa2ef5
aleclofabbro <alessandro.giansanti@gmail.com>
2022-07-25 14:59:38 +0200

    ignore MN2 packages from npm search

--- packages/extensions-manager/src/lib.ts
+++ packages/extensions-manager/src/lib.ts
@@ -17,8 +17,15 @@ export async function searchPackagesFromRegistry({
 
   const res = await axios.get<SearchResponse>(`${registry}/-/v1/search`, { params: { text } })
   console.log('npm resp packages', inspect(res.data.objects.map(_ => _.package)))
+
+  // !!!!!!! REMOVE ME ! JUST FOR DEMO !
+  res.data.objects = res.data.objects.filter(({ package: { name } }) => !___ignore___mn2_pkgs.includes(name))
+  // !!!!!!!
+
   return res.data
 }
 /* 
 keywords:moodlenetPackage
  */
+
+const ___ignore___mn2_pkgs = ['@moodlenet/webapp', '@moodlenet/common', '@moodlenet/backend', '@moodlenet/ce-platform']
