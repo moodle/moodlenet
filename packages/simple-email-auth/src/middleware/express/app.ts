1c680b5b15a1b93a71571e28ea45dd346f48c3ce
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-08-08 09:33:44 +0200

    fix error

--- packages/simple-email-auth/src/middleware/express/app.ts
+++ packages/simple-email-auth/src/middleware/express/app.ts
@@ -1,9 +1,9 @@
 // import csrf from 'csurf'
 import { Shell } from '@moodlenet/core'
 
-export function prepareApp(shell: Shell<any>, app: any) {
+export function prepareApp(_: Shell<any>, app: any) {
   // app.use(csrf())
-  app.get('/confirm', async (req:any, res:any, next:any) => {
+  app.get('/confirm', async (_:any, __:any) => {
    console.log('email has confirm xxxxxxxxxxxxx')
   })
 
