c39645f0a38738bc1aedf88e79b39d4eb325f8da
aleclofabbro <alessandro.giansanti@gmail.com>
2022-08-19 11:11:50 +0200

    renamed  to "organization"

--- packages/organization/src/index.ts
+++ packages/organization/src/index.ts
@@ -1,23 +1,23 @@
-import type { CoreExt, Ext, ExtDef, SubTopo } from '@moodlenet/core';
-import { KeyValueStoreExtDef } from '@moodlenet/key-value-store';
-import type { ReactAppExt } from '@moodlenet/react-app';
-import { resolve } from 'path';
+import type { CoreExt, Ext, ExtDef, SubTopo } from '@moodlenet/core'
+import { KeyValueStoreExtDef } from '@moodlenet/key-value-store'
+import type { ReactAppExt } from '@moodlenet/react-app'
+import { resolve } from 'path'
 
 /* const stmpServer = sibTransport({
   apiKey: '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',
 }) */
 
 interface Organization {
-  name:string;
-  title:string;
-  subtitle:string;
+  name: string
+  title: string
+  subtitle: string
 }
 
 const configLocal = {
   service: 'SendinBlue', // no need to set host or port etc.
   auth: {
     user: 'shukeenkel@gmail.com',
-    pass: 'MTF0wXL7mrOVA4sQ',  
+    pass: 'MTF0wXL7mrOVA4sQ',
   },
 }
 
@@ -32,17 +32,17 @@ const configLocal = {
 */
 
 export type OrganizationData = ExtDef<
-  '@moodlenet/organization-data',
+  '@moodlenet/organization',
   '0.1.0',
   void,
   {
-    set: SubTopo<{ payload: Organization }, { valid: true} | { valid: false }>
-    get:SubTopo<{}, { valid: true, data:Organization} | { valid: false }>
+    set: SubTopo<{ payload: Organization }, { valid: true } | { valid: false }>
+    get: SubTopo<{}, { valid: true; data: Organization } | { valid: false }>
   }
 >
 
 const ext: Ext<OrganizationData, [CoreExt, ReactAppExt, KeyValueStoreExtDef]> = {
-  name: '@moodlenet/organization-data',
+  name: '@moodlenet/organization',
   version: '0.1.0',
   requires: ['@moodlenet/core@0.1.0', '@moodlenet/react-app@0.1.0', '@moodlenet/key-value-store@0.1.0'],
   async connect(shell) {
@@ -61,12 +61,12 @@ const ext: Ext<OrganizationData, [CoreExt, ReactAppExt, KeyValueStoreExtDef]> = 
         reactApp.plug.setup({
           routes: {
             moduleLoc: resolve(__dirname, '..', 'src', 'webapp', 'Router.tsx'),
-            // rootPath: 'organization-data', // http://localhost:3000/my-test
+            // rootPath: 'organization', // http://localhost:3000/my-test
           },
         })
 
         shell.expose({
-          // http://localhost:8080/_/_/raw-sub/moodlenet-organization-data/0.1.10/_test  body:{"paramIn2": "33"}
+          // http://localhost:8080/_/_/raw-sub/moodlenet-organization/0.1.10/_test  body:{"paramIn2": "33"}
           'get/sub': {
             validate(/* data */) {
               return { valid: true }
@@ -80,7 +80,7 @@ const ext: Ext<OrganizationData, [CoreExt, ReactAppExt, KeyValueStoreExtDef]> = 
         })
 
         shell.provide.services({
-          async set({payload}) {
+          async set({ payload }) {
             kvStore.set('organization', '', { privateKey, publicKey })
             return msg
           },
