235c8f11c9f40c0643ce8f44f82e5867f3e244eb
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-08-19 10:11:39 +0200

    plugin organization

--- packages/organization-data/src/index.ts
+++ packages/organization-data/src/index.ts
@@ -1,17 +1,23 @@
-import type { CoreExt, Ext, ExtDef, SubTopo } from '@moodlenet/core'
-import type { ReactAppExt } from '@moodlenet/react-app'
-import { resolve } from 'path'
-import { getNodemailerSendEmailAdapter, SendResp } from './emailSender/nodemailer/nodemailer'
+import type { CoreExt, Ext, ExtDef, SubTopo } from '@moodlenet/core';
+import { KeyValueStoreExtDef } from '@moodlenet/key-value-store';
+import type { ReactAppExt } from '@moodlenet/react-app';
+import { resolve } from 'path';
 
 /* const stmpServer = sibTransport({
   apiKey: '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',
 }) */
 
+interface Organization {
+  name:string;
+  title:string;
+  subtitle:string;
+}
+
 const configLocal = {
   service: 'SendinBlue', // no need to set host or port etc.
   auth: {
     user: 'shukeenkel@gmail.com',
-    pass: 'MTF0wXL7mrOVA4sQ',
+    pass: 'MTF0wXL7mrOVA4sQ',  
   },
 }
 
@@ -25,26 +31,26 @@ const configLocal = {
 }
 */
 
-export type EmailService = ExtDef<
+export type OrganizationData = ExtDef<
   '@moodlenet/organization-data',
   '0.1.0',
   void,
   {
-    send: SubTopo<{ paramIn1: string }, SendResp>
+    set: SubTopo<{ payload: Organization }, { valid: true} | { valid: false }>
+    get:SubTopo<{}, { valid: true, data:Organization} | { valid: false }>
   }
 >
 
-const ext: Ext<EmailService, [CoreExt, ReactAppExt]> = {
+const ext: Ext<OrganizationData, [CoreExt, ReactAppExt, KeyValueStoreExtDef]> = {
   name: '@moodlenet/organization-data',
   version: '0.1.0',
-  requires: ['@moodlenet/core@0.1.0', '@moodlenet/react-app@0.1.0'],
-  connect(shell) {
-    const [, reactApp] = shell.deps
-
+  requires: ['@moodlenet/core@0.1.0', '@moodlenet/react-app@0.1.0', '@moodlenet/key-value-store@0.1.0'],
+  async connect(shell) {
+    const [, reactApp, kvStore] = shell.deps
+    const kvtore = await kvStore.plug.getStore<{ organitation: Organization }>()
     return {
       deploy() {
         // come lo passo nel codice ?
-        const mailer = getNodemailerSendEmailAdapter(configLocal)
         // business logic, wire-up to the message system,
         // other packages integration
         //   listen to messages -> send other messages
@@ -61,7 +67,12 @@ const ext: Ext<EmailService, [CoreExt, ReactAppExt]> = {
 
         shell.expose({
           // http://localhost:8080/_/_/raw-sub/moodlenet-organization-data/0.1.10/_test  body:{"paramIn2": "33"}
-          'send/sub': {
+          'get/sub': {
+            validate(/* data */) {
+              return { valid: true }
+            },
+          },
+          'set/sub': {
             validate(/* data */) {
               return { valid: true }
             },
@@ -69,15 +80,9 @@ const ext: Ext<EmailService, [CoreExt, ReactAppExt]> = {
         })
 
         shell.provide.services({
-          async send(/* {paramIn1},msg */) {
-            const msg = {
-              from: 'shukeenkel@gmail.com',
-              to: 'ettorebevilacqua@gmail.com',
-              subject: 'subject text ',
-              html: '<h3>Hy test</h3>',
-            }
-            const resp = await mailer(msg)
-            return resp
+          async set({payload}) {
+            kvStore.set('organization', '', { privateKey, publicKey })
+            return msg
           },
         })
         return {}
