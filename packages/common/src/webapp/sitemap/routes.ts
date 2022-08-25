b304f52816867d55e570f33f92527c19e2b81580
aleclofabbro <alessandro.giansanti@gmail.com>
2022-08-25 16:59:58 +0200

    parametrized followers page

--- packages/common/src/webapp/sitemap/routes.ts
+++ packages/common/src/webapp/sitemap/routes.ts
@@ -11,12 +11,16 @@ export type GlobalSearch = RouteDef<'/search', EmptyParams>
 export type CreateNewResource = RouteDef<'/create-new-resource', EmptyParams>
 export type CreateNewCollection = RouteDef<'/create-new-collection', EmptyParams>
 export type BookmarksPage = RouteDef<'/bookmarks', EmptyParams>
-export type FollowersPage = RouteDef<'/followers', EmptyParams>
-export type FollowingPage = RouteDef<'/following', EmptyParams>
 export type SettingsPage = RouteDef<'/settings', EmptyParams>
 export type LMSMoodleLanding = RouteDef<'/lms/moodle/search', EmptyParams>
 export type CookiesPolicy = RouteDef<'/cookies-policy', EmptyParams>
 export type UserAgreement = RouteDef<'/user-agreement', EmptyParams>
+
+// export type FollowersPage = RouteDef<'/followers', EmptyParams>
+// export type FollowersPage = RouteDef<'/followers/:typename/:slug', { typename: string; slug: string }>
+export type FollowersPage = RouteDef<'/followers/:__typename/:slug', { __typename: string; slug: string }>
+// export type FollowingPage = RouteDef<'/following/:typename/:slug', { typename: string; slug: string }>
+export type FollowingPage = RouteDef<'/following', EmptyParams>
 
 // node home pages
 export type ContentNodeHomePage = RouteDef<`/${string}/:slug`, { slug: string }>
