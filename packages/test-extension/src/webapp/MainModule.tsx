bc39ee0479dc0507c8e991d7bad16be6cee15791
aleclofabbro <alessandro.giansanti@gmail.com>
2022-09-09 23:42:25 +0200

    http adapters (BE/FE) better error mngmnt

--- packages/test-extension/src/webapp/MainModule.tsx
+++ packages/test-extension/src/webapp/MainModule.tsx
@@ -13,7 +13,8 @@ const mainModule: TestExtensionWebappPlugin = {
       .then(resp => {
         console.log('testSub resp', JSON.stringify(resp, null, 2))
       })
-
+    http.fetch('testEmpty')().then(console.log.bind(null, 'testEmpty'), console.error.bind(null, 'testEmpty'))
+    http.fetch('testErr')().then(console.log.bind(null, 'testErr'), console.error.bind(null, 'testErr'))
     return {
       pkgLibFor(_) {
         return { a: 1 }
