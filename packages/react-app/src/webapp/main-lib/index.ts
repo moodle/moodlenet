623fb3621f787a7878244167a3f47bb0e155a9c6
aleclofabbro <alessandro.giansanti@gmail.com>
2022-09-13 15:54:29 +0200

    Login & Signup Registries

--- packages/react-app/src/webapp/main-lib/index.ts
+++ packages/react-app/src/webapp/main-lib/index.ts
@@ -1,7 +1,7 @@
 import * as ui from '../ui'
 import defaultAvatar from '../ui/assets/img/default-avatar.svg'
 import { ContentGraphContext } from '../ui/components/pages/ContentGraph/ContentGraphProvider'
-import * as auth from './auth'
+import { AuthCtx } from './auth'
 import priHttp from './pri-http'
 
 export type MainLib = typeof lib
@@ -9,7 +9,7 @@ export type MainLib = typeof lib
 const lib = {
   priHttp,
   ui,
-  auth,
+  AuthCtx,
   contentGraph: { Context: ContentGraphContext },
   defaultAvatar,
 }
