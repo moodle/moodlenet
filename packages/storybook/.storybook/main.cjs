c716d2bd67a73a325f36169ad34c89a74c4fccfd
Bru Mas Ribera <bru.mas@moodle.com>
2022-10-10 17:36:29 +0200

    Try to fix Vite svg error

--- packages/storybook/.storybook/main.cjs
+++ packages/storybook/.storybook/main.cjs
@@ -1,3 +1,7 @@
+const react = require('@vitejs/plugin-react').default
+const svgr = require('vite-plugin-svgr')
+const { mergeConfig } = require('vite')
+
 module.exports = {
   stories: [
     '../../component-library/src/webapp/ui/components/**/*.stories.tsx',
@@ -7,12 +11,25 @@ module.exports = {
     // '../src/**/*.stories.@(js|jsx|ts|tsx)',
     // '../../*/src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
   },
   features: {
     storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      plugins: [react(), svgr()],
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    })
   },
 }
