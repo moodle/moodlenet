8305bd5cf56fa46ae55eadc2fcf05cacfcec6f64
Bru Mas Ribera <brumasribera@gmail.com>
2022-10-10 17:36:29 +0200

    Try to fix Vite svg error

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -1,12 +1,11 @@
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
-import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite'
+
+console.log('sono da vite config')
+process.exit()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({include: '../**/*.svg'})],
-  resolve: {
-    alias: {"@/Components": path.resolve(__dirname, "../src/components")},
-   },
+  plugins: [react(), svgr()],
 })
