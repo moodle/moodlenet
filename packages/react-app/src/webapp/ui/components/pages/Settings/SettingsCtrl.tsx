c5aaae66c3f6127961983902da51d1878b208cae
aleclofabbro <alessandro.giansanti@gmail.com>
2022-10-11 17:37:59 +0200

    some guidelines: - Static Settings Page + Controller - mainColumnContent on ProfilePageProps

---
+++ packages/react-app/src/webapp/ui/components/pages/Settings/SettingsCtrl.tsx
@@ -0,0 +1,34 @@
+import { FC, useContext, useMemo, useState } from 'react'
+import { Card } from '../../atoms/Card/Card.js'
+import MainLayout from '../../layout/MainLayout/MainLayout.js'
+// import { Link } from '../../../../elements/link'
+// import { RegistryEntry } from '../../../../main-lib/registry'
+import { MainContext } from '../../../../MainContext.js'
+import { registries } from '../../../../web-lib.mjs'
+import { RegistryEntry } from '../../../../web-lib/registry.js'
+import Appearance from './Appearance.js'
+import { GeneralContent } from './General.js'
+import './Settings.scss'
+import { SettingsSectionItem } from './SettingsContext.js'
+import { Settings, SettingsItem, SettingsProps } from './Settings.js'
+
+export const SettingsCtrl: FC = () => {
+  const { pkgId } = useContext(MainContext)
+  const { registry: sectionsReg } = registries.settingsSections.useRegistry()
+  const settingsItems = useMemo(() => {
+    const baseSettingsItems: RegistryEntry<SettingsSectionItem>[] = [
+      { pkgId, item: { Menu: () => <span>General</span>, Content: GeneralContent } },
+      { pkgId, item: { Menu: () => <span>Appearance</span>, Content: Appearance } },
+      // { def: { Menu: () => <span>Extensions</span>, Content: () => <Navigate to={'/extensions'} /> } },
+    ]
+    return baseSettingsItems
+      .concat(sectionsReg.entries)
+      .map<SettingsItem>(({ item: { Content, Menu }, pkgId }) => ({
+        key: pkgId.name,
+        Menu,
+        Panel: Content,
+      }))
+  }, [pkgId, sectionsReg.entries])
+
+  return <Settings settingsItems={settingsItems} />
+}
