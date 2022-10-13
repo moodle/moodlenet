373824541c661d2cf509e3bf84545bb78789efc9
aleclofabbro <alessandro.giansanti@gmail.com>
2022-10-13 17:37:07 +0200

    try this Bru !

--- packages/component-library/src/webapp/ui/components/atoms/Card/Card.stories.tsx
+++ packages/component-library/src/webapp/ui/components/atoms/Card/Card.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
 import { Card, CardProps } from './Card.js'
 
 const meta: ComponentMeta<typeof Card> = {
--- packages/storybook/.storybook/main.cjs
+++ packages/storybook/.storybook/main.cjs
@@ -2,10 +2,15 @@ const react = require('@vitejs/plugin-react').default
 const svgr = require('vite-plugin-svgr')
 const { mergeConfig } = require('vite')
 
+
 module.exports = {
   stories: [
+    //'../src/stories/*.stories.tsx',
+    // '/home/alec/MOODLENET/repo/moodlenet3/packages/storybook/src/stories/*CL*.stories.tsx',
+    // './src/stories/CL.stories.tsx',
+    // '../../component-library/src/webapp/ui/components/atoms/Card/Card.stories.tsx',
     '../../component-library/src/webapp/ui/components/**/*.stories.tsx',
-    '../../web-user/src/webapp/**/*.stories.tsx',
+    // '../../web-user/src/webapp/**/*.stories.tsx',
     // '../../component-library/src/webapp/ui/components/organisms/Header/HeaderTitle/HeaderTitle.stories.tsx',
     // '../src/**/*.stories.mdx',
     // '../src/**/*.stories.@(js|jsx|ts|tsx)',
@@ -26,10 +31,10 @@ module.exports = {
   async viteFinal(config) {
     // Merge custom configuration into the default config
     return mergeConfig(config, {
-      plugins: [react(), svgr()],
-      optimizeDeps: {
-        include: ['storybook-dark-mode'],
-      },
+      plugins: [/* react(), */ svgr()],
+      // optimizeDeps: {
+      //   include: ['storybook-dark-mode'],
+      // },
     })
   },
 }
