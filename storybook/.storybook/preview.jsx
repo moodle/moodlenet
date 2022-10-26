dc5fd9819f65c04483e9e3753b5bf990a81e8bf7
Bru Mas Ribera <bru.mas@moodle.com>
2022-10-26 11:40:19 +0200

    Improve storybook appearance

--- storybook/.storybook/preview.jsx
+++ storybook/.storybook/preview.jsx
@@ -4,7 +4,24 @@ import { Wrapper } from '../src/Wrapper'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    storySort: {
+      order: [
+        'Atoms',
+        'Molecules',
+        'Organisms',
+        'Templates',
+        'Pages',
+        ['Access', 'Landing', 'Profile', 'Settings'],
+        'Emails',
+      ],
+    },
+    sidebar: {
+      collapsedRoots: ['atoms', 'molecules', 'organisms', 'templates', 'pages', 'emails'],
+    },
+  },
   controls: {
+    expanded: false,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
