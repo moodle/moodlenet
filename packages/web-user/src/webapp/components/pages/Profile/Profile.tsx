714458c8077308d5e2a1a1ba9571cb3c81e605a1
Bru Mas Ribera <bru.mas@moodle.com>
2022-10-28 12:06:31 +0200

    Add global styles Storybook and message modal

--- packages/web-user/src/webapp/components/pages/Profile/Profile.tsx
+++ packages/web-user/src/webapp/components/pages/Profile/Profile.tsx
@@ -1,5 +1,6 @@
+import { InputTextField, Modal, PrimaryButton } from '@moodlenet/component-library'
 import { MainLayout, MainLayoutProps } from '@moodlenet/react-app/ui.mjs'
-import { ComponentType, FC } from 'react'
+import { ComponentType, FC, useState } from 'react'
 import { OverallCard, OverallCardProps } from '../../molecules/OverallCard/OverallCard.js'
 import { ProfileCard, ProfileCardProps } from '../../organisms/ProfileCard/ProfileCard.js'
 import './Profile.scss'
@@ -9,6 +10,7 @@ export type ProfileProps = {
   overallCardProps: OverallCardProps
   profileCardProps: ProfileCardProps
   mainColumnContent?: { Comp: ComponentType<{ callback(): void }>; key: string; callback(): void }[]
+  displayName: string
   // mainColumnContent?: { Comp: ComponentType; key: string }[]
 }
 
@@ -17,13 +19,49 @@ export const Profile: FC<ProfileProps> = ({
   overallCardProps,
   profileCardProps,
   mainColumnContent,
+  displayName,
 }) => {
+  const [isSendingMessage, setIsSendingMessage] = useState<boolean>(false)
+
+  const modals = [
+    isSendingMessage /* && sendEmailForm  */ && (
+      <Modal
+        title={`${/* t */ `Send a message to`} ${displayName}`}
+        actions={
+          <PrimaryButton
+            onClick={() => {
+              // sendEmailForm.submitForm()
+              setIsSendingMessage(false)
+            }}
+          >
+            {/* <Trans> */}
+            Send
+            {/* </Trans> */}
+          </PrimaryButton>
+        }
+        onClose={() => setIsSendingMessage(false)}
+        style={{ maxWidth: '400px' }}
+      >
+        <InputTextField
+          textarea={true}
+          name="text"
+          edit
+          // onChange={sendEmailForm.handleChange}
+        />
+      </Modal>
+    ),
+  ]
+
   return (
     <MainLayout {...mainLayoutProps}>
+      {modals}
       <div className="profile">
         <div className="content">
           <div className="main-column">
-            <ProfileCard {...profileCardProps} />
+            <ProfileCard
+              {...profileCardProps}
+              openSendMessage={() => setIsSendingMessage(/* !!sendEmailForm */ true)}
+            />
             {mainColumnContent?.map(({ Comp, key, callback }) => {
               return <Comp key={key} callback={callback} />
             })}
