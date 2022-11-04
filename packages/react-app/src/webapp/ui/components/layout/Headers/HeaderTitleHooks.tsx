79bbdec38840753e3afd117d81f0c8185fbe026f
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-11-04 11:07:52 +0100

    Implemente hook HeaderTitle

--- packages/react-app/src/webapp/ui/components/layout/Headers/HeaderTitleHooks.tsx
+++ packages/react-app/src/webapp/ui/components/layout/Headers/HeaderTitleHooks.tsx
@@ -1,12 +1,17 @@
 import { HeaderTitleProps } from '@moodlenet/component-library'
+import { useContext } from 'react'
+import { SettingsCtx } from '../../pages/Settings/SettingsContext.js'
 
 export const useHeaderTitleProps = (): HeaderTitleProps => {
+  const { organizationData } = useContext(SettingsCtx)
+
+  const { logo, smallLogo } = organizationData
   // usa i server
   // usa i context
   // ritorna HeaderTitleProps
   return {
     logo,
     smallLogo,
-    url,
+    url: '/',
   }
 }
