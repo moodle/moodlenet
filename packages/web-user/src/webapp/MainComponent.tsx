e7dc7de24c7ad4dc116dd71372c7a66d4ec44504
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-11-15 17:36:41 +0100

    profile and profileCard hook Fix avatarMenu

--- packages/web-user/src/webapp/MainComponent.tsx
+++ packages/web-user/src/webapp/MainComponent.tsx
@@ -1,3 +1,4 @@
+import { HeaderMenuItemRegItem } from '@moodlenet/react-app/ui'
 import { ReactAppMainComponent, registries } from '@moodlenet/react-app/web-lib'
 import { useMemo } from 'react'
 import * as avatarmenuItem from './components/organisms/Header/Header.js'
@@ -5,8 +6,13 @@ import { MainContext } from './MainContext.js'
 import Router from './Router.js'
 import { MainContextT, WebPkgDeps } from './types.mjs'
 
+const avatarmenuItemReg: HeaderMenuItemRegItem = {
+  Icon: <>{avatarmenuItem.Icon}</>,
+  Text: '',
+  Path: { url: '/', ext: true },
+}
 const MainComponent: ReactAppMainComponent<WebPkgDeps> = ({ pkgs, pkgId, children }) => {
-  registries.avatarMenuItems.useRegister(pkgId, avatarmenuItem)
+  registries.avatarMenuItems.useRegister(pkgId, avatarmenuItemReg)
   registries.routes.useRegister(pkgId, Router)
 
   const mainContext = useMemo<MainContextT>(() => {
