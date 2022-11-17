91dceca94e28a0aa95b41567a99eeef167aea6f8
aleclofabbro <alessandro.giansanti@gmail.com>
2022-11-17 17:17:44 +0100

    remove forever

--- bin/env.mjs
+++ bin/env.mjs
@@ -1,20 +1,8 @@
 import yargs from 'yargs'
 import { resolve } from 'path'
 
-const opts = yargs(process.argv.slice(2)).argv
-const [instdirname] = opts._
-
+export const opts = yargs(process.argv.slice(2)).argv
+const [instdirname, ...rest_args] = opts._
+export const args = rest_args.map(String)
 export const moodlenetDevDir = resolve(process.cwd(), `.dev-machines`, String(instdirname))
-export const clean = !!opts.clean
-export const registry = opts.registry ? String(opts.registry) : undefined
-export const useRegistry = opts['use-reg'] === true
-
 process.env.NODE_ENV = 'development'
-registry && (process.env.npm_config_registry = registry)
-
-// console.log({
-//   moodlenetDevDir,
-//   clean,
-//   registry,
-//   useRegistry,
-// })
--- packages/core/src/main/sys.mts
+++ packages/core/src/main/sys.mts
@@ -1,3 +1,3 @@
 export function rebootSystem(to = 500) {
-  setTimeout(() => process.exit(0), 500)
+  setTimeout(() => process.exit(0), to)
 }
