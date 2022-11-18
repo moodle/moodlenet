8e9b81f7483911f1c6a5a5847e7ee221e148d164
aleclofabbro <alessandro.giansanti@gmail.com>
2022-11-18 09:54:54 +0100

    with context, api call and link

--- packages/my-moodlenet-pkg-template/lib/webapp/HelloWorldPage.jsx
+++ packages/my-moodlenet-pkg-template/lib/webapp/HelloWorldPage.jsx
@@ -1,9 +1,16 @@
 import { SimpleLayoutContainer } from '@moodlenet/react-app/ui'
+import { useContext } from 'react'
+import { MyContext } from './Context'
 
 const HelloWorldPage = () => {
+  const { apiResponse } = useContext(MyContext)
   return (
     <SimpleLayoutContainer>
       <h1>Hello World</h1>
+      <pre>
+        apiResponse:
+        {JSON.stringify(apiResponse, null, 4)}
+      </pre>
     </SimpleLayoutContainer>
   )
 }
