46462a5933b8099bb758d2e69c3107620041c380
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-11-18 07:12:00 +0100

    extension hook

--- packages/extensions-manager/src/webapp/components/pages/InstallExtension/InstallExtensionHook.tsx
+++ packages/extensions-manager/src/webapp/components/pages/InstallExtension/InstallExtensionHook.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import { SearchPackagesResObject } from '../../../../types.mjs'
 import { MainContext } from '../../../MainContext.js'
-import { ExtensionType } from './InstallExtension.js'
+import { ExtensionType } from '../Extensions/Extensions.js'
 
 // export type ExtensionType = {
 //   name: string
@@ -17,11 +17,14 @@ const approvedExtensions = [
   '@moodlenet/arangodb',
 ]
 
+const pkgName = '@moodlenet/ce-platform'
+
 export const useFilterExtensionList = (
   rawExtensions: SearchPackagesResObject[],
 ): ExtensionType[] => {
   const { defaultRegistry } = useContext(MainContext)
   const [readme, setReadme] = useState('')
+  const [toggleInstallingUninstalling, setToggleInstallingUninstalling] = useState(false)
   useEffect(() => {
     //fetch(`https://registry.npmjs.org/@moodlenet/ce-platform`, {
     fetch(`${defaultRegistry}/${pkgName}`, {
@@ -29,7 +32,11 @@ export const useFilterExtensionList = (
     })
       .then(_ => _.json())
       .then(({ readme }) => setReadme(readme))
-  }, [])
+  }, [defaultRegistry])
+
+  const _toggleInstallingUninstalling = () =>
+    setToggleInstallingUninstalling(!toggleInstallingUninstalling)
+
   return rawExtensions
     .filter(rawExtension => approvedExtensions.includes(rawExtension.pkgName))
     .map(rawExtension => {
@@ -38,6 +45,11 @@ export const useFilterExtensionList = (
         description: rawExtension.description,
         readme: readme,
         installed: rawExtension.installed,
+        toggleInstallingUninstalling: _toggleInstallingUninstalling,
+        displayName: rawExtension.pkgName,
+        isInstallingUninstalling: toggleInstallingUninstalling,
+        installUninstallSucces: true,
+        repositoryUrl: '',
       }
     })
 }
