ef3b1811a864d75f6817933ab29e602927d186c1
aleclofabbro <alessandro.giansanti@gmail.com>
2022-11-18 13:07:32 +0100

    using pm2

--- bin/boot.mjs
+++ bin/boot.mjs
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 import execa from 'execa'
-import { moodlenetDevDir, args } from './env.mjs'
+import { moodlenetDevDir, opts_arr } from './env.mjs'
 
-await execa('npm', ['start', '--', moodlenetDevDir, ...args], {
+console.log({ opts_arr })
+await execa('npm', ['start' /* moodlenetDevDir, */, '--', ...opts_arr], {
   cwd: moodlenetDevDir,
   stdout: process.stdout,
 })
--- bin/env.mjs
+++ bin/env.mjs
@@ -1,8 +1,14 @@
 import yargs from 'yargs'
 import { resolve } from 'path'
 
-export const opts = yargs(process.argv.slice(2)).argv
-const [instdirname, ...rest_args] = opts._
+export const yOpts = yargs(process.argv.slice(2)).argv
+console.log({ yOpts })
+export const { _, $0, ...restOpts } = yOpts
+const [instdirname, ...rest_args] = _
+export const opts = restOpts
+export const opts_arr = Object.entries(restOpts).reduce((_, [k, v]) => {
+  return v === true ? [..._, `--${k}`] : [..._, `--${k}`, `${v}`]
+}, [])
 export const args = rest_args.map(String)
 export const moodlenetDevDir = resolve(process.cwd(), `.dev-machines`, String(instdirname))
-process.env.NODE_ENV = 'development'
+// process.env.NODE_ENV = 'development'
