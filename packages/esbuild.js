64b0e4868b5b4fdd2c7a84b2046ab4fa76eaa1f5
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-11-28 20:55:27 +0100

    add esbuild compiling

--- packages/esbuild.js
+++ packages/esbuild.js
@@ -1,9 +1,9 @@
-// import * as pkg from './package.json'
 import { build } from 'esbuild'
 import { nodeExternalsPlugin } from 'esbuild-node-externals'
 import { readdir, readFile } from 'fs/promises'
 import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+import execa from 'execa'
+// import tsPaths from 'esbuild-ts-paths'
 import glob from 'glob'
 
 const pkg = JSON.parse(await readFile(resolve('./', 'package.json'), 'utf-8'))
@@ -18,27 +18,47 @@ alternative command :
 "build": "esbuild `find server \\( -name '*.ts' \\)` --platform=node --outdir=build/server",
 */
 
-const inject = glob.sync('src/*.mts')
-
 const buildDefault = {
   outdir: './dist/',
   minify: false,
   bundle: false,
   outbase: './src',
-  format: 'esm',
+  format: ['js', 'jsx', 'ts', 'tsx'].includes('js') ? undefined : 'esm',
   target: 'node16',
+  sourcemap: true,
+  outExtension: { '.js': '.mjs' },
   // external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [nodeExternalsPlugin()],
 }
+
+// build by tsconfig
+// build({ plugins: [tsPaths('../../tsconfig-base.json')] }).catch(() => process.exit(1))
 
 export default function builder(config) {
   console.log('package', pkg.name)
   console.time('timer')
   build({ ...buildDefault, ...{ outdir: './dist' }, ...config })
     .catch(() => process.exit(1))
-    .then(() => {
+    .then(res => {
       console.timeEnd('timer')
+      execa('tscx', ['--emitDeclarationOnly'], {
+        cwd: resolve(process.cwd(), ''),
+        stdout: process.stdout,
+      })
+        .then(ex => console.log('', ex.command))
+        .catch(error => console.log('error execa', error))
+      res.warnings && console.log('warning', res.warnings)
     })
 }
 
+const inject = glob.sync('src/**/*.mts')
 builder({ entryPoints: inject })
+
+/* can import css
+
+require('esbuild').buildSync({
+  entryPoints: ['app.css'],
+  bundle: true,
+  outfile: 'main.css',
+})
+*/
