68e764d14bd31916e5532dd2e323f5b863199a62
Bru Mas Ribera <bru.mas@moodle.com>
2023-01-18 10:18:53 +0100

    Setup resource package base

---
+++ packages/resource/src/common/types.mts
@@ -0,0 +1,9 @@
+export type ResourceFormValues = {
+  displayName: string
+  description: string
+  organizationName?: string
+  location?: string
+  siteUrl?: string
+  backgroundImage?: string | File | null
+  avatarImage?: string | File | null
+}
---
+++ packages/resource/src/webapp/components/pages/Resource/ResourcePageRoute.tsx
@@ -0,0 +1,9 @@
+// import { FC } from 'react'
+// import { useParams } from 'react-router-dom'
+// import { ResourceContainer } from './ResourceContainer.js'
+
+// export const ResourcePageRoute: FC = () => {
+//   const { key } = useParams()
+
+//   return <ResourceContainer resourceKey={key ?? ''} />
+// }
