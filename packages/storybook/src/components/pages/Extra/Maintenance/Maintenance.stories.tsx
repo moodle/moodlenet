bd9cce5ff9c63b5ea52d3b56cc56fefd6b4344d8
Bru Mas Ribera <bru.mas@moodle.com>
2023-01-28 10:27:54 +0100

    Fix visual details

--- packages/storybook/src/components/pages/Extra/Maintenance/Maintenance.stories.tsx
+++ packages/storybook/src/components/pages/Extra/Maintenance/Maintenance.stories.tsx
@@ -1,40 +1,40 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
-import {
-  Maintenance,
-  MaintenanceProps,
-} from '../../../../../../react-app/src/webapp/ui/components/pages/Extra/Maintenance/Maintenance.js'
-import { MainLayoutLoggedOutStoryProps } from '../../../layout/MainLayout/MainLayout.stories.js'
+// import { ComponentMeta, ComponentStory } from '@storybook/react'
+// import {
+//   Maintenance,
+//   MaintenanceProps,
+// } from '../../../../../../react-app/src/webapp/ui/components/pages/Extra/Maintenance/Maintenance.js'
+// import { MainLayoutLoggedOutStoryProps } from '../../../layout/MainLayout/MainLayout.stories.js'
 
-const meta: ComponentMeta<typeof Maintenance> = {
-  title: 'Pages/Extra/Maintenance',
-  component: Maintenance,
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
-  parameters: { layout: 'fullscreen' },
-  excludeStories: [
-    'MaintenanceStoryProps',
-    'MaintenanceMoodleStoryProps',
-    'MaintenanceOrganizationStoryProps',
-  ],
-}
+// const meta: ComponentMeta<typeof Maintenance> = {
+//   title: 'Pages/Extra/Maintenance',
+//   component: Maintenance,
+//   argTypes: {
+//     // backgroundColor: { control: 'color' },
+//   },
+//   parameters: { layout: 'fullscreen' },
+//   excludeStories: [
+//     'MaintenanceStoryProps',
+//     'MaintenanceMoodleStoryProps',
+//     'MaintenanceOrganizationStoryProps',
+//   ],
+// }
 
-const MaintenanceStory: ComponentStory<typeof Maintenance> = args => <Maintenance {...args} />
+// const MaintenanceStory: ComponentStory<typeof Maintenance> = args => <Maintenance {...args} />
 
-export const MaintenanceMoodleStoryProps: MaintenanceProps = {
-  mainLayout: MainLayoutLoggedOutStoryProps,
-  // headerPageTemplateProps: {
-  //   headerPageProps: HeaderPageLoggedOutStoryProps,
-  //   isAuthenticated: false,
-  //   mainPageWrapperProps: {
-  //     userAcceptsPolicies: null,
-  //     cookiesPolicyHref: href('Pages/Policies/CookiesPolicy/Default'),
-  //   },
-  // },
-  // headerTitleProps: HeaderTitleStoryProps,
-}
+// export const MaintenanceMoodleStoryProps: MaintenanceProps = {
+//   mainLayout: MainLayoutLoggedOutStoryProps,
+//   // headerPageTemplateProps: {
+//   //   headerPageProps: HeaderPageLoggedOutStoryProps,
+//   //   isAuthenticated: false,
+//   //   mainPageWrapperProps: {
+//   //     userAcceptsPolicies: null,
+//   //     cookiesPolicyHref: href('Pages/Policies/CookiesPolicy/Default'),
+//   //   },
+//   // },
+//   // headerTitleProps: HeaderTitleStoryProps,
+// }
 
-export const Moodle = MaintenanceStory.bind({})
-Moodle.args = MaintenanceMoodleStoryProps
+// export const Moodle = MaintenanceStory.bind({})
+// Moodle.args = MaintenanceMoodleStoryProps
 
-export default meta
+// export default meta
