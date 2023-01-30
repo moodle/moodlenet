ddc85c7cbaa2cc31b37f229cfd4045b2eb4eeded
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-01-30 13:25:07 +0100

    Check is admin by route for setting and menu,

--- packages/react-app/src/webapp/ui/components/pages/Settings/Settings/Hook/SettingsContainer.tsx
+++ packages/react-app/src/webapp/ui/components/pages/Settings/Settings/Hook/SettingsContainer.tsx
@@ -1,15 +1,9 @@
-import { FC, useContext } from 'react'
-import { Navigate } from 'react-router-dom'
-import { AuthCtx } from '../../../../../../web-lib.mjs'
+import { FC } from 'react'
 import { Settings } from '../Settings.js'
 import { useSettingsProps } from './SettingsHooks.js'
 
 export const SettingsContainer: FC = () => {
   const settingsProps = useSettingsProps()
-  const { clientSessionData } = useContext(AuthCtx)
-  if (!clientSessionData?.myUserNode) {
-    return <Navigate to="/login" />
-  }
 
   return <Settings {...settingsProps} />
 }
