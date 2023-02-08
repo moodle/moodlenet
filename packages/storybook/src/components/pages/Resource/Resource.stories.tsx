8089dd75e399a9d646c07a551a74d5b0dd20b4f5
Bru Mas Ribera <brumasribera@gmail.com>
2023-02-08 10:12:00 +0100

    Improve main resource card

--- packages/storybook/src/components/pages/Resource/Resource.stories.tsx
+++ packages/storybook/src/components/pages/Resource/Resource.stories.tsx
@@ -19,6 +19,7 @@ const meta: ComponentMeta<typeof Resource> = {
     'ResourceActivatedStoryProps',
     'ResourceAdminStoryProps',
     'ResourceApprovedStoryProps',
+    'NewResourceProps',
   ],
 }
 
