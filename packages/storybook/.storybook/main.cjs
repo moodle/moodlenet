1fd4fe961ab5d70612b449028e6567db8e865322
Bru Mas Ribera <brumasribera@gmail.com>
2023-03-06 09:44:30 +0100

    Fix package.json dist

--- packages/storybook/.storybook/main.cjs
+++ packages/storybook/.storybook/main.cjs
@@ -9,6 +9,28 @@ const path = require('path')
 const packagesDirs = readdirSync(path.join('..')).map(pkg_name => path.join('..', pkg_name))
 hackPackageJsonExports()
 
+const recoverHackSignals = [
+  'SIGTERM',
+  'SIGINT',
+  'SIGHUP',
+  'SIGBREAK',
+  // 'SIGKILL',
+  // 'SIGSTOP',
+  'SIGBUS',
+  'SIGFPE',
+  'SIGSEGV',
+  'SIGILL',
+  'exit',
+  'beforeExit',
+  'abort',
+  'unhandledRejection',
+  'uncaughtException',
+  'disconnect',
+]
+recoverHackSignals.forEach(evname => {
+  process.once(evname, () => hackPackageJsonExports(true))
+})
+
 module.exports = {
   stories: [
     '../src/components/**/*.stories.tsx',
@@ -43,25 +65,3 @@ module.exports = {
     })
   },
 }
-
-const recoverHackSignals = [
-  'SIGTERM',
-  'SIGINT',
-  'SIGHUP',
-  'SIGBREAK',
-  // 'SIGKILL',
-  // 'SIGSTOP',
-  'SIGBUS',
-  'SIGFPE',
-  'SIGSEGV',
-  'SIGILL',
-  'exit',
-  'beforeExit',
-  'abort',
-  'unhandledRejection',
-  'uncaughtException',
-  'disconnect',
-]
-recoverHackSignals.forEach(evname => {
-  process.once(evname, () => hackPackageJsonExports(true))
-})
