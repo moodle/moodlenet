a83abab0adae8a3b94fa79573171646ae2a7c66e
Bru Mas Ribera <brumasribera@gmail.com>
2023-03-14 14:34:02 +0100

    Fixed Browser reloading problem

--- packages/react-app/src/webapp/ui/components/pages/Search/Search.tsx
+++ packages/react-app/src/webapp/ui/components/pages/Search/Search.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import MainLayout, { MainLayoutProps } from '../../layout/MainLayout/MainLayout.js'
-import { Browser, BrowserProps } from '../../organisms/Browser/Browser.js'
+import Browser, { BrowserProps } from '../../organisms/Browser/Browser.js'
 import './Search.scss'
 
 export type SearchProps = {
@@ -8,6 +8,9 @@ export type SearchProps = {
   browserProps: BrowserProps
 }
 export const Search: FC<SearchProps> = ({ mainLayoutProps, browserProps }) => {
+  // const props = useMemo(() => browserProps, [browserProps])
+  // console.log('browserProps', props)
+
   return (
     <MainLayout {...mainLayoutProps}>
       <div className="search">
