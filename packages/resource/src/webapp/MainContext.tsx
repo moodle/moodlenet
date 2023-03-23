64bff8716168ef450975d813c9dbe86753ffd167
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-03-23 16:41:53 +0100

    fix type

--- packages/resource/src/webapp/MainContext.tsx
+++ packages/resource/src/webapp/MainContext.tsx
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
-import { MainContextResourceType } from './common/types.mjs'
+import { MainContextResourceType } from '../common/types.mjs'
 
 export const MainContext = createContext<MainContextResourceType>(null as any)
