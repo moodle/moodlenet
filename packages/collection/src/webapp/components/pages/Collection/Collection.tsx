930277c545f1d077cf52cad07602badbc1bb5003
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-03-23 19:52:41 +0100

    some fix

--- packages/collection/src/webapp/components/pages/Collection/Collection.tsx
+++ packages/collection/src/webapp/components/pages/Collection/Collection.tsx
@@ -9,6 +9,8 @@ import { MainLayout, MainLayoutProps } from '@moodlenet/react-app/ui'
 import { useFormik } from 'formik'
 import { FC, useState } from 'react'
 import { SchemaOf } from 'yup'
+import { ResourceCard, ResourceCardProps } from '../../../../../../resource/dist/ui.mjs'
+
 import {
   CollectionAccess,
   CollectionActions,
