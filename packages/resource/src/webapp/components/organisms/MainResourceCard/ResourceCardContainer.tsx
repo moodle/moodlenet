029940c9be7863515b30cffb24626496d988bdcf
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-03-23 16:16:32 +0100

    respurce fix

--- packages/resource/src/webapp/components/organisms/MainResourceCard/ResourceCardContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-import MainResourceCard from './MainResourceCard.js'
-import { useResourceCardProps } from './ResourceCardHook.js'
-
-export const ResourceCardContainer: FC<{ resourceKey: string }> = ({ resourceKey }) => {
-  const panelProps = useResourceCardProps({ resourceKey })
-
-  return panelProps && <MainResourceCard {...panelProps} />
-}
--- packages/resource/src/webapp/components/pages/Resource/ResourcePageHooks.tsx
+++ packages/resource/src/webapp/components/pages/Resource/ResourcePageHooks.tsx
@@ -2,10 +2,9 @@ import { OptionItemProp } from '@moodlenet/component-library'
 import { useMainLayoutProps } from '@moodlenet/react-app/ui'
 import { useMemo } from 'react'
 import { maxUploadSize, ResourceFormValues } from '../../../../common.mjs'
+import { validationSchema } from '../../../../common/validationSchema.mjs'
 import { useResourceBaseProps } from '../../../../ResourceHooks.js'
 import { ResourceContributorCardProps } from '../../molecules/ResourceContributorCard/ResourceContributorCard.js'
-import { useResourceCardProps } from '../../organisms/MainResourceCard/ResourceCardHook.js'
-import { validationSchema } from '../../organisms/MainResourceCard/resourceForm.js'
 import { ResourceProps } from './Resource.js'
 
 export const collectionTextOptionProps: OptionItemProp[] = [
@@ -18,20 +17,17 @@ export const collectionTextOptionProps: OptionItemProp[] = [
   { label: 'English Literature', value: 'English Literature' },
 ]
 
-const log = (str: string, val?: any) => console.log(str, val)
-
 export const useResourcePageProps = ({
   resourceKey,
 }: {
   resourceKey: string
   overrides?: Partial<ResourceFormValues>
 }): ResourceProps | null => {
-  const mainResourceCardProps = useResourceCardProps({ resourceKey })
   const mainLayoutProps = useMainLayoutProps()
   const _baseProps = useResourceBaseProps({ resourceKey })
 
   const props = useMemo<ResourceProps | null>((): ResourceProps | null => {
-    if (!mainResourceCardProps || !_baseProps) return null
+    if (!_baseProps) return null
     const {
       actions,
       props: { data, resourceForm, state, authFlags: access },
@@ -68,7 +64,7 @@ export const useResourcePageProps = ({
       access,
       fileMaxSize: maxUploadSize,
     }
-  }, [_baseProps, mainLayoutProps, mainResourceCardProps])
+  }, [_baseProps, mainLayoutProps])
 
   return props
 }
