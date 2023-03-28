ba07cf3c2415d7ca4e4e655f0f5a31c81e6fb52b
aleclofabbro <alessandro.giansanti@gmail.com>
2023-03-28 15:38:53 +0200

    webapp canonical routes redirect to custom base route

--- packages/react-app/src/webapp/app-routes.tsx
+++ packages/react-app/src/webapp/app-routes.tsx
@@ -1,5 +1,5 @@
-import { ReactElement, useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { FC, ReactElement, useContext, useEffect } from 'react'
+import { redirect, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import { MainContext } from './context/MainContext.mjs'
 import LoginPageContainer from './ui/components/pages/Access/Login/LoginPageContainer.js'
 import { RootLoginContainer } from './ui/components/pages/Access/RootLogin/RootLoginContainer.js'
@@ -9,6 +9,7 @@ import { LandingContainer } from './ui/components/pages/Landing/LandingContainer
 import { MyProfilePageRoute } from './ui/components/pages/Profile/MyProfilePageRoute.js'
 import { ProfilePageRoute } from './ui/components/pages/Profile/ProfilePageRoute.js'
 import { SettingsPageRoute } from './ui/components/pages/Settings/Settings/Hook/SettingsPageRoute.js'
+import { RegistryEntry } from './web-lib/registry.js'
 
 export type RouteRegItem = { routes: ReactElement; rootPath?: string }
 
@@ -18,36 +19,64 @@ const AppRouter = () => {
   } = useContext(MainContext)
   // console.log({ routesRegistry })
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingContainer />} />
-        {/* <Route path={nodeHomePage.route} element={<nodeHomePage.Component />} /> */}
-        <Route path="settings" element={<SettingsPageRoute />} />
-        <Route path="login">
-          <Route index element={<LoginPageContainer />} />
-          <Route path="root" element={<RootLoginContainer />} />
-        </Route>
-        <Route path="signup">
-          <Route index element={<SignUpContainer />} />
-        </Route>
-        <Route path="profile/:key/" element={<ProfilePageRoute />} />
-        <Route path="my-profile/" element={<MyProfilePageRoute />} />
-        {/* <Route
+    <Routes>
+      <Route path="/" element={<LandingContainer />} />
+      {/* <Route path={nodeHomePage.route} element={<nodeHomePage.Component />} /> */}
+      <Route path="settings" element={<SettingsPageRoute />} />
+      <Route path="login">
+        <Route index element={<LoginPageContainer />} />
+        <Route path="root" element={<RootLoginContainer />} />
+      </Route>
+      <Route path="signup">
+        <Route index element={<SignUpContainer />} />
+      </Route>
+      <Route path="profile/:key/" element={<ProfilePageRoute />} />
+      <Route path="my-profile/" element={<MyProfilePageRoute />} />
+      {/* <Route
       path="a"
       element={
           <A />
       }
     /> */}
-        {routes.registry.entries.map(({ pkgId, item: { routes, rootPath } }) => {
-          return (
-            <Route path={rootPath ?? pkgId.name} key={`${pkgId.name}`} caseSensitive>
+      {routes.registry.entries.map(entry => {
+        const {
+          pkgId,
+          item: { routes, rootPath },
+        } = entry
+        return rootPath ? (
+          <Route key={`custom-pkg-route:${pkgId.name}`}>
+            <Route path={rootPath} key={`custom-base-route:${pkgId.name}`} caseSensitive>
               {routes}
             </Route>
-          )
-        })}
-      </Routes>
-    </>
+            <Route
+              path={`/${pkgId.name}/*`}
+              key={`canonical-redirect:${pkgId.name}`}
+              caseSensitive
+              element={<RedirectToCustomRoute entry={entry} />}
+            />
+          </Route>
+        ) : (
+          <Route path={`/${pkgId.name}/`} key={`canonical:${pkgId.name}`} caseSensitive>
+            {routes}
+          </Route>
+        )
+      })}
+    </Routes>
   )
 }
 
 export default AppRouter
+
+const RedirectToCustomRoute: FC<{ entry: RegistryEntry<RouteRegItem> }> = ({ entry }) => {
+  const loc = useLocation()
+  const redirectString = loc.pathname.replace(
+    new RegExp(`^/${entry.pkgId.name}/`),
+    `${entry.item.rootPath}`,
+  )
+  const nav = useNavigate()
+  redirect(redirectString)
+  useEffect(() => {
+    nav(redirectString, { replace: true })
+  }, [nav, redirectString])
+  return null
+}
