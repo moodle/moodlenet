3ca4354507c2fa4d7d390f455638c606b40fc7d0
Bru Mas Ribera <brumasribera@gmail.com>
2023-03-29 13:23:50 +0200

    Style fixes on collection and resource buttons

--- packages/storybook/src/components/organisms/Browser/Browser.stories.tsx
+++ packages/storybook/src/components/organisms/Browser/Browser.stories.tsx
@@ -168,15 +168,57 @@ export const useBrowserLoggedOutStoryProps = (): BrowserProps => {
           )
         },
         filters: [
+          // {
+          //   name: 'Connections',
+          //   menuContent: ['1st', '2nd', '3rd+']
+          //     .map(e => <div key={e.toLocaleLowerCase()}>{e}</div>)
+          //     .map(e =>
+          //       getFilterContentDefaultListElement({ Item: () => e, key: e.key?.valueOf() ?? '' }),
+          //     ),
+          //   menuContentType: 'menu-content-default-list',
+          //   key: 'connections',
+          // },
           {
-            name: 'Connections',
-            menuContent: ['1st', '2nd', '3rd+']
+            name: 'Subjects',
+            menuContent: [
+              'Generic programmes and qualifications',
+              'Literacy and numeracy',
+              'Arts and humanities',
+              'Social sciences',
+              'Business and administration',
+              'Natural sciences',
+              'Engineering and technology',
+              'Agriculture, forestry and fisheries',
+              'Health and welfare',
+              'Personal skills and development',
+            ]
               .map(e => <div key={e.toLocaleLowerCase()}>{e}</div>)
               .map(e =>
                 getFilterContentDefaultListElement({ Item: () => e, key: e.key?.valueOf() ?? '' }),
               ),
             menuContentType: 'menu-content-default-list',
-            key: 'connections',
+            key: 'locations',
+          },
+          {
+            name: 'Entities',
+            menuContent: [
+              'Massachussets Institute of Technology',
+              'University of Cambridge',
+              'Stanford University',
+              'University of Oxford',
+              'Harvard University',
+              'California Institute of Technology',
+              'Imperial College London',
+              'University College London',
+              'ETH Zurich',
+              'Australian National University',
+            ]
+              .map(e => <div key={e.toLocaleLowerCase()}>{e}</div>)
+              .map(e =>
+                getFilterContentDefaultListElement({ Item: () => e, key: e.key?.valueOf() ?? '' }),
+              ),
+            menuContentType: 'menu-content-default-list',
+            key: 'locations',
           },
           {
             name: 'Locations',
@@ -191,27 +233,6 @@ export const useBrowserLoggedOutStoryProps = (): BrowserProps => {
               'Italy',
               'Spain',
               'Portugal',
-            ]
-              .map(e => <div key={e.toLocaleLowerCase()}>{e}</div>)
-              .map(e =>
-                getFilterContentDefaultListElement({ Item: () => e, key: e.key?.valueOf() ?? '' }),
-              ),
-            menuContentType: 'menu-content-default-list',
-            key: 'locations',
-          },
-          {
-            name: 'Entity',
-            menuContent: [
-              'Massachussets Institute of Technology',
-              'University of Cambridge',
-              'Stanford University',
-              'University of Oxford',
-              'Harvard University',
-              'California Institute of Technology',
-              'Imperial College London',
-              'University College London',
-              'ETH Zurich',
-              'Australian National University',
             ]
               .map(e => <div key={e.toLocaleLowerCase()}>{e}</div>)
               .map(e =>
