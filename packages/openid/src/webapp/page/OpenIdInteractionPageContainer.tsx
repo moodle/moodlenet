077c4d130636b747c5e6bc1c28a81d3da4d610de
aleclofabbro <alessandro.giansanti@gmail.com>
2023-03-29 16:18:36 +0200

    refactored openid interaction pag container/hook/comp better code and better use of useNeedsWebUserLogin()

--- packages/openid/src/webapp/page/OpenIdInteractionPageContainer.tsx
+++ packages/openid/src/webapp/page/OpenIdInteractionPageContainer.tsx
@@ -1,28 +1,19 @@
-import { useNeedsWebUserLogin } from '@moodlenet/react-app/web-lib'
-import { FC, useEffect } from 'react'
-import { post_to_url } from './helper.mjs'
+import { FC } from 'react'
 import { OpenIdInteractionPage } from './OpenIdInteractionPage.js'
 import { useOpenIdInteractionPage } from './OpenIdInteractionPageHook.js'
 
 export const OpenIdInteractionPageContainer: FC<{ interactionId: string }> = ({
   interactionId,
 }) => {
-  const webUser = useNeedsWebUserLogin()
-  const openIdInteractionPageResult = useOpenIdInteractionPage({ interactionId })
-  useEffect(() => {
-    if (openIdInteractionPageResult?.needsLogin) {
-      post_to_url(`/.pkg/@moodlenet/openid/interaction/${interactionId}/login`)
-    }
-  }, [interactionId, openIdInteractionPageResult?.needsLogin])
+  const openIdInteractionProps = useOpenIdInteractionPage({ interactionId })
 
-  if (openIdInteractionPageResult === null) {
+  if (openIdInteractionProps === null) {
     return <div>Interaction Id not found</div>
   }
 
-  if (openIdInteractionPageResult === undefined || openIdInteractionPageResult.needsLogin) {
+  if (openIdInteractionProps === undefined) {
     return null
   }
 
-  const openIdInteractionProps = openIdInteractionPageResult.props
-  return webUser && <OpenIdInteractionPage {...openIdInteractionProps} />
+  return <OpenIdInteractionPage {...openIdInteractionProps} />
 }
