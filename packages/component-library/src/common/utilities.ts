ee41840555b70ebe4a9a67160f1d42cbb9a2ad2c
aleclofabbro <alessandro.giansanti@gmail.com>
2023-04-06 16:13:55 +0200

    import lodash/defaultsDeep.js only

--- packages/component-library/src/common/utilities.ts
+++ packages/component-library/src/common/utilities.ts
@@ -1,4 +1,4 @@
-import { defaultsDeep } from 'lodash'
+import defaultsDeep from 'lodash/defaultsDeep.js'
 import { PartialDeep } from 'type-fest'
 
 export const overrideDeep = <T>(base: T, overrides: PartialDeep<T> | undefined): T => {
