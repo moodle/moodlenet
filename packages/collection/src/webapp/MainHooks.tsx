2681799c783f4587a6ea19cfc2601914801e69de
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-04-07 08:14:05 +0200

    save state and debounce

--- packages/collection/src/webapp/MainHooks.tsx
+++ packages/collection/src/webapp/MainHooks.tsx
@@ -1,3 +1,4 @@
+import debounce from 'lodash/debounce.js'
 import { useContext, useEffect, useMemo, useState } from 'react'
 import {
   CollectionActions,
@@ -5,38 +6,46 @@ import {
   CollectionMainProps,
   CollectionProps,
 } from '../common/types.mjs'
-
 import { MainContext } from './MainContext.js'
 
 type myProps = { collectionKey: string }
 export const useMainHook = ({ collectionKey }: myProps): CollectionMainProps | null => {
   const { rpcCaller } = useContext(MainContext)
   const [collection, setCollection] = useState<CollectionProps | null>()
+  const [saved, setSaved] = useState({ data: false, image: false })
 
   useEffect(() => {
     rpcCaller.get(collectionKey).then(data => setCollection(data))
   }, [collectionKey, rpcCaller, setCollection])
 
   const actions = useMemo((): CollectionActions => {
-    // const updateRespForm = (resourceForm: CollectionFormProps) => (
-    //   collection && setCollection(current => current && { ...current, resourceForm }), resourceForm
-    // )
-    const { _delete, edit, setIsPublished, setImage } = rpcCaller
+    const setterSave = (key: 'data' | 'image', val: boolean) => setSaved({ ...saved, [key]: val })
+    const updateData = <T,>(key: string, val: T): typeof collection =>
+      collection && { ...collection, data: { ...collection.data, [key]: val } }
 
-    const actions: CollectionActions = {
-      editData: async (res: CollectionFormProps) => edit(collectionKey, res),
+    const updateImageUrl = (imageUrl: string) => {
+      setterSave('image', false)
+      setCollection(updateData('imageUrl', imageUrl))
+    }
+
+    const { _delete, edit, setIsPublished, setImage } = rpcCaller
+    return {
+      async editData(res: CollectionFormProps) {
+        setterSave('data', true)
+        debounce(() => edit(collectionKey, res).then(() => setterSave('data', false))) // edit(collectionKey, file).then(() => setterSave('data', false))
+      },
+      async setImage(file: File) {
+        setterSave('image', true)
+        setImage(collectionKey, file).then(updateImageUrl)
+      },
       deleteCollection: () => _delete(collectionKey),
       publish: () => setIsPublished(collectionKey, true),
       unpublish: () => setIsPublished(collectionKey, false),
-      setImage: async (file: File) => {
-        setImage(collectionKey, file)
-      },
     }
-    return actions
-  }, [collectionKey, rpcCaller])
+  }, [collection, collectionKey, rpcCaller, saved])
 
   return useMemo<CollectionMainProps | null>(
-    () => (!collection ? null : { actions, props: collection }),
-    [actions, collection],
+    () => (!collection ? null : { actions, props: collection, saveState: saved }),
+    [actions, collection, saved],
   )
 }
--- packages/resource/src/webapp/ResourceHooks.tsx
+++ packages/resource/src/webapp/ResourceHooks.tsx
@@ -1,3 +1,4 @@
+import debounce from 'lodash/debounce.js'
 import { useContext, useEffect, useMemo, useState } from 'react'
 import { ResourceActions, ResourceFormProps, ResourceProps } from '../common.mjs'
 
@@ -16,36 +17,51 @@ export type ResourceCommonProps = {
   actions: ResourceActions
   props: ResourceProps
 }
+type SaveState = { form: boolean; image: boolean; content: boolean }
 
 type myProps = { resourceKey: string }
 export const useResourceBaseProps = ({ resourceKey }: myProps) => {
   const { rpcCaller } = useContext(MainContext)
   const [resource, setResource] = useState<ResourceProps | null>()
+  const [saved, setSaved] = useState<SaveState>({ form: false, image: false, content: false })
 
   useEffect(() => {
     rpcCaller.get(resourceKey).then(res => setResource(res))
   }, [resourceKey, rpcCaller, setResource])
 
   const actions = useMemo<ResourceActions>(() => {
-    // const updateResp = (resourceData: ResourceProps) => {
-    //   setResource(current => current && { ...current, resourceData })
-    // }
-    // const updateRespForm = (resourceForm: ResourceFormProps): ResourceFormProps => (
-    //   resource && updateResp({ ...resource, resourceForm }), resourceForm
-    // )
-    const { edit, setImage, setIsPublished, setContent, _delete } = rpcCaller // toggleBookmark, toggleLike,
+    const setterSave = (key: keyof SaveState, val: boolean) => setSaved({ ...saved, [key]: val })
+    const updateResource = <T,>(key: string, val: T): typeof resource =>
+      resource && { ...resource, [key]: val }
+    const updateData = <T,>(key: string, val: T) => updateResource('data', { [key]: val })
 
+    const updateImageUrl = (imageUrl: string) => (
+      setterSave('image', false), setResource(updateData('imageUrl', imageUrl)), imageUrl
+    )
+    const updateContent = (contentUrl: string) => (
+      setterSave('content', false), setResource(updateData('contentUrl', contentUrl)), contentUrl
+    )
+
+    const { edit, setImage, setIsPublished, setContent, _delete } = rpcCaller // toggleBookmark, toggleLike,
     const resourceActions: ResourceActions = {
+      async editData(res: ResourceFormProps) {
+        setterSave('form', true)
+        debounce(() => edit(resourceKey, res).then(() => setterSave('form', false))) // edit(collectionKey, file).then(() => setterSave('data', false))
+      },
+      async setImage(file: File) {
+        setterSave('image', true)
+        return setImage(resourceKey, file).then(updateImageUrl)
+      },
+      setContent(content: File | string) {
+        setterSave('content', true)
+        setContent(resourceKey, content).then(updateContent)
+      },
       publish: () => setIsPublished(resourceKey, true),
       unpublish: () => setIsPublished(resourceKey, false),
-      editData: (values: ResourceFormProps) => edit(resourceKey, values),
       deleteResource: () => _delete(resourceKey),
-      setImage: (file: File) => setImage(resourceKey, file),
-      setContent: (content: File | string) => setContent(resourceKey, content),
-      // toggleBookmark: () => toggleBookmark(resourceKey).then(updateResourceResp), toggleLike: () => toggleLike(resourceKey).then(updateResourceResp),
     }
     return resourceActions
-  }, [resourceKey, rpcCaller])
+  }, [resource, resourceKey, rpcCaller, saved])
 
   return useMemo<ResourceCommonProps | null>(
     () => (!resource ? null : { actions, props: resource }),
