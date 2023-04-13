542aea12ea8141840225d0650a34a501e9e7b099
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-04-13 15:49:43 +0200

    clean code

--- packages/collection/src/webapp/components/pages/Collection/CollectionPageContainer.tsx
+++ packages/collection/src/webapp/components/pages/Collection/CollectionPageContainer.tsx
@@ -5,5 +5,5 @@ import { useCollectionPageProps } from './CollectionPageHooks.js'
 export const CollectionContainer: FC<{ collectionKey: string }> = ({ collectionKey }) => {
   const panelProps = useCollectionPageProps({ collectionKey })
 
-  return panelProps ? <Collection {...panelProps} /> : <h2>Data not load</h2>
+  return panelProps ? <Collection {...panelProps} /> : null
 }
