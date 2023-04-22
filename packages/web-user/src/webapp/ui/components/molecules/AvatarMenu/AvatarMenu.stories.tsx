792b655469632f1d57b019d775ea06697b22fc2d
Bru Mas Ribera <brumasribera@gmail.com>
2023-04-22 11:01:46 +0200

    Add AddMenu to header

--- packages/web-user/src/webapp/ui/components/molecules/AvatarMenu/AvatarMenu.stories.tsx
+++ packages/web-user/src/webapp/ui/components/molecules/AvatarMenu/AvatarMenu.stories.tsx
@@ -1,12 +1,12 @@
 import { AddonItem } from '@moodlenet/component-library'
 import { href } from '@moodlenet/react-app/common'
 import { HeaderMenuItem } from '@moodlenet/react-app/ui'
-import { AvatarMenu } from './AvatarMenu.js'
 import {
+  AvatarMenu,
   HeaderProfileIcon,
   profileAvatarmenuItemReg,
   signoutAvatarmenuItemReg,
-} from './HeaderProfile.js'
+} from './AvatarMenu.js'
 
 const avatarPicture =
   'https://images.pexels.com/photos/3746326/pexels-photo-3746326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200'
