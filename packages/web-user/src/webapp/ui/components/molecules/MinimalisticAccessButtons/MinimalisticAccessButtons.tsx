e2995ff559f5d577af81cfe151a93b4c535e48bf
aleclofabbro <alessandro.giansanti@gmail.com>
2023-04-26 16:17:06 +0200

    fix minimalistic - needs registries to be implemented

--- packages/web-user/src/webapp/ui/components/molecules/MinimalisticAccessButtons/MinimalisticAccessButtons.tsx
+++ packages/web-user/src/webapp/ui/components/molecules/MinimalisticAccessButtons/MinimalisticAccessButtons.tsx
@@ -1,81 +1,18 @@
-import { AddonItem, PrimaryButton, SecondaryButton } from '@moodlenet/component-library'
+import { SecondaryButton } from '@moodlenet/component-library'
 import { Href } from '@moodlenet/react-app/common'
 import { Link } from '@moodlenet/react-app/ui'
 
-import { ReactElement } from 'react'
+import { FC } from 'react'
 import './MinimalisticAccessButtons.scss'
 
-export type MinimalisticHeaderProps = {
-  showLoginButton: boolean
-  showSignupButton: boolean
-  showLearnMoreButton: boolean
-  signupHref: Href
-  loginHref: Href
-}
-export type MinimalisticSlots = {
-  leftItems: AddonItem[]
-  centerItems: AddonItem[]
-  rightItems: AddonItem[]
-}
+export const SignupButtonMini: FC<{ signupHref: Href }> = ({ signupHref }) => (
+  <Link href={signupHref}>
+    <SecondaryButton color="orange">Sign up</SecondaryButton>
+  </Link>
+)
 
-export const getAccesMinimalisticHeaderItems = (
-  props: MinimalisticHeaderProps,
-): MinimalisticSlots => {
-  const { showLearnMoreButton, showLoginButton, showSignupButton, loginHref, signupHref } = props
-
-  const rightButtons = [
-    showSignupButton ? (
-      <Link href={signupHref}>
-        {/* // TODO //@ETTO Implement on Controller */}
-        <SecondaryButton className={`${showLoginButton ? 'visible' : ''}`} color="orange">
-          {/* <Trans> */}
-          Sign up
-          {/* </Trans> */}
-        </SecondaryButton>
-      </Link>
-    ) : null,
-    showLoginButton ? (
-      <Link href={loginHref}>
-        {/* TODO //@ETTO Implement on Controller */}
-        {showSignupButton ? (
-          <PrimaryButton>
-            {/* <Trans> */}
-            Main login
-            {/* </Trans> */}
-          </PrimaryButton>
-        ) : (
-          <SecondaryButton color="orange">
-            {/* <Trans> */}
-            Log in
-            {/* </Trans> */}
-          </SecondaryButton>
-        )}
-      </Link>
-    ) : null,
-    showLearnMoreButton ? (
-      <a href="https://moodle.com/moodlenet/" target="__blank">
-        <PrimaryButton color="grey">
-          {/* <Trans> */}
-          Learn more
-          {/* </Trans> */}
-        </PrimaryButton>
-      </a>
-    ) : null,
-  ].filter((item): item is ReactElement => !!item)
-
-  const updatedRightButtons: AddonItem[] =
-    rightButtons.length > 0
-      ? [
-          {
-            Item: () => <div className="minimalistic-access-buttons">{rightButtons}</div>,
-            key: '"access-buttons',
-          },
-        ]
-      : []
-
-  return {
-    leftItems: [],
-    centerItems: [],
-    rightItems: [...updatedRightButtons],
-  }
-}
+export const LoginButtonMini: FC<{ loginHref: Href }> = ({ loginHref }) => (
+  <Link href={loginHref}>
+    <SecondaryButton color="orange">Log in</SecondaryButton>
+  </Link>
+)
