cebb350d844d0db33ac2d58007f9ebe2c8f6c59a
aleclofabbro <alessandro.giansanti@gmail.com>
2023-05-11 14:32:20 +0200

    MySystemEntitiesId#getIdentifiersByKey

--- packages/system-entities/src/webapp/rt/use-my-sys-entity-ids.tsx
+++ packages/system-entities/src/webapp/rt/use-my-sys-entity-ids.tsx
@@ -1,10 +1,22 @@
 import { usePkgContext } from '@moodlenet/react-app/webapp'
 import { useMemo } from 'react'
-import { getIdAndEntityIdentifier, isOfSameClass } from '../../common/entity-identification.mjs'
+import {
+  getEntityIdentifiersByKey,
+  getIdAndEntityIdentifier,
+  isOfSameClass,
+} from '../../common/entity-identification.mjs'
 import type { EntityClass, EntityIdentifier } from '../../common/types.mjs'
 
 export type MySystemEntitiesId<EntityTypeNames extends string> = {
   isIdOfType(id: string | EntityIdentifier, type: EntityTypeNames): boolean
+  getIdentifiersByKey(_: { _key: string; type: EntityTypeNames }): {
+    entityIdentifier: EntityIdentifier
+    _id: string
+  }
+  // getIdentifiersById(_: { _id: string; type: EntityTypeNames }): {
+  //   entityIdentifier: EntityIdentifier
+  //   _id: string
+  // }
 }
 
 export function useMySystemEntitiesId<EntityTypeNames extends string>() {
@@ -25,8 +37,16 @@ export function useMySystemEntitiesId<EntityTypeNames extends string>() {
       return isOfSameClass(idClass, targetClass)
     }
 
+    const getIdentifiersByKey: MySystemEntitiesId<EntityTypeNames>['getIdentifiersByKey'] = ({
+      _key,
+      type,
+    }) => {
+      return getEntityIdentifiersByKey({ _key, pkgName, type })
+    }
+
     const mySystemEntitiesId: MySystemEntitiesId<EntityTypeNames> = {
       isIdOfType,
+      getIdentifiersByKey,
     }
 
     return mySystemEntitiesId
