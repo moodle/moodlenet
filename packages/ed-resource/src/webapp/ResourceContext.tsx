dcc3c4f7074fc846dfcf6a26db5cd9db8c8756ab
aleclofabbro <alessandro.giansanti@gmail.com>
2023-05-11 12:16:57 +0200

    MySystemEntitiesId in resource and collection contexts

--- packages/ed-resource/src/webapp/ResourceContext.tsx
+++ packages/ed-resource/src/webapp/ResourceContext.tsx
@@ -1,27 +1,42 @@
+import type { MySystemEntitiesId } from '@moodlenet/system-entities/webapp/rt'
+import { useMySystemEntitiesId } from '@moodlenet/system-entities/webapp/rt'
 import type { FC, PropsWithChildren } from 'react'
-import { createContext, useContext, useMemo } from 'react'
+import { createContext, useCallback, useContext, useMemo } from 'react'
+import type { EdResourceEntityNames } from '../common/types.mjs'
+import { getResourceHomePageRoutePath } from '../common/webapp-routes.mjs'
 import { MainContext } from './MainContext.js'
 
 export type ResourceContextT = {
-  create(): Promise<{ homePath: string }>
+  createResource(): Promise<{ homePath: string }>
+  resourceEntitiesId: MySystemEntitiesId<EdResourceEntityNames>
 }
 export const ResourceContext = createContext<ResourceContextT>(null as any)
 
-export function useResourceContext() {
+export function useResourceContextValue() {
   const { rpcCaller } = useContext(MainContext)
+
+  const resourceEntitiesId = useMySystemEntitiesId<EdResourceEntityNames>()
+
+  const createResource = useCallback<ResourceContextT['createResource']>(
+    async function create() {
+      const { _key } = await rpcCaller.create()
+      return { homePath: getResourceHomePageRoutePath({ _key }) }
+    },
+    [rpcCaller],
+  )
+
   const resourceContext = useMemo<ResourceContextT>(() => {
     const resourceContext: ResourceContextT = {
-      async create() {
-        const { _key } = await rpcCaller.create()
-        return { homePath: `/resource/${_key}` }
-      },
+      createResource,
+      resourceEntitiesId,
     }
     return resourceContext
-  }, [rpcCaller])
+  }, [createResource, resourceEntitiesId])
+
   return resourceContext
 }
 
 export const ResourceContextProvider: FC<PropsWithChildren> = ({ children }) => {
-  const resourceContext = useResourceContext()
+  const resourceContext = useResourceContextValue()
   return <ResourceContext.Provider value={resourceContext}>{children}</ResourceContext.Provider>
 }
