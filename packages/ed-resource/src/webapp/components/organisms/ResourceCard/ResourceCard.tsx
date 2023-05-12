55ec1dd1e51ca1779124d40561163423306af2d7
aleclofabbro <alessandro.giansanti@gmail.com>
2023-05-12 12:17:21 +0200

    resource list in collection page

--- packages/ed-resource/src/webapp/components/organisms/ResourceCard/ResourceCard.tsx
+++ packages/ed-resource/src/webapp/components/organisms/ResourceCard/ResourceCard.tsx
@@ -22,9 +22,8 @@ import {
   isEllipsisActive,
   TertiaryButton,
 } from '@moodlenet/component-library'
-import { getBackupImage, Link } from '@moodlenet/react-app/ui'
+import { getBackupImage, Link, proxied } from '@moodlenet/react-app/ui'
 import { Public } from '@mui/icons-material'
-import type { FC } from 'react'
 import { useEffect, useRef, useState } from 'react'
 import type {
   ResourceCardAccess,
@@ -52,227 +51,233 @@ type ResourceCardPropsUI = {
   orientation?: 'vertical' | 'horizontal'
 }
 
-export const ResourceCard: FC<ResourceCardProps & ResourceCardPropsUI> = ({
-  mainColumnItems,
-  topLeftItems,
-  topRightItems,
-  bottomLeftItems,
-  bottomRightItems,
+export const ResourceCard = proxied<ResourceCardProps & ResourceCardPropsUI>(
+  ({
+    mainColumnItems,
+    topLeftItems,
+    topRightItems,
+    bottomLeftItems,
+    bottomRightItems,
 
-  className,
-  orientation = 'vertical',
+    className,
+    orientation = 'vertical',
 
-  data,
-  state,
-  actions,
-  access,
-}) => {
-  const {
-    resourceId,
-    contentUrl,
-    imageUrl,
-    title,
-    // numLikes,
-    owner,
-    resourceHomeHref,
-    contentType,
-    downloadFilename,
-  } = data
-  const {
-    // liked,
-    // bookmarked,
-    isPublished,
-    // isSelected,
-    // selectionMode,
-  } = state
-  const {
-    // toggleLike,
-    //  toggleBookmark,
-    publish,
-    unpublish,
-  } = actions
-  const {
-    canPublish,
-    // canLike,
-    // isCreator,
-    // isAuthenticated,
-  } = access
+    data,
+    state,
+    actions,
+    access,
+  }) => {
+    const {
+      resourceId,
+      contentUrl,
+      imageUrl,
+      title,
+      // numLikes,
+      owner,
+      resourceHomeHref,
+      contentType,
+      downloadFilename,
+    } = data
+    const {
+      // liked,
+      // bookmarked,
+      isPublished,
+      // isSelected,
+      // selectionMode,
+    } = state
+    const {
+      // toggleLike,
+      //  toggleBookmark,
+      publish,
+      unpublish,
+    } = actions
+    const {
+      canPublish,
+      // canLike,
+      // isCreator,
+      // isAuthenticated,
+    } = access
 
-  const resourceCard = useRef<HTMLDivElement>(null)
-  const [size, setSize] = useState<'micro' | 'tiny' | 'small' | 'medium' | 'big'>('medium')
+    const resourceCard = useRef<HTMLDivElement>(null)
+    const [size, setSize] = useState<'micro' | 'tiny' | 'small' | 'medium' | 'big'>('medium')
 
-  const { typeName, typeColor } = getResourceTypeInfo(
-    contentType,
-    contentType === 'file' ? downloadFilename : contentUrl,
-  )
+    const { typeName, typeColor } = getResourceTypeInfo(
+      contentType,
+      contentType === 'file' ? downloadFilename : contentUrl,
+    )
 
-  const thumbnail = contentUrl && getThumbnailFromUrl(contentUrl)
+    const thumbnail = contentUrl && getThumbnailFromUrl(contentUrl)
 
-  const avatar = {
-    backgroundImage: 'url("' + (owner.avatar ? owner.avatar : defaultAvatar) + '")',
-    backgroundSize: 'cover',
-  }
-  let background = {}
-  if (orientation === 'horizontal') {
-    background = {
-      background:
-        'url("' + (thumbnail ? thumbnail : imageUrl ? imageUrl : getBackupImage(resourceId)) + '")',
+    const avatar = {
+      backgroundImage: 'url("' + (owner.avatar ? owner.avatar : defaultAvatar) + '")',
+      backgroundSize: 'cover',
     }
-  } else {
-    background = {
-      background:
-        'linear-gradient(0deg, rgba(0, 0, 0, 0.91) 0%, rgba(0, 0, 0, 0.1729) 45.15%, rgba(0, 0, 0, 0) 100%), url(' +
-        (thumbnail ? thumbnail : imageUrl ? imageUrl : getBackupImage(resourceId)) +
-        ')',
+    let background = {}
+    if (orientation === 'horizontal') {
+      background = {
+        background:
+          'url("' +
+          (thumbnail ? thumbnail : imageUrl ? imageUrl : getBackupImage(resourceId)) +
+          '")',
+      }
+    } else {
+      background = {
+        background:
+          'linear-gradient(0deg, rgba(0, 0, 0, 0.91) 0%, rgba(0, 0, 0, 0.1729) 45.15%, rgba(0, 0, 0, 0) 100%), url(' +
+          (thumbnail ? thumbnail : imageUrl ? imageUrl : getBackupImage(resourceId)) +
+          ')',
+      }
     }
-  }
-  background = { ...background, backgroundSize: 'cover', backgroundPosition: 'center center' }
+    background = { ...background, backgroundSize: 'cover', backgroundPosition: 'center center' }
 
-  const titleRef = useRef<HTMLElement>(null)
-  const [showTitleAbbr, setShowTitleAbbr] = useState(false)
-  useEffect(() => {
-    titleRef.current instanceof HTMLElement && setShowTitleAbbr(isEllipsisActive(titleRef.current))
-  }, [titleRef])
+    const titleRef = useRef<HTMLElement>(null)
+    const [showTitleAbbr, setShowTitleAbbr] = useState(false)
+    useEffect(() => {
+      titleRef.current instanceof HTMLElement &&
+        setShowTitleAbbr(isEllipsisActive(titleRef.current))
+    }, [titleRef])
 
-  const content = (
-    <div className="content">
-      {orientation === 'horizontal' && <div className={`image ${size}`} style={background} />}
-      <div className={`resource-card-content ${orientation ?? ''} ${size}`}>
-        <abbr className={`title ${orientation ?? ''}`} {...(showTitleAbbr && { title: title })}>
-          <span ref={titleRef}>{title}</span>
-        </abbr>
+    const content = (
+      <div className="content">
+        {orientation === 'horizontal' && <div className={`image ${size}`} style={background} />}
+        <div className={`resource-card-content ${orientation ?? ''} ${size}`}>
+          <abbr className={`title ${orientation ?? ''}`} {...(showTitleAbbr && { title: title })}>
+            <span ref={titleRef}>{title}</span>
+          </abbr>
+        </div>
       </div>
-    </div>
-  )
+    )
 
-  useEffect(() => {
-    const updateSize = () => {
-      setSize(
-        resourceCard.current && resourceCard.current.clientWidth < 300
-          ? 'micro'
-          : resourceCard.current && resourceCard.current.clientWidth < 400
-          ? 'tiny'
-          : resourceCard.current && resourceCard.current.clientWidth < 500
-          ? 'small'
-          : resourceCard.current && resourceCard.current.clientWidth < 700
-          ? 'medium'
-          : 'big',
-      )
-    }
-    updateSize()
-    window.addEventListener('resize', updateSize)
-    return () => window.removeEventListener('resize', updateSize)
-  }, [resourceCard])
+    useEffect(() => {
+      const updateSize = () => {
+        setSize(
+          resourceCard.current && resourceCard.current.clientWidth < 300
+            ? 'micro'
+            : resourceCard.current && resourceCard.current.clientWidth < 400
+            ? 'tiny'
+            : resourceCard.current && resourceCard.current.clientWidth < 500
+            ? 'small'
+            : resourceCard.current && resourceCard.current.clientWidth < 700
+            ? 'medium'
+            : 'big',
+        )
+      }
+      updateSize()
+      window.addEventListener('resize', updateSize)
+      return () => window.removeEventListener('resize', updateSize)
+    }, [resourceCard])
 
-  const typeLabel =
-    typeName && typeColor ? (
-      <div className="type" key="type-label" style={{ background: typeColor }}>
-        {typeName}
-      </div>
-    ) : null
+    const typeLabel =
+      typeName && typeColor ? (
+        <div className="type" key="type-label" style={{ background: typeColor }}>
+          {typeName}
+        </div>
+      ) : null
 
-  const updatedTopLeftItems = [typeLabel, ...(topLeftItems ?? [])].filter(
-    (item): item is AddonItem | JSX.Element => !!item,
-  )
+    const updatedTopLeftItems = [typeLabel, ...(topLeftItems ?? [])].filter(
+      (item): item is AddonItem | JSX.Element => !!item,
+    )
 
-  const updatedTopRightItems = [...(topRightItems ?? [])].filter(
-    (item): item is AddonItem /*  | JSX.Element */ => !!item,
-  )
+    const updatedTopRightItems = [...(topRightItems ?? [])].filter(
+      (item): item is AddonItem /*  | JSX.Element */ => !!item,
+    )
 
-  const header = (
-    <div className={`resource-card-header ${orientation} ${size}`} key="resource-card-header">
-      <div className="header-left">
-        {updatedTopLeftItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
-      </div>
-      <div className="header-right">
-        {updatedTopRightItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
-      </div>
-      {/* <div className={`level`}>
+    const header = (
+      <div className={`resource-card-header ${orientation} ${size}`} key="resource-card-header">
+        <div className="header-left">
+          {updatedTopLeftItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+        </div>
+        <div className="header-right">
+          {updatedTopRightItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+        </div>
+        {/* <div className={`level`}>
         <div className="name">
         L1
         </div>
       </div> */}
-    </div>
-  )
-
-  const ownerNameRef = useRef<HTMLElement>(null)
-  const [showOwnerNameAbbr, setShowOwnerNameAbbr] = useState(false)
-  useEffect(() => {
-    ownerNameRef.current instanceof HTMLElement &&
-      setShowOwnerNameAbbr(isEllipsisActive(ownerNameRef.current))
-  }, [ownerNameRef])
-
-  const avatarLabel = (
-    <Link href={owner.profileHref} key="avatar-label">
-      <div style={avatar} className="avatar" />
-      <abbr ref={ownerNameRef} {...(showOwnerNameAbbr && { title: owner.displayName })}>
-        {owner.displayName}
-      </abbr>
-    </Link>
-  )
-
-  const pulishButton = canPublish && (
-    <TertiaryButton
-      key="publish-button"
-      onClick={isPublished ? unpublish : publish}
-      className={`publish-button ${isPublished ? 'published' : 'unpublished'}`}
-      abbr={isPublished ? 'Unpublish' : 'Publish'}
-    >
-      <Public />
-    </TertiaryButton>
-  )
-
-  const updatedBottomLeftItems = [avatarLabel, ...(bottomLeftItems ?? [])].filter(
-    (item): item is AddonItem | JSX.Element => !!item,
-  )
-
-  const updatedBottomRightItems = [pulishButton, ...(bottomRightItems ?? [])].filter(
-    (item): item is AddonItem | JSX.Element => !!item,
-  )
-
-  const footer = (
-    <div className={`resource-card-footer ${orientation} ${size}`} key="footer">
-      <div className={`footer-left  ${orientation}`}>
-        {updatedBottomLeftItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
       </div>
-      <div className={`footer-right  ${orientation}`}>
-        {updatedBottomRightItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+    )
+
+    const ownerNameRef = useRef<HTMLElement>(null)
+    const [showOwnerNameAbbr, setShowOwnerNameAbbr] = useState(false)
+    useEffect(() => {
+      ownerNameRef.current instanceof HTMLElement &&
+        setShowOwnerNameAbbr(isEllipsisActive(ownerNameRef.current))
+    }, [ownerNameRef])
+
+    const avatarLabel = (
+      <Link href={owner.profileHref} key="avatar-label">
+        <div style={avatar} className="avatar" />
+        <abbr ref={ownerNameRef} {...(showOwnerNameAbbr && { title: owner.displayName })}>
+          {owner.displayName}
+        </abbr>
+      </Link>
+    )
+
+    const pulishButton = canPublish && (
+      <TertiaryButton
+        key="publish-button"
+        onClick={isPublished ? unpublish : publish}
+        className={`publish-button ${isPublished ? 'published' : 'unpublished'}`}
+        abbr={isPublished ? 'Unpublish' : 'Publish'}
+      >
+        <Public />
+      </TertiaryButton>
+    )
+
+    const updatedBottomLeftItems = [avatarLabel, ...(bottomLeftItems ?? [])].filter(
+      (item): item is AddonItem | JSX.Element => !!item,
+    )
+
+    const updatedBottomRightItems = [pulishButton, ...(bottomRightItems ?? [])].filter(
+      (item): item is AddonItem | JSX.Element => !!item,
+    )
+
+    const footer = (
+      <div className={`resource-card-footer ${orientation} ${size}`} key="footer">
+        <div className={`footer-left  ${orientation}`}>
+          {updatedBottomLeftItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+        </div>
+        <div className={`footer-right  ${orientation}`}>
+          {updatedBottomRightItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+        </div>
       </div>
-    </div>
-  )
+    )
 
-  const contentContainer = resourceHomeHref /* && !selectionMode */ ? (
-    <Link
-      className="content-container"
-      href={resourceHomeHref}
-      role="navigation"
-      key="content-container"
-    >
-      {content}
-    </Link>
-  ) : (
-    <div className="content-container">{content}</div>
-  )
+    const contentContainer = resourceHomeHref /* && !selectionMode */ ? (
+      <Link
+        className="content-container"
+        href={resourceHomeHref}
+        role="navigation"
+        key="content-container"
+      >
+        {content}
+      </Link>
+    ) : (
+      <div className="content-container">{content}</div>
+    )
 
-  const updatedMainColumnItems = [
-    header,
-    contentContainer,
-    footer,
-    ...(mainColumnItems ?? []),
-  ].filter((item): item is AddonItem | JSX.Element => !!item)
+    const updatedMainColumnItems = [
+      header,
+      contentContainer,
+      footer,
+      ...(mainColumnItems ?? []),
+    ].filter((item): item is AddonItem | JSX.Element => !!item)
 
-  return (
-    <Card
-      ref={resourceCard}
-      className={`resource-card ${className} ${
-        '' /* isSelected ? 'selected' : '' */
-      } ${orientation} `}
-      hover={true}
-      style={orientation === 'vertical' ? background : {}}
-    >
-      {updatedMainColumnItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
-    </Card>
-  )
-}
+    return (
+      <Card
+        ref={resourceCard}
+        className={`resource-card ${className} ${
+          '' /* isSelected ? 'selected' : '' */
+        } ${orientation} `}
+        hover={true}
+        style={orientation === 'vertical' ? background : {}}
+      >
+        {updatedMainColumnItems.map(i => ('Item' in i ? <i.Item key={i.key} /> : i))}
+      </Card>
+    )
+  },
+  'ResourceCard',
+)
 
 export default ResourceCard
