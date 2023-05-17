32f2eaf63fb38ee370f8756a9c3ce874d27e987b
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-05-17 09:14:44 +0200

    bookmarkButtonHook

--- packages/web-user/src/webapp/ui/components/atoms/BookmarkButton/BookmarkButtonHook.tsx
+++ packages/web-user/src/webapp/ui/components/atoms/BookmarkButton/BookmarkButtonHook.tsx
@@ -1,31 +1,36 @@
 // import { AuthCtx } from '@moodlenet/web-user/webapp'
-import { useMemo } from 'react'
-import { useProfileProps } from '../../pages/Profile/ProfileHooks.js'
+import { useContext, useMemo } from 'react'
+import { AuthCtx } from '../../../../context/AuthContext.js'
+import { shell } from '../../../../shell.mjs'
 import type { BookmarkButtonProps } from './BookmarkButton.js'
+
+const getVoid = () => {
+  return
+}
 
 export const useBookmarkButtonProps = ({
   profileKey,
 }: {
   profileKey: string
 }): BookmarkButtonProps | null => {
-  const pageProgs = useProfileProps({ profileKey })
+  const { isAuthenticated } = useContext(AuthCtx)
 
   const props = useMemo(() => {
-    if (!pageProgs) return null
-    const {
-      actions: { toggleFollow },
-      access: { canBookmark, isAuthenticated },
-    } = pageProgs
-
+    //  if (!pageProgs) return null
+    const toggleBookmark = () => {
+      shell.rpc.me[
+        'webapp/feature-entity/:action(add|remove)/:feature(bookmark|follow|like)/:entity_id'
+      ](getVoid(), { action: `add`, feature: 'bookmark', entity_id: profileKey })
+    }
     const props: BookmarkButtonProps = {
       bookmarked: true,
-      canBookmark,
+      canBookmark: true,
       isAuthenticated,
-      toggleBookmark: toggleFollow,
+      toggleBookmark,
     }
 
     return props
-  }, [pageProgs])
+  }, [isAuthenticated, profileKey])
 
   return props
 }
