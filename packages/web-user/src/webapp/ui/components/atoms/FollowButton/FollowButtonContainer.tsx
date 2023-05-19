ae54e7d7efd9cab616a7dbc6fd51da5bda1461d1
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-05-19 07:43:39 +0200

    renaming

--- packages/web-user/src/webapp/ui/components/atoms/FollowButton/FollowButtonContainer.tsx
+++ packages/web-user/src/webapp/ui/components/atoms/FollowButton/FollowButtonContainer.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react'
 import { FollowButton } from './FollowButton.js'
 import { useFollowButtonProps } from './FollowButtonHook.js'
 
-export const ProfileContainer: FC<{ profileKey: string }> = ({ profileKey }) => {
+export const FollowButtonContainer: FC<{ profileKey: string }> = ({ profileKey }) => {
   const panelProps = useFollowButtonProps({ profileKey })
   if (!panelProps) {
     return null
