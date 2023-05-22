e20c6a9a4cf57f7822d2b9bbb11c8f24003c1326
aleclofabbro <alessandro.giansanti@gmail.com>
2023-05-22 15:19:23 +0200

    added some social buttonss in some cards

--- packages/web-user/src/webapp/ui/components/atoms/LikeButton/LikeButtonHook.tsx
+++ packages/web-user/src/webapp/ui/components/atoms/LikeButton/LikeButtonHook.tsx
@@ -2,7 +2,7 @@
 import { useContext, useMemo, useState } from 'react'
 import type { KnownEntityType } from '../../../../../common/types.mjs'
 import { AuthCtx } from '../../../../context/AuthContext.js'
-import { useSocialActionHook } from '../../../../FeaturedEntitiesHook.js'
+import { useMyFeaturedEntity } from '../../../../MyProfile/MyFeaturedEntities.js'
 import type { LikeButtonProps } from './LikeButton.js'
 
 export const useLikeButtonProps = ({
@@ -13,25 +13,25 @@ export const useLikeButtonProps = ({
   entityType: KnownEntityType
 }): LikeButtonProps | null => {
   const { isAuthenticated } = useContext(AuthCtx)
-  const [liked, toggleLiked] = useSocialActionHook({ _key, entityType, feature: 'like' })
-  const [numLikes, setNumLikes] = useState(0)
+  const { isFeatured, toggle } = useMyFeaturedEntity({ _key, entityType, feature: 'like' })
+  const [numLikes, setNumLikes] = useState(0) // @ETTO use the service
 
   const props = useMemo<LikeButtonProps>(() => {
-    const toggleLike = () => {
-      toggleLiked().then(() => setNumLikes(num => ++num))
-    }
-
     const props: LikeButtonProps = {
-      liked,
+      liked: isFeatured,
       canLike: true,
       isCreator: false,
       isAuthenticated,
       toggleLike,
       numLikes,
     }
-
     return props
-  }, [isAuthenticated, liked, numLikes, toggleLiked])
+
+    function toggleLike() {
+      const deltaNumLikes = isFeatured ? -1 : 1
+      toggle().then(() => setNumLikes(num => num + deltaNumLikes))
+    }
+  }, [isAuthenticated, isFeatured, numLikes, toggle])
 
   return props
 }
