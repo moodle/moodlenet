e4ac0f5787664aafc79096d20ca4cfec8f22f0a3
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-05-25 16:54:33 +0200

    fix  to many render loop

--- packages/web-user/src/webapp/rt/init.tsx
+++ packages/web-user/src/webapp/rt/init.tsx
@@ -1,9 +1,7 @@
-import {
-  AddToCollectionButtonByResourceContextContainer as addResourceToCollectionButton,
-  CollectionCardPlugins,
-  CollectionPagePlugins,
-} from '@moodlenet/collection/webapp'
+import { BrowserCollectionList } from '@moodlenet/collection/ui'
+import { CollectionCardPlugins, CollectionPagePlugins } from '@moodlenet/collection/webapp'
 import { ResourceCardPlugins, ResourcePagePlugins } from '@moodlenet/ed-resource/webapp'
+import type { BrowserMainColumnItemBase } from '@moodlenet/react-app/ui'
 import {
   HeaderPlugins,
   registerAppRoutes,
@@ -11,19 +9,17 @@ import {
   type MainAppPluginHookResult,
 } from '@moodlenet/react-app/webapp'
 import { useMemo } from 'react'
-import './shell.mjs'
-
-import { BrowserCollectionList } from '@moodlenet/collection/ui'
-import type { BrowserMainColumnItemBase } from '@moodlenet/react-app/ui'
 import { useSwichAddonsByAuth } from './init/AddonsByUserRule.js'
 import MainWrapper from './MainWrapper.js'
-import { AddMenuContainer } from './menus/AddMenuContainer.js'
-import { AvatarMenuContainer } from './menus/AvatarMenuContainer.js'
-import { LoginButtonContainer, SignupButtonContainer } from './page/access/AccessContainers.js'
+import {
+  menuAddonsDefaultSetting,
+  menuHeaderButtonsAuthAddons,
+  resourcePageAddonsByAuth,
+} from './menus/menuAddons.js'
 import { BookmarksPagePlugin } from './page/bookmarks/BookmarksPageHook.mjs'
 import { useMyBookmarkedBrowserCollectionListDataProps as useBrowseBookCollection } from './page/bookmarks/MyBookmarkedBrowserCollectionListHook.mjs'
-import { UsersContainer, UsersMenu } from './page/settings/UsersContainer.js'
 import { pkgRoutes } from './routes.js'
+import './shell.mjs'
 import {
   useFollowAndBookMarkButtons,
   useLikeAndBookMarkButtons,
@@ -32,41 +28,23 @@ import {
 registerAppRoutes(pkgRoutes)
 registerMainAppPluginHook(() => useMemo<MainAppPluginHookResult>(() => ({ MainWrapper }), []))
 
-const menuAddonsHeaderButtons = {
-  loginButton: { Item: LoginButtonContainer },
-  signupButton: { Item: SignupButtonContainer },
-  avatarMenu: { Item: AvatarMenuContainer },
-  addMenu: { Item: AddMenuContainer },
-}
-const { loginButton, signupButton, avatarMenu, addMenu } = menuAddonsHeaderButtons
-const _ = {
-  guest: { loginButton, signupButton },
-  auth: { addMenu, avatarMenu },
-  root: { addMenu: null, avatarMenu },
-}
-HeaderPlugins.register(({ useRightItems }) => {
-  const addons = useSwichAddonsByAuth(_)
+HeaderPlugins.register(({ useRightItems }) =>
+  useRightItems(useSwichAddonsByAuth(menuHeaderButtonsAuthAddons)),
+)
 
-  useRightItems(addons)
-})
+AdminSettingsPagePlugins.register(({ useAdminSettingsSection }) =>
+  useAdminSettingsSection(menuAddonsDefaultSetting),
+)
 
-const menuAddonsDefaultSetting = { default: { Content: UsersContainer, Menu: UsersMenu } }
-AdminSettingsPagePlugins.register(({ useAdminSettingsSection }) => {
-  useAdminSettingsSection(menuAddonsDefaultSetting)
-})
-
-const authItems = { addToCollectionButton: { Item: addResourceToCollectionButton } }
-const emptyItems = { addToCollectionButton: null }
-const __ = { guest: emptyItems, root: emptyItems, auth: authItems }
 ResourcePagePlugins.register(({ useGeneralAction, useTopRightHeaderItems, resourceKey }) => {
-  const addons = useSwichAddonsByAuth(__)
-  useGeneralAction(addons)
+  useGeneralAction(useSwichAddonsByAuth(resourcePageAddonsByAuth))
   useTopRightHeaderItems(useLikeAndBookMarkButtons(resourceKey, 'resource'))
 })
 
-ResourceCardPlugins.register(({ useTopRightItems, resourceKey }) =>
-  useTopRightItems(useLikeAndBookMarkButtons(resourceKey, 'resource')),
-)
+ResourceCardPlugins.register(({ useTopRightItems, resourceKey }) => {
+  useTopRightItems(useLikeAndBookMarkButtons(resourceKey, 'resource'))
+  // useTopRightItems({bookMarkButton: bookMarkButton(resourceKey) })
+})
 
 CollectionCardPlugins.register(({ collectionKey, useTopRightItems }) =>
   useTopRightItems(useFollowAndBookMarkButtons(collectionKey, 'collection')),
