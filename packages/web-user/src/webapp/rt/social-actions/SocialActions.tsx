f2760a97ed008a44ff94d1cdd229408d4bca3c53
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-05-27 04:53:51 +0200

    move out map function

--- packages/web-user/src/webapp/rt/social-actions/SocialActions.tsx
+++ packages/web-user/src/webapp/rt/social-actions/SocialActions.tsx
@@ -9,24 +9,27 @@ export type SocialActionsName = 'follow' | 'like' | 'bookmark'
 export type EntityAndKey = { _key: string; entityType: KnownEntityType }
 export type SocialActions = Partial<Record<SocialActionsName, FC<EntityAndKey>>>
 export type SocialActionsConfig = Partial<Record<KnownEntityType, SocialActionsName[]>>
+export type PkgAddOnsByName = Record<SocialActionsName, PkgAddOns<AddonItemNoKey>>
 // test if type work const pakElem: PkgAddOns<AddonItemNoKey> = { aaa: { Item: () => <LikeButtonContainer _key="dd" entityType="collection" /> }
 
 const mapSocialActionsToPkgAddons = (sc: SocialActions, props: EntityAndKey) =>
   objectMap(sc, (Fc, name) => ({ [name]: { Item: () => (!Fc ? null : <Fc {...props} />) } }))
 
-type MyPkgAddOns = PkgAddOns<AddonItemNoKey> | null // alias
-export const socialItemsAddons = (
-  socialKeyItems: SocialActions,
-  addonsByEnity: SocialActionsConfig,
-  props: EntityAndKey,
-) => {
-  const pkgAddons = mapSocialActionsToPkgAddons(socialKeyItems, props)
-  const mapSocialActionDef = (acc: MyPkgAddOns, name: SocialActionsName) => ({
+const mapSocialActionElements =
+  (pkgAddons: PkgAddOnsByName) => (acc: MyPkgAddOns, name: SocialActionsName) => ({
     ...acc,
     ...pkgAddons[name],
   })
+
+type MyPkgAddOns = PkgAddOns<AddonItemNoKey> | null // alias
+export const socialItemsAddons = (
+  socialActions: SocialActions,
+  addonsByEnity: SocialActionsConfig,
+  props: EntityAndKey,
+) => {
+  const pkgAddons = mapSocialActionsToPkgAddons(socialActions, props)
   const itemELemStrList = addonsByEnity[props.entityType]
-  return !itemELemStrList ? null : itemELemStrList.reduce(mapSocialActionDef, {})
+  return !itemELemStrList ? null : itemELemStrList.reduce(mapSocialActionElements(pkgAddons), {})
 }
 
 export function getUseSocialActions(actions: SocialActions, config: SocialActionsConfig) {
