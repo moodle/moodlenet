3d28467f034e92d30c66778233449d4008d6dc37
Bru Mas Ribera <brumasribera@gmail.com>
2023-07-24 13:13:19 +0200

    Tag empty collections as Unnamed on Add to collection modal

--- packages/collection/src/webapp/components/atoms/AddToCollectionButton/storiesData.ts
+++ packages/collection/src/webapp/components/atoms/AddToCollectionButton/storiesData.ts
@@ -6,6 +6,7 @@ export const CollectionTextOptionProps: OptionItemProp[] = [
   { label: 'Algebra', value: 'Algebra' },
   { label: 'Phycology', value: 'Phycology' },
   { label: 'Phylosophy', value: 'Phylosophy' },
+  { label: '', value: '' },
   { label: 'Sociology', value: 'Sociology' },
   { label: 'English Literature', value: 'English Literature' },
 ]
