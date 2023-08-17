4669d3d0592ef7a17c40429f053488f9cf5f7a8c
aleclofabbro <alessandro.giansanti@gmail.com>
2023-08-17 09:06:32 +0200

    web-user fetch and use configs

--- packages/web-user/src/webapp/rt/MainWrapper.tsx
+++ packages/web-user/src/webapp/rt/MainWrapper.tsx
@@ -3,12 +3,15 @@ import { AuthCtxProvider } from './context/AuthContext.js'
 
 import { GeneralSettingsCtxProvider } from './context/GeneralSettingsCtxProvider.js'
 import { MyProfileContextProvider } from './context/MyProfileContext.js'
+import { ProfileContextProvider } from './context/ProfileContext.js'
 
 const MainComponent: MainAppPluginWrapper = ({ children }) => {
   return (
     <GeneralSettingsCtxProvider>
       <AuthCtxProvider>
-        <MyProfileContextProvider>{children}</MyProfileContextProvider>
+        <ProfileContextProvider>
+          <MyProfileContextProvider>{children}</MyProfileContextProvider>
+        </ProfileContextProvider>
       </AuthCtxProvider>
     </GeneralSettingsCtxProvider>
   )
