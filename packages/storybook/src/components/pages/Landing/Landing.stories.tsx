01334fa12d668107ab6eb843c6764ef2f517b1a0
Bru Mas Ribera <brumasribera@gmail.com>
2023-09-26 10:59:11 +0200

    Minor searchbox font size fix

--- packages/storybook/src/components/pages/Landing/Landing.stories.tsx
+++ packages/storybook/src/components/pages/Landing/Landing.stories.tsx
@@ -104,7 +104,7 @@ export const LandingLoggedOutStoryProps: LandingProps = {
 export const LandingLoggedInStoryProps: LandingProps = {
   ...LandingLoggedOutStoryProps,
   mainLayoutProps: MainLayoutLoggedInStoryProps,
-  showSetInterestsSnackbar: true,
+  // showSetInterestsSnackbar: true,
   mainColumnItems: [
     {
       Item: () => (
