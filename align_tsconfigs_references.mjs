ef727aca0b76c459ea00999864237384bd063c0f
aleclofabbro <alessandro.giansanti@gmail.com>
2024-03-05 17:51:17 +0100

    lerna bootstrap --hoist & aligned all shared packages versions

---
+++ align_tsconfigs_references.mjs
@@ -0,0 +1,36 @@
+import { readdirSync, readFileSync, statSync, writeFileSync } from 'fs'
+import * as path from 'path'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+const packagesDirs = readdirSync(path.resolve(__dirname, 'packages')).map(pkg_name =>
+  path.resolve(__dirname, 'packages', pkg_name),
+)
+
+packagesDirs.forEach(pkgDir => {
+  if (pkgDir === 'storybook') {
+    return
+  }
+  console.log('pkgDir', pkgDir)
+  const pkgJsonFile = path.resolve(pkgDir, 'package.json')
+  const pkgJsonFileExists = !!statSync(pkgJsonFile, { throwIfNoEntry: false })
+  if (!pkgJsonFileExists) return
+  const tsconfigJsonFile = path.resolve(pkgDir, 'tsconfig.json')
+  const tsconfigJsonFileExists = !!statSync(tsconfigJsonFile, { throwIfNoEntry: false })
+  if (!tsconfigJsonFileExists) return
+
+  const pkgJson = JSON.parse(readFileSync(pkgJsonFile, 'utf8'))
+  const tsconfigJson = JSON.parse(readFileSync(tsconfigJsonFile, 'utf8'))
+
+  tsconfigJson.references = [
+    ...new Set(
+      Object.keys({
+        ...pkgJson.devDependencies,
+        ...pkgJson.peerDependencies,
+        ...pkgJson.dependencies,
+      }).filter(depName => depName.startsWith('@moodlenet/')),
+    ),
+  ].map(_ => ({ path: `../${_.replace(/^@moodlenet\//, '')}` }))
+
+  writeFileSync(tsconfigJsonFile, JSON.stringify(tsconfigJson, null, 2) + '\n')
+})
