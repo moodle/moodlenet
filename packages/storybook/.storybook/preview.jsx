d036b33024bef9611e60cf62106a85b680162448
Bru Mas Ribera <brumasribera@gmail.com>
2024-04-15 17:16:28 +0200

    Start using React Email

--- packages/storybook/.storybook/preview.jsx
+++ packages/storybook/.storybook/preview.jsx
@@ -1,9 +1,8 @@
 import { MemoryRouter } from 'react-router-dom'
 import SnackbarCtxProvider from '../../component-library/src/webapp/ui/components/atoms/Snackbar/SnackbarContext'
 import { ProvideViewport } from '../../react-app/src/webapp/ui/lib/viewport'
-import { ProvideStorybookLinkComponent } from '../src/lib/SBLinkComponent'
-import { ProvideSBMainSearchBoxCtx } from '../src/MainSearchBoxCtxProvider'
 import { Wrapper } from '../src/Wrapper'
+import { ProvideStorybookLinkComponent } from '../src/lib/SBLinkComponent'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -35,19 +34,27 @@ export const parameters = {
 }
 
 export const decorators = [
-  Story => (
-    <ProvideViewport>
-      <MemoryRouter>
-        <ProvideStorybookLinkComponent>
-          <Wrapper>
-            <ProvideSBMainSearchBoxCtx>
+  (Story, context) => {
+    const isNoWrapper = context.parameters?.layout === 'no-wrapper'
+
+    return (
+      <ProvideViewport>
+        <MemoryRouter>
+          <ProvideStorybookLinkComponent>
+            {isNoWrapper ? (
               <SnackbarCtxProvider>
                 <Story />
               </SnackbarCtxProvider>
-            </ProvideSBMainSearchBoxCtx>
-          </Wrapper>
-        </ProvideStorybookLinkComponent>
-      </MemoryRouter>
-    </ProvideViewport>
-  ),
+            ) : (
+              <Wrapper>
+                <SnackbarCtxProvider>
+                  <Story />
+                </SnackbarCtxProvider>
+              </Wrapper>
+            )}
+          </ProvideStorybookLinkComponent>
+        </MemoryRouter>
+      </ProvideViewport>
+    )
+  },
 ]
