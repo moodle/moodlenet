4c127f0711a80d7a2c305af751f5480593b3e4bd
aleclofabbro <alessandro.giansanti@gmail.com>
2024-10-25 19:03:54 +0200

    moodlenet-nextjs-react-app

--- pri/moodlenet-nextjs-react-app-e2e/playwright.config.ts
+++ pri/moodlenet-nextjs-react-app-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx start moodlenet-react-app',
+    command: 'npx nx start moodlenet-nextjs-react-app',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
--- pri/moodlenet-nextjs-react-app/jest.config.ts
+++ pri/moodlenet-nextjs-react-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'moodlenet-react-app',
+  displayName: 'moodlenet-nextjs-react-app',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/pri/moodlenet-react-app',
+  coverageDirectory: '../../coverage/pri/moodlenet-nextjs-react-app',
 }
