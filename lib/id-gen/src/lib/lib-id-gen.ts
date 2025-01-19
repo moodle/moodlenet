1fadf8549370bca5c20aaa3d0641e5f2fe26c1b8
aleclofabbro <alessandro.giansanti@gmail.com>
2025-01-19 23:02:58 +0100

    reviewed alphanum generation 5 chars time based id + 3 chars nanoid time slots of 0.2 secs to base62 string + 238328 nanoid combinations (62**3 cahrs)

--- lib/id-gen/src/lib/lib-id-gen.ts
+++ lib/id-gen/src/lib/lib-id-gen.ts
@@ -15,20 +15,24 @@ export function generateUlid({ onDate }: { onDate: Date | number | string }) {
   const date = new Date(onDate)
   return globalMonoUlid(date.valueOf())
 }
+const ALPHNUM_62 = `useandom26T198340PX75pxJACKVERYMINDBUSHWOLFGQZbfghjklqvwyzrict` // urlAlphabet except -_
+const DEFAULT_ALPHANUM_ID_LENGTH = 8
+export function generateAlphanumId(opts?: { length?: number }) {
+  const length = opts?.length ?? DEFAULT_ALPHANUM_ID_LENGTH
+  const restId = customAlphabet(ALPHNUM_62)(length - 5)
+  const currentZeroDeltaDateAlphanum5 = getCurrentZeroDeltaDateAlphanum5()
 
-export function generateNanoId(opts?: { alpabet?: string; length?: number }) {
-  const id = customAlphabet(
-    //opts?.alpabet || urlAlphabet,
-    opts?.alpabet || `useandom26T198340PX75pxJACKVERYMINDBUSHWOLFGQZbfghjklqvwyzrict`, // urlAlphabet except -_
-    8,
-  )(opts?.length)
-  return id
+  const alphanumId = `${currentZeroDeltaDateAlphanum5}${restId}`
+  console.log({ length, restId, currentZeroDeltaDateAlphanum5, alphanumId })
+  return alphanumId
 }
+//FIXME: REMOVE generateNanoId by renaming to generateAlphanumId
+export const generateNanoId = generateAlphanumId
 
 export function generateId(id_type: id_type) {
   switch (id_type.type) {
     case 'alphanumeric':
-      return generateNanoId({ length: id_type.length })
+      return generateAlphanumId({ length: id_type.length })
     case 'ulid':
       return generateUlid({ onDate: id_type.onDate })
   }
@@ -39,4 +43,46 @@ export function decodeUlid(ulid: string) {
     return null
   }
   return ulidx.decodeTime(ulid)
+}
+
+const ZERO_DATE = Number(new Date('01/01/2020'))
+
+function getCurrentZeroDeltaDateAlphanum5() {
+  const fifthSecondsFromZeroDate = Math.floor((Number(new Date()) - ZERO_DATE) / 200)
+
+  const reversedFifthSecondsFromZeroDateStringArray = String(fifthSecondsFromZeroDate).split('').reverse()
+
+  // ensures first reversed digit is not 0
+  reversedFifthSecondsFromZeroDateStringArray[0] =
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    reversedFifthSecondsFromZeroDateStringArray[0] === '0' ? '1' : reversedFifthSecondsFromZeroDateStringArray[0]!
+  const base62encodingNumber = Number(reversedFifthSecondsFromZeroDateStringArray.join(''))
+  const encoded62 = encodeBase62(base62encodingNumber)
+  console.log({
+    fifthSecondsFromZeroDate,
+    reversedFifthSecondsFromZeroDateStringArray,
+    base62encodingNumber,
+    encoded62,
+  })
+  return encoded62
+}
+
+// https://lowrey.me/encoding-decoding-base-62-in-es6-javascript/
+function encodeBase62(integer: number) {
+  if (integer === 0) {
+    return 0
+  }
+  let s: string[] = []
+  while (integer > 0) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    s = [ALPHNUM_62[integer % 62]!, ...s]
+    integer = Math.floor(integer / 62)
+  }
+  return s.join('')
+}
+function _decodeBase62(chars: string) {
+  return chars
+    .split('')
+    .reverse()
+    .reduce((prev, curr, i) => prev + ALPHNUM_62.indexOf(curr) * 62 ** i, 0)
 }
