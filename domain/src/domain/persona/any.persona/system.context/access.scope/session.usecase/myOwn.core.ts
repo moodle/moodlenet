b8337317b37bb392db7baf8a415e0d825574bde5
aleclofabbro <alessandro.giansanti@gmail.com>
2025-03-06 13:46:44 +0100

    better ^^'

--- domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.core.ts
+++ domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.core.ts
@@ -1,11 +1,12 @@
 import type { myOwn as myOwnType } from './myOwn.endpoint'
-export const myOwn: moo.core.endpoint<myOwnType> = async (_, { model, over }, ctx) => {
-  const authSessionToken = ctx.coreRequest.gateRequest.info.claims.server.authSessionToken
-
-  ctx.log.debug(`myOwn ctx.coreRequest.gateRequest.info.claims.server.authSessionToken: ${authSessionToken}`)
-  const { info } = await over(model.accessControl.getTokenPermissionsInfo).call.query({ authSessionToken })
-  ctx.log.debug(`myOwn info: `, info)
-  return {
-    permissions: info.tree,
-  }
+export const myOwn: moo.core.endpoint<myOwnType> = async (_, __, ctx) => {
+  return { permissions: ctx.coreRequest.permissionsInfo.tree }
 }
+// export const myOwn: moo.core.endpoint<myOwnType> = async (_, { model, over }, ctx) => {
+//   const authSessionToken = ctx.coreRequest.gateRequest.info.claims.server.authSessionToken
+
+//   const { info } = await over(model.accessControl.getTokenPermissionsInfo).call.query({ authSessionToken })
+//   return {
+//     permissions: info.tree,
+//   }
+// }
