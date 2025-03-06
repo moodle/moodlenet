84c5d04914d7acc109b15aeb64824d02a8c7d41f
aleclofabbro <alessandro.giansanti@gmail.com>
2025-03-06 13:40:31 +0100

    any/system/access/session/myOwn

--- domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.core.ts
+++ domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.core.ts
@@ -1,3 +1,11 @@
-import { any_ } from '@moodle/lib-types'
 import type { myOwn as myOwnType } from './myOwn.endpoint'
-export const myOwn: moo.core.endpoint<myOwnType> =undefined as any_
+export const myOwn: moo.core.endpoint<myOwnType> = async (_, { model, over }, ctx) => {
+  const authSessionToken = ctx.coreRequest.gateRequest.info.claims.server.authSessionToken
+
+  ctx.log.debug(`myOwn ctx.coreRequest.gateRequest.info.claims.server.authSessionToken: ${authSessionToken}`)
+  const { info } = await over(model.accessControl.getTokenPermissionsInfo).call.query({ authSessionToken })
+  ctx.log.debug(`myOwn info: `, info)
+  return {
+    permissions: info.tree,
+  }
+}
--- domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.endpoint.ts
+++ domain/src/domain/persona/any.persona/system.context/access.scope/session.usecase/myOwn.endpoint.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-invalid-void-type */
-import { signed_token } from '@moodle/lib-types'
 import * as E from 'fp-ts/Either'
 import { void as voidz, ZodVoid } from 'zod'
 
-export type myOwn = moo.persona.endpoint<[ZodVoid, { permissions: moo.permissions.user.tree; token: signed_token }, undefined]>
+export type myOwn = moo.persona.endpoint<[ZodVoid, { permissions: moo.permissions.user.tree }, undefined]>
 
 export const myOwn: moo.gate.provider.endpoint<myOwn> = () => E.right({ zod: voidz() })
